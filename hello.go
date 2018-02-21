1c1727b4cce6acfe46126a1e60722e271ebe4161
Andy Williams <andy@andywilliams.me>
2018-02-21 22:26:39 +0000

    Updated example to show layout and button

--- hello.go
+++ hello.go
@@ -1,13 +1,25 @@
 package main
 
+import "fmt"
+
 import "github.com/fyne-io/fyne/ui"
+import "github.com/fyne-io/fyne/ui/layout"
+import "github.com/fyne-io/fyne/ui/widget"
 import "github.com/fyne-io/fyne-app"
 
 func main() {
 	app := fyneapp.NewApp()
 
 	w := app.NewWindow("Hello")
-	w.Canvas().SetContent(ui.NewText("Hello Fyne!"))
+	quit := widget.NewButton("Quit", func() {
+		app.Quit()
+	})
+	w.Canvas().SetContent(ui.NewContainer(
+		[]ui.CanvasObject{
+			ui.NewText("Hello Fyne!"),
+			button,
+		},
+		layout.NewGridLayout(1)))
 
 	w.Show()
 }
