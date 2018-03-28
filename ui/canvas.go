0ff0d07ac22ceaea73ebd271e73b967ad7a2c463
Andy Williams <andy@andywilliams.me>
2018-03-28 15:44:27 +0100

    Start moving towards refreshing each change

--- ui/canvas.go
+++ ui/canvas.go
@@ -4,6 +4,7 @@ package ui
 // Each canvas has a scale which is automatically applied during the render process.
 type Canvas interface {
 	SetContent(CanvasObject)
+	Refresh(CanvasObject)
 
 	Scale() float32
 	SetScale(float32)
