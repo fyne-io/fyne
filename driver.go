b48543713661f91b4cd0491ce4bb8a69ff28910f
Andy Williams <andy@andywilliams.me>
2018-03-31 20:20:36 +0100

    Fix formatting

--- driver.go
+++ driver.go
@@ -5,7 +5,7 @@ import "github.com/fyne-io/fyne/ui"
 // Driver defines an abstract concept of a Fyne render driver.
 // Any implementation must provide at least these methods.
 type Driver interface {
-	Run() // Start the driver
-	Quit() // Cleanly exit the driver
+	Run()                          // Start the driver
+	Quit()                         // Cleanly exit the driver
 	CreateWindow(string) ui.Window // Create a new UI Window
 }
