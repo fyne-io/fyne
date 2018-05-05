b438e5a90123216215fc22fc4d55d5bc60227645
Andy Williams <andy@andywilliams.me>
2018-05-05 23:00:04 +0100

    fix typo

--- examples/canvas.go
+++ examples/canvas.go
@@ -8,8 +8,8 @@ import "github.com/fyne-io/fyne/ui/canvas"
 import "github.com/fyne-io/fyne/ui/layout"
 
 func rgbGradient(x, y, w, h int) color.RGBA {
-	g := int(float32(x) / float32(h) * float32(255))
-	b := int(float32(y) / float32(w) * float32(255))
+	g := int(float32(x) / float32(w) * float32(255))
+	b := int(float32(y) / float32(h) * float32(255))
 
 	return color.RGBA{uint8(255 - b), uint8(g), uint8(b), 0xff}
 }
