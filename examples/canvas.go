24e054f5fe83913371a4deb67fc891adf38317f2
Andy Williams <andy@andywilliams.me>
2018-05-09 20:23:30 +0100

    Use the new image file loader in our canvas example

--- examples/canvas.go
+++ examples/canvas.go
@@ -1,6 +1,8 @@
 package examples
 
+import "runtime"
 import "image/color"
+import "path"
 
 import "github.com/fyne-io/fyne/app"
 import "github.com/fyne-io/fyne/ui"
@@ -17,12 +19,15 @@ func rgbGradient(x, y, w, h int) color.RGBA {
 func Canvas(app app.App) {
 	w := app.NewWindow("Main")
 
+	_, filename, _, _ := runtime.Caller(0)
+	exampleDir := path.Dir(path.Dir(filename))
 	content := ui.NewContainer(
 		canvas.NewText("Resize me"),
 		&canvas.Rectangle{FillColor: color.RGBA{0x80, 0, 0, 0xff},
 			StrokeColor: color.RGBA{0xff, 0xff, 0xff, 0xff},
 			StrokeWidth: 1},
 		canvas.NewRaster(rgbGradient),
+		canvas.NewImageFromFile(path.Join(exampleDir, "fyne.png")),
 		&canvas.Line{StrokeColor: color.RGBA{0, 0x80, 0, 0xff}, StrokeWidth: 5},
 		&canvas.Circle{StrokeColor: color.RGBA{0, 0, 0x80, 0xff},
 			FillColor:   color.RGBA{0x30, 0x30, 0x30, 0x60},
