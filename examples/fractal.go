8c653d70016e3d5c0b5ce335f50472c11b5fe642
Andy Williams <andy@andywilliams.me>
2018-05-15 13:26:57 +0100

    Add keyboard handling to fractal app to zoom and pan
    This requires reworking of the maxIteration code to work at scale

--- examples/fractal.go
+++ examples/fractal.go
@@ -8,10 +8,14 @@ import "github.com/fyne-io/fyne/ui"
 import "github.com/fyne-io/fyne/ui/canvas"
 import "github.com/fyne-io/fyne/ui/theme"
 
-const maxIterations = 100
-const currScale = 3.2
-const currX = -0.75
-const currY = 0.0
+var currIterations uint = 100
+
+var currScale = 1.0
+var currX = -0.75
+var currY = 0.0
+
+var fractalWindow ui.Window
+var fractalCanvas ui.CanvasObject
 
 type fractalLayout struct {
 	canvas ui.CanvasObject
@@ -25,6 +29,12 @@ func (c *fractalLayout) MinSize(objects []ui.CanvasObject) ui.Size {
 	return ui.NewSize(320, 240)
 }
 
+func refresh() {
+	currIterations = uint(100 * (1 + math.Pow((math.Log10(1/currScale)), 1.25)))
+
+	fractalWindow.Canvas().Refresh(fractalCanvas)
+}
+
 func scaleColor(c float64, start, end uint8) uint8 {
 	if end >= start {
 		return (uint8)(c*float64(end-start)) + start
@@ -34,14 +44,15 @@ func scaleColor(c float64, start, end uint8) uint8 {
 }
 
 func mandelbrot(px, py, w, h int) color.RGBA {
+	drawScale := 3.5 * currScale
 	aspect := (float64(h) / float64(w))
-	c_re := ((float64(px)/float64(w))-0.5)*currScale + currX
-	c_im := ((float64(py)/float64(w))-(0.5*aspect))*currScale - currY
+	c_re := ((float64(px)/float64(w))-0.5)*drawScale + currX
+	c_im := ((float64(py)/float64(w))-(0.5*aspect))*drawScale - currY
 
-	var i int
+	var i uint
 	var x, y, xsq, ysq float64
 
-	for i = 0; i < maxIterations && (xsq+ysq <= 4); i++ {
+	for i = 0; i < currIterations && (xsq+ysq <= 4); i++ {
 		x_new := float64(xsq-ysq) + c_re
 		y = 2*x*y + c_im
 		x = x_new
@@ -50,10 +61,10 @@ func mandelbrot(px, py, w, h int) color.RGBA {
 		ysq = y * y
 	}
 
-	if i == maxIterations {
+	if i == currIterations {
 		return theme.BackgroundColor()
 	} else {
-		mu := (float64(i) / float64(maxIterations))
+		mu := (float64(i) / float64(currIterations))
 		c := math.Sin((mu / 2) * math.Pi)
 
 		return color.RGBA{scaleColor(c, theme.PrimaryColor().R, theme.TextColor().R),
@@ -62,12 +73,31 @@ func mandelbrot(px, py, w, h int) color.RGBA {
 	}
 }
 
-func Fractal(app app.App) {
-	fractalWindow := app.NewWindow("Fractal")
-	fractal := &fractalLayout{canvas: canvas.NewRaster(mandelbrot)}
+func fractalKeyDown(ev *ui.KeyEvent) {
+	delta := currScale * 0.2
+	if ev.Name == "Up" {
+		currY -= delta
+	} else if ev.Name == "Down" {
+		currY += delta
+	} else if ev.Name == "Left" {
+		currX += delta
+	} else if ev.Name == "Right" {
+		currX -= delta
+	} else if ev.String == "+" {
+		currScale /= 1.1
+	} else if ev.String == "-" {
+		currScale *= 1.1
+	}
 
-	container := ui.NewContainer(fractal.canvas)
-	container.Layout = fractal
+	refresh()
+}
+func Fractal(app app.App) {
+	fractalWindow = app.NewWindow("Fractal")
+	fractalCanvas = canvas.NewRaster(mandelbrot)
+
+	container := ui.NewContainer(fractalCanvas)
+	container.Layout = &fractalLayout{canvas: fractalCanvas}
 	fractalWindow.Canvas().SetContent(container)
+	fractalWindow.Canvas().SetOnKeyDown(fractalKeyDown)
 	fractalWindow.Show()
 }
