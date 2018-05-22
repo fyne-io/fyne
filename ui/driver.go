2187255d823b7a6b62ec3864e43c98ac2702b317
Andy Williams <andy@andywilliams.me>
2018-05-22 21:40:38 +0100

    Refactoring driver to hide unnecessary lifecycle details

--- ui/driver.go
+++ ui/driver.go
@@ -3,8 +3,6 @@ package ui
 // Driver defines an abstract concept of a Fyne render driver.
 // Any implementation must provide at least these methods.
 type Driver interface {
-	Run()                       // Start the driver
-	Quit()                      // Cleanly exit the driver
 	CreateWindow(string) Window // Create a new UI Window
 	AllWindows() []Window       // Get a slice containing all app windows
 }
