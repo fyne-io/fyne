0bc5ff3f3d0783c4d760e551aea2acfed7e0a526
Andy Williams <andy@andywilliams.me>
2018-05-29 22:42:31 +0100

    Correct the windows URL opening

--- fyneapp_windows.go
+++ fyneapp_windows.go
@@ -3,5 +3,5 @@ package fyneapp
 import "os/exec"
 
 func (app *eflApp) OpenURL(url string) {
-	exec.Command("start", "", url).Run()
+	exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Run()
 }
