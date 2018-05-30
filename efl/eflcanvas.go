5eff7906f3d978ad49ea765abe151c1871b40d12
Andy Williams <andy@andywilliams.me>
2018-05-30 15:46:13 +0100

    Refactor text metrics and helper functions
    Match the new requirement that drivers report on text MinSize

--- efl/eflcanvas.go
+++ efl/eflcanvas.go
@@ -130,22 +130,6 @@ func queueRender(c *eflCanvas, co ui.CanvasObject) {
 	<-done
 }
 
-func nativeTextBounds(obj *C.Evas_Object) ui.Size {
-	width, height := 0, 0
-	var w, h C.Evas_Coord
-	length := int(C.strlen(C.evas_object_text_text_get(obj)))
-
-	for i := 0; i < length; i++ {
-		C.evas_object_text_char_pos_get(obj, C.int(i), nil, nil, &w, &h)
-		width += int(w) + 2
-		if int(h) > height {
-			height = int(h)
-		}
-	}
-
-	return ui.Size{width, height}
-}
-
 func (c *eflCanvas) buildObject(o ui.CanvasObject, target ui.CanvasObject, size ui.Size) *C.Evas_Object {
 	var obj *C.Evas_Object
 
@@ -159,9 +143,6 @@ func (c *eflCanvas) buildObject(o ui.CanvasObject, target ui.CanvasObject, size 
 			C.int(to.Color.B), C.int(to.Color.A))
 
 		updateFont(obj, c, to)
-		native := nativeTextBounds(obj)
-		min := ui.Size{unscaleInt(c, native.Width), unscaleInt(c, native.Height)}
-		to.SetMinSize(min)
 	case *canvas.Rectangle:
 		obj = C.evas_object_rectangle_add(c.evas)
 		ro, _ := o.(*canvas.Rectangle)
@@ -281,18 +262,7 @@ func (c *eflCanvas) refreshObject(o, o2 ui.CanvasObject, pos ui.Position, size u
 			C.int(to.Color.B), C.int(to.Color.A))
 
 		updateFont(obj, c, to)
-		native := nativeTextBounds(obj)
-		min := ui.Size{unscaleInt(c, native.Width), unscaleInt(c, native.Height)}
-		to.SetMinSize(min)
-
-		switch to.Alignment {
-		case ui.TextAlignLeading:
-			pos = ui.NewPos(pos.X, pos.Y+(size.Height-min.Height)/2)
-		case ui.TextAlignCenter:
-			pos = ui.NewPos(pos.X+(size.Width-min.Width)/2, pos.Y+(size.Height-min.Height)/2)
-		case ui.TextAlignTrailing:
-			pos = ui.NewPos(pos.X+size.Width-min.Width, pos.Y+(size.Height-min.Height)/2)
-		}
+		pos = getTextPosition(to, pos, size)
 
 		C.evas_object_geometry_set(obj, C.Evas_Coord(scaleInt(c, pos.X)), C.Evas_Coord(scaleInt(c, pos.Y)),
 			C.Evas_Coord(scaleInt(c, size.Width)), C.Evas_Coord(scaleInt(c, size.Height)))
@@ -478,22 +448,6 @@ func (c *eflCanvas) SetContent(o ui.CanvasObject) {
 
 	c.setup(o)
 	c.Refresh(o)
-}
-
-func updateFont(obj *C.Evas_Object, c *eflCanvas, t *canvas.Text) {
-	font := theme.TextFont()
-
-	if t.Bold {
-		if t.Italic {
-			font = theme.TextBoldItalicFont()
-		} else {
-			font = theme.TextBoldFont()
-		}
-	} else if t.Italic {
-		font = theme.TextItalicFont()
-	}
-
-	C.evas_object_text_font_set(obj, C.CString(font), C.Evas_Font_Size(scaleInt(c, t.FontSize)))
 }
 
 func scaleInt(c ui.Canvas, v int) int {
---
+++ efl/text.go
@@ -0,0 +1,65 @@
+package efl
+
+// #cgo pkg-config: ecore-evas
+// #include <Evas.h>
+import "C"
+
+import "github.com/fyne-io/fyne/ui"
+import "github.com/fyne-io/fyne/ui/canvas"
+import "github.com/fyne-io/fyne/ui/theme"
+
+func updateFont(obj *C.Evas_Object, c *eflCanvas, t *canvas.Text) {
+	font := theme.TextFont()
+
+	if t.Bold {
+		if t.Italic {
+			font = theme.TextBoldItalicFont()
+		} else {
+			font = theme.TextBoldFont()
+		}
+	} else if t.Italic {
+		font = theme.TextItalicFont()
+	}
+
+	C.evas_object_text_font_set(obj, C.CString(font), C.Evas_Font_Size(scaleInt(c, t.FontSize)))
+}
+
+func getTextPosition(t *canvas.Text, pos ui.Position, size ui.Size) ui.Position {
+	min := t.MinSize()
+
+	switch t.Alignment {
+	case ui.TextAlignCenter:
+		return ui.NewPos(pos.X+(size.Width-min.Width)/2, pos.Y+(size.Height-min.Height)/2)
+	case ui.TextAlignTrailing:
+		return ui.NewPos(pos.X+size.Width-min.Width, pos.Y+(size.Height-min.Height)/2)
+	default:
+		return ui.NewPos(pos.X, pos.Y+(size.Height-min.Height)/2)
+	}
+}
+
+func nativeTextBounds(obj *C.Evas_Object) ui.Size {
+	width, height := 0, 0
+	var w, h C.Evas_Coord
+	length := int(C.strlen(C.evas_object_text_text_get(obj)))
+
+	for i := 0; i < length; i++ {
+		C.evas_object_text_char_pos_get(obj, C.int(i), nil, nil, &w, &h)
+		width += int(w) + 2
+		if int(h) > height {
+			height = int(h)
+		}
+	}
+
+	return ui.Size{width, height}
+}
+
+func (d *eFLDriver) RenderedTextSize(text string, size int) ui.Size {
+	c := ui.GetDriver().AllWindows()[0].Canvas().(*eflCanvas)
+
+	textObj := C.evas_object_text_add(c.evas)
+	C.evas_object_text_text_set(textObj, C.CString(text))
+	C.evas_object_text_font_set(textObj, C.CString(theme.TextFont()), C.Evas_Font_Size(scaleInt(c, size)))
+
+	native := nativeTextBounds(textObj)
+	return ui.NewSize(unscaleInt(c, native.Width), unscaleInt(c, native.Height))
+}
