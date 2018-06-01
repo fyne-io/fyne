9c027224967adf9d3adba5b589a67f8233de7447
Andy Williams <andy@andywilliams.me>
2018-06-01 14:59:05 +0100

    Move implementation to desktop/ folder ready for merge

--- desktop/fyneapp_darwin.go
+++ desktop/fyneapp_darwin.go
@@ -2,7 +2,7 @@ package fyneapp
 
 import "os/exec"
 
-import _ "github.com/fyne-io/fyne-app/efl" // import our Fyne driver for Mac OS X
+import _ "github.com/fyne-io/fyne-app/desltop/efl" // import our Fyne driver for Mac OS X
 
 func (app *fyneApp) OpenURL(url string) {
 	exec.Command("open", url).Run()
