3520fc4f3ac9aaa37d03ab7c1699b0eac81b866b
Andy Williams <andy@andywilliams.me>
2018-06-02 23:52:59 +0100

    Fix lint issues

--- examples/driver/desktop.go
+++ examples/driver/desktop.go
@@ -5,6 +5,7 @@ package driver
 import "github.com/fyne-io/fyne/api/app"
 import "github.com/fyne-io/fyne/desktop"
 
+// NewApp creates a new desktop app to run the examples
 func NewApp() app.App {
 	return desktop.NewApp()
 }
--- examples/driver/headless.go
+++ examples/driver/headless.go
@@ -5,6 +5,7 @@ package driver
 import "github.com/fyne-io/fyne/api/app"
 import "github.com/fyne-io/fyne/test"
 
+// NewApp creates a new headless app to test the examples code
 func NewApp() app.App {
 	return test.NewTestApp()
 }
