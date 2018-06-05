924cf0f1d2a45bffdf864ee97a35b86034ab3cde
Andy Williams <andy@andywilliams.me>
2018-06-05 14:16:13 +0100

    Add some more list testing

--- api/ui/widget/list_test.go
+++ api/ui/widget/list_test.go
@@ -7,10 +7,24 @@ import "github.com/stretchr/testify/assert"
 import _ "github.com/fyne-io/fyne/test"
 
 func TestListSize(t *testing.T) {
+	list := NewList(NewLabel("Hello"), NewLabel("World"))
+	assert.True(t, list.MinSize().Height > 0)
+}
+
+func TestListPrepend(t *testing.T) {
 	list := NewList(NewLabel("World"))
 	assert.True(t, list.MinSize().Height > 0)
 
 	min := list.MinSize()
 	list.Prepend(NewLabel("Hello"))
+	assert.True(t, list.MinSize().Height > min.Height)
+}
+
+func TestListAppend(t *testing.T) {
+	list := NewList(NewLabel("Hello"))
+	assert.True(t, list.MinSize().Height > 0)
+
+	min := list.MinSize()
+	list.Append(NewLabel("World"))
 	assert.True(t, list.MinSize().Height > min.Height)
 }
