72d0e4d39cae94c0a4c6c25240af1e8d1b0199c0
Andy Williams <andy@andy.xyz>
2018-11-19 16:57:27 +0000

    Update legacy entry point for GL driver (in case this merges soon)

--- desktop/app-gl.go
+++ desktop/app-gl.go
@@ -2,11 +2,14 @@
 
 package desktop
 
+import "log"
+
 import "github.com/fyne-io/fyne"
-import "github.com/fyne-io/fyne/desktop/gl"
+import "github.com/fyne-io/fyne/app"
+import "github.com/fyne-io/fyne/driver/gl"
 
 // NewApp returns a new app instance using the desktop (GL) driver.
 func NewApp() fyne.App {
 	log.Println("desktop.NewApp() is deprecated - please use app.New()")
-	return fyne.NewAppWithDriver(gl.NewGLDriver())
+	return app.NewAppWithDriver(gl.NewGLDriver())
 }
