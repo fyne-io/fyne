39f5d5b6fce73d33d3cc20d73ffe4e90ef92300b
Andy Williams <andy@andy.xyz>
2018-11-20 11:58:02 +0000

    Fix naming collision

--- driver/gl/draw.go
+++ driver/gl/draw.go
@@ -66,10 +66,10 @@ func compileShader(source string, shaderType uint32) (uint32, error) {
 		var logLength int32
 		gl.GetShaderiv(shader, gl.INFO_LOG_LENGTH, &logLength)
 
-		log := strings.Repeat("\x00", int(logLength+1))
-		gl.GetShaderInfoLog(shader, logLength, nil, gl.Str(log))
+		info := strings.Repeat("\x00", int(logLength+1))
+		gl.GetShaderInfoLog(shader, logLength, nil, gl.Str(info))
 
-		return 0, fmt.Errorf("failed to compile %v: %v", source, log)
+		return 0, fmt.Errorf("failed to compile %v: %v", source, info)
 	}
 
 	return shader, nil
