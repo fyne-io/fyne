1da5ee5460deee5c7b1eea5689bb7fd73205ead5
Andy Williams <andy@andy.xyz>
2018-12-19 21:44:02 +0000

    Update example docs in the main package

--- fyne.go
+++ fyne.go
@@ -1,6 +1,7 @@
 // Package fyne describes the objects and components available to any Fyne app.
 // These can all be created, manipulated and tested without renering (for speed).
-// Your main package should import a driver which will actually render your UI.
+// Your main package should use the app package to create an application with
+// a default driver that will render your UI.
 //
 // A simple application may look like this:
 //
@@ -13,7 +14,7 @@
 //   	a := app.New()
 //
 //   	w := a.NewWindow("Hello")
-//   	w.SetContent(widget.NewList(
+//   	w.SetContent(widget.NewVBox(
 //   		widget.NewLabel("Hello Fyne!"),
 //   		widget.NewButton("Quit", func() {
 //   			app.Quit()
