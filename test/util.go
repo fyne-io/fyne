4b1e2b97c2666fd287421bd758a2f7f285a24074
Andy Williams <andy@andy.xyz>
2018-12-30 17:16:34 +0000

    Add some handy test utilities

---
+++ test/util.go
@@ -0,0 +1,21 @@
+package test
+
+import "github.com/fyne-io/fyne"
+
+// Click simulates a mouse click on the specified object
+func Click(obj fyne.ClickableObject) {
+	ev := &fyne.MouseEvent{Button:fyne.LeftMouseButton, Position:fyne.NewPos(1, 1)}
+	obj.OnMouseDown(ev)
+}
+
+// Type performs a series of key events to simulate typing of a value into the specified object.
+// The focusable object will be focused before typing begins.
+// The chars parameter will be input one rune at a time to the focused object.
+func Type(obj fyne.FocusableObject, chars string) {
+	obj.OnFocusGained()
+
+	for _, char := range chars {
+		ev := &fyne.KeyEvent{String: string(char), Name: fyne.KeyName(char)}
+		obj.OnKeyDown(ev)
+	}
+}
