917ecc490d6f6f6b34427acc4cd37cd27789bb00
Andy Williams <andy@andy.xyz>
2019-01-08 21:51:10 +0000

    Import basic macOS package code

--- cmd/fyne/package.go
+++ cmd/fyne/package.go
@@ -3,10 +3,17 @@ package main
 import (
 	"flag"
 	"fmt"
+	"image"
+	_ "image/jpeg"
+	_ "image/png"
+	"io"
+	"log"
 	"os"
 	"path/filepath"
 	"runtime"
 	"strings"
+
+	"github.com/jackmordaunt/icns"
 )
 
 func exists(path string) bool {
@@ -17,8 +24,38 @@ func exists(path string) bool {
 	return true
 }
 
+func ensureSubDir(parent, name string) string {
+	path := filepath.Join(parent, name)
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		os.Mkdir(path, os.ModePerm)
+	}
+	return path
+}
+
+func copyFile(src, tgt string) error {
+	_, err := os.Stat(src)
+	if err != nil {
+		return err
+	}
+
+	source, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer source.Close()
+
+	target, err := os.Create(tgt)
+	if err != nil {
+		return err
+	}
+	defer target.Close()
+
+	_, err = io.Copy(target, source)
+	return err
+}
+
 type packager struct {
-	os, name, exe string
+	os, name, dir, exe, icon string
 }
 
 func (p *packager) packageLinux() {
@@ -26,7 +63,64 @@ func (p *packager) packageLinux() {
 }
 
 func (p *packager) packageDarwin() {
+	appDir := ensureSubDir(p.dir, p.name+".app")
 
+	contentsDir := ensureSubDir(appDir, "Contents")
+	info := filepath.Join(contentsDir, "Info.plist")
+	infoFile, _ := os.Create(info)
+	io.WriteString(infoFile, "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"+
+		"<!DOCTYPE plist PUBLIC \"-//Apple Computer//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\">"+
+		"<plist version=\"1.0\">"+
+		"<dict>"+
+		"<key>CFBundleGetInfoString</key>"+
+		"<string>"+p.name+"</string>"+
+		"<key>CFBundleExecutable</key>"+
+		"<string>"+p.name+"</string>"+
+		"<key>CFBundleIdentifier</key>"+
+		"<string>com.example."+p.name+"</string>"+
+		"<key>CFBundleName</key>"+
+		"<string>"+p.name+"</string>"+
+		"<key>CFBundleIconFile</key>"+
+		"<string>icon.icns</string>"+
+		"<key>CFBundleShortVersionString</key>"+
+		"<string>1.0</string>"+
+		"<key>CFBundleInfoDictionaryVersion</key>"+
+		"<string>6.0</string>"+
+		"<key>CFBundlePackageType</key>"+
+		"<string>APPL</string>"+
+		"<key>IFMajorVersion</key>"+
+		"<integer>0</integer>"+
+		"<key>IFMinorVersion</key>"+
+		"<integer>1</integer>"+
+		"<key>NSHighResolutionCapable</key><true/>"+
+		"<key>NSSupportsAutomaticGraphicsSwitching</key><true/>"+
+		"</dict>"+
+		"</plist>")
+
+	macOSDir := ensureSubDir(contentsDir, "MacOS")
+	binName := filepath.Join(macOSDir, p.name)
+	copyFile(p.exe, binName)
+
+	resDir := ensureSubDir(contentsDir, "Resources")
+	icnsPath := filepath.Join(resDir, "icon.icns")
+
+	img, err := os.Open(p.icon)
+	if err != nil {
+		log.Fatalf("opening source image: %v", err)
+	}
+	defer img.Close()
+	srcImg, _, err := image.Decode(img)
+	if err != nil {
+		log.Fatalf("decoding source image: %v", err)
+	}
+	dest, err := os.Create(icnsPath)
+	if err != nil {
+		log.Fatalf("opening destination file: %v", err)
+	}
+	defer dest.Close()
+	if err := icns.Encode(dest, srcImg); err != nil {
+		log.Fatalf("encoding icns: %v", err)
+	}
 }
 
 func (p *packager) packageWindows() {
@@ -37,6 +131,7 @@ func (p *packager) addFlags() {
 	flag.StringVar(&p.os, "os", runtime.GOOS, "The operating system to target (like GOOS)")
 	flag.StringVar(&p.exe, "executable", "", "The path to the executable, default is the current dir main binary")
 	flag.StringVar(&p.name, "name", "", "The name of the application, default is the executable file name")
+	flag.StringVar(&p.icon, "icon", "", "The name of the application icon file")
 }
 
 func (*packager) printHelp(indent string) {
@@ -46,12 +141,13 @@ func (*packager) printHelp(indent string) {
 }
 
 func (p *packager) run(_ []string) {
-	if p.exe == "" {
-		dir, err := os.Getwd()
-		if err != nil {
-			fmt.Fprintln(os.Stderr, "Unable to get the current directory, needed to find main executable")
-		}
+	dir, err := os.Getwd()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "Unable to get the current directory, needed to find main executable")
+	}
+	p.dir = dir
 
+	if p.exe == "" {
 		exeName := filepath.Base(dir)
 		if p.os == "windows" {
 			exeName = exeName + ".exe"
@@ -74,7 +170,11 @@ func (p *packager) run(_ []string) {
 	case "linux":
 		p.packageLinux()
 	case "darwin":
-		p.packageDarwin()
+		if p.icon == "" {
+			fmt.Fprintln(os.Stderr, "You must specify the -icon parameter for macOS packaging")
+		} else {
+			p.packageDarwin()
+		}
 	case "windows":
 		p.packageWindows()
 	default:
