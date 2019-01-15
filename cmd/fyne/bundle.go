fe0aa20318cbf7e8b5a104369fb288a0aa3d7a0c
Andy Williams <andy@andy.xyz>
2019-01-15 13:57:37 +0000

    Better sanitising of variable names, and add a prefix

--- cmd/fyne/bundle.go
+++ cmd/fyne/bundle.go
@@ -7,6 +7,7 @@ import (
 	"io/ioutil"
 	"os"
 	"path"
+	"regexp"
 	"strings"
 
 	"fyne.io/fyne"
@@ -14,6 +15,7 @@ import (
 
 type bundler struct {
 	name, pkg string
+	prefix    string
 	noheader  bool
 }
 
@@ -35,11 +37,13 @@ func writeHeader(pkg string) {
 	fmt.Println("import \"fyne.io/fyne\"")
 }
 
-func sanitiseName(file string) string {
-	name := strings.Replace(file, ".", "-", -1)
-	name = strings.Replace(name, " ", "", -1)
+func sanitiseName(file, prefix string) string {
+	titled := strings.Title(file)
 
-	return strings.ToLower(name)
+	reg, _ := regexp.Compile("[^a-zA-Z0-9]+")
+	name := reg.ReplaceAllString(titled, "")
+
+	return prefix + name
 }
 
 // Bundle takes a file (at filepath) and serialises it into Go to be output into
@@ -47,14 +51,14 @@ func sanitiseName(file string) string {
 // (pkg) and the data will be assigned to variable named "name". If you are
 // appending an existing resource file then pass true to noheader as the headers
 // should only be output once per file.
-func doBundle(name, pkg string, noheader bool, filepath string) {
+func doBundle(name, pkg, prefix string, noheader bool, filepath string) {
 	if !noheader {
 		writeHeader(pkg)
 	}
 	fmt.Println()
 
 	if name == "" {
-		name = sanitiseName(path.Base(filepath))
+		name = sanitiseName(path.Base(filepath), prefix)
 	}
 	writeResource(filepath, name)
 }
@@ -62,12 +66,14 @@ func doBundle(name, pkg string, noheader bool, filepath string) {
 func (b *bundler) addFlags() {
 	flag.StringVar(&b.name, "name", "", "The variable name to assign the serialised resource")
 	flag.StringVar(&b.pkg, "package", "main", "The package to output in headers (if not appending)")
+	flag.StringVar(&b.prefix, "prefix", "resource", "A prefix for variables (ignored if name is set)")
 	flag.BoolVar(&b.noheader, "append", false, "Append an existing go file (don't output headers)")
 }
 
 func (b *bundler) printHelp(indent string) {
 	fmt.Println(indent, "The bundle command embeds static content into your go application.")
 	fmt.Println(indent, "Command usage: fyne bundle [parameters] file")
+	fmt.Println(indent, "Each resource will have a generated filename unless specified")
 }
 
 func (b *bundler) run(args []string) {
@@ -76,5 +82,5 @@ func (b *bundler) run(args []string) {
 		return
 	}
 
-	doBundle(b.name, b.pkg, b.noheader, args[0])
+	doBundle(b.name, b.pkg, b.prefix, b.noheader, args[0])
 }
