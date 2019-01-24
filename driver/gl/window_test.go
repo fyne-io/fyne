2ebefe76c2a1f503d754b4d55729dabf761717b4
Andy Williams <andy@andy.xyz>
2019-01-24 22:30:46 +0000

    Add test for issue fixed in PR #94

---
+++ driver/gl/window_test.go
@@ -0,0 +1,19 @@
+package gl
+
+import (
+	"testing"
+
+	_ "fyne.io/fyne/test"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestWindow_SetTitle(t *testing.T) {
+	d := NewGLDriver()
+	w := d.CreateWindow("Test")
+
+	title := "My title"
+	w.SetTitle(title)
+
+	assert.Equal(t, title, w.Title())
+}
