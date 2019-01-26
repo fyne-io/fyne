4abb6525a31bbe2b9745bececc7381a7640bb710
lucor <lu.corbo@gmail.com>
2019-01-26 21:17:28 +0100

    Add clipboard interface

--- clipboard.go
+++ clipboard.go
@@ -1,20 +1,9 @@
 package fyne
 
-// Clipboard represents the system clipboard
-type Clipboard struct {
-	Window
-}
-
-// Content returns the clipboard content
-func (c *Clipboard) Content() string {
-	content, err := c.Window.GetClipboardString()
-	if err != nil {
-		return ""
-	}
-	return content
-}
-
-// SetContent sets the clipboard content
-func (c *Clipboard) SetContent(content string) {
-	c.Window.SetClipboardString(content)
+// Clipboard represents the system clipboard interface
+type Clipboard interface {
+	// Content returns the clipboard content
+	Content() string
+	// SetContent sets the clipboard content
+	SetContent(content string)
 }
