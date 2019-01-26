c505b7b90bc71c3e4b631c054c4b65f168123960
Andy Williams <andy@andy.xyz>
2019-01-26 14:31:00 +0000

    Add missing progress docs

--- dialog/progress.go
+++ dialog/progress.go
@@ -6,16 +6,20 @@ import (
 	"fyne.io/fyne/widget"
 )
 
+// ProgressDialog is a simple dialog window that displays text and a progress bar.
 type ProgressDialog struct {
 	*dialog
 
 	bar *widget.ProgressBar
 }
 
+// SetValue updates the value of the progress bar - this should be between 0.0 and 1.0.
 func (p *ProgressDialog) SetValue(v float64) {
 	p.bar.SetValue(v)
 }
 
+// NewProgress creates a progress dialog and returns the handle.
+// Using the returned type you should call Show() and then set it's value through SetValue().
 func NewProgress(title, message string, parent fyne.Window) *ProgressDialog {
 	d := newDialog(title, message, theme.InfoIcon(), nil /*cancel?*/, parent)
 	bar := widget.NewProgressBar()
