c1ebca3111fe09a1382ccb237bfa5deb32428c05
Andy Williams <andy@andy.xyz>
2019-02-02 21:09:12 +0000

    Add missing theme file from demo

---
+++ cmd/fyne_demo/theme.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"image/color"
+
+	"fyne.io/fyne"
+	"fyne.io/fyne/theme"
+)
+
+var (
+	purple = &color.RGBA{128, 0, 128, 255}
+	orange = &color.RGBA{198, 123, 0, 255}
+	grey   = &color.Gray{123}
+)
+
+type customTheme struct {
+}
+
+func (customTheme) BackgroundColor() color.Color {
+	return purple
+}
+
+func (customTheme) ButtonColor() color.Color {
+	return color.Black
+}
+
+func (customTheme) HyperlinkColor() color.Color {
+	return orange
+}
+
+func (customTheme) TextColor() color.Color {
+	return color.White
+}
+
+func (customTheme) PlaceHolderColor() color.Color {
+	return grey
+}
+
+func (customTheme) PrimaryColor() color.Color {
+	return orange
+}
+
+func (customTheme) FocusColor() color.Color {
+	return orange
+}
+
+func (customTheme) TextSize() int {
+	return 12
+}
+
+func (customTheme) TextFont() fyne.Resource {
+	return theme.DefaultTextBoldFont()
+}
+
+func (customTheme) TextBoldFont() fyne.Resource {
+	return theme.DefaultTextBoldFont()
+}
+
+func (customTheme) TextItalicFont() fyne.Resource {
+	return theme.DefaultTextBoldItalicFont()
+}
+
+func (customTheme) TextBoldItalicFont() fyne.Resource {
+	return theme.DefaultTextBoldItalicFont()
+}
+
+func (customTheme) TextMonospaceFont() fyne.Resource {
+	return theme.DefaultTextMonospaceFont()
+}
+
+func (customTheme) Padding() int {
+	return 10
+}
+
+func (customTheme) IconInlineSize() int {
+	return 20
+}
+
+func newCustomTheme() fyne.Theme {
+	return &customTheme{}
+}
