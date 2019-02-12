b82de4f60a00bb4a9e48e8dbc2ed0d1be3de99b1
Andy Williams <andy@andywilliams.me>
2019-02-12 22:18:51 +0000

    Fix build of EFL darwin with cursors

--- driver/efl/efl_darwin.go
+++ driver/efl/efl_darwin.go
@@ -7,6 +7,9 @@ package efl
 // #include <Ecore_Cocoa.h>
 import "C"
 
+import "fyne.io/fyne"
+import "fyne.io/fyne/widget"
+
 func oSEngineName() string {
 	return "opengl_cocoa"
 }
@@ -27,10 +30,10 @@ func setCursor(w *window, object fyne.CanvasObject) {
 		cursor = C.ECORE_COCOA_CURSOR_POINTING_HAND
 	}
 
-	C.ecore_cocoa_window_cursor_set(win, C.uint(cursor))
+	C.ecore_cocoa_window_cursor_set(win, C.Ecore_Cocoa_Cursor(cursor))
 }
 
 func unsetCursor(w *window, object fyne.CanvasObject) {
 	win := C.ecore_evas_cocoa_window_get(w.ee)
-	C.ecore_cocoa_window_cursor_set(win, C.ECORE_X_CURSOR_X)
+	C.ecore_cocoa_window_cursor_set(win, C.ECORE_COCOA_CURSOR_ARROW)
 }
