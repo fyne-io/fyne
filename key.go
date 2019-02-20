7053fd60172fd5f9c30ad05276487946a4e582d6
Andy Williams <andy@andy.xyz>
2019-02-20 11:22:31 +0000

    Fix the key name types

--- key.go
+++ key.go
@@ -7,56 +7,56 @@ const (
 	// KeyEscape is the "esc" key
 	KeyEscape KeyName = "Escape"
 	// KeyReturn is the carriage return (main keyboard)
-	KeyReturn = "Return"
+	KeyReturn KeyName = "Return"
 	// KeyTab is the tab advance key
-	KeyTab = "Tab"
+	KeyTab KeyName = "Tab"
 	// KeyBackspace is the delete-before-cursor key
-	KeyBackspace = "BackSpace"
+	KeyBackspace KeyName = "BackSpace"
 	// KeyInsert is the insert mode key
-	KeyInsert = "Insert"
+	KeyInsert KeyName = "Insert"
 	// KeyDelete is the delete-after-cursor key
-	KeyDelete = "Delete"
+	KeyDelete KeyName = "Delete"
 	// KeyRight is the right arrow key
-	KeyRight = "Right"
+	KeyRight KeyName = "Right"
 	// KeyLeft is the left arrow key
-	KeyLeft = "Left"
+	KeyLeft KeyName = "Left"
 	// KeyDown is the down arrow key
-	KeyDown = "Down"
+	KeyDown KeyName = "Down"
 	// KeyUp is the up arrow key
-	KeyUp = "Up"
+	KeyUp KeyName = "Up"
 	// KeyPageUp is the page up num-pad key
-	KeyPageUp = "Prior"
+	KeyPageUp KeyName = "Prior"
 	// KeyPageDown is the page down num-pad key
-	KeyPageDown = "Next"
+	KeyPageDown KeyName = "Next"
 	// KeyHome is the line-home key
-	KeyHome = "Home"
+	KeyHome KeyName = "Home"
 	// KeyEnd is the line-end key
-	KeyEnd = "End"
+	KeyEnd KeyName = "End"
 
 	// KeyF1 is the first function key
-	KeyF1 = "F1"
+	KeyF1 KeyName = "F1"
 	// KeyF2 is the second function key
-	KeyF2 = "F2"
+	KeyF2 KeyName = "F2"
 	// KeyF3 is the third function key
-	KeyF3 = "F3"
+	KeyF3 KeyName = "F3"
 	// KeyF4 is the fourth function key
-	KeyF4 = "F4"
+	KeyF4 KeyName = "F4"
 	// KeyF5 is the fifth function key
-	KeyF5 = "F5"
+	KeyF5 KeyName = "F5"
 	// KeyF6 is the sixth function key
-	KeyF6 = "F6"
+	KeyF6 KeyName = "F6"
 	// KeyF7 is the seventh function key
-	KeyF7 = "F7"
+	KeyF7 KeyName = "F7"
 	// KeyF8 is the eighth function key
-	KeyF8 = "F8"
+	KeyF8 KeyName = "F8"
 	// KeyF9 is the ninth function key
-	KeyF9 = "F9"
+	KeyF9 KeyName = "F9"
 	// KeyF10 is the tenth function key
-	KeyF10 = "F10"
+	KeyF10 KeyName = "F10"
 	// KeyF11 is the eleventh function key
-	KeyF11 = "F11"
+	KeyF11 KeyName = "F11"
 	// KeyF12 is the twelfth function key
-	KeyF12 = "F12"
+	KeyF12 KeyName = "F12"
 	/*
 		F13
 		...
@@ -64,5 +64,5 @@ const (
 	*/
 
 	// KeyEnter is the enter/ return key (keypad)
-	KeyEnter = "KP_Enter"
+	KeyEnter KeyName = "KP_Enter"
 )
