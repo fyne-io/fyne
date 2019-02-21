b83a67502ad76fac7767f8cca36b3481b9b859ee
Andy Williams <andy@andy.xyz>
2019-02-21 23:29:54 +0000

    Don't run tests requiring X11 on Travis

--- driver/gl/draw_test.go
+++ driver/gl/draw_test.go
@@ -5,7 +5,6 @@ import (
 
 	"fyne.io/fyne"
 	"fyne.io/fyne/canvas"
-	"fyne.io/fyne/theme"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -59,27 +58,4 @@ func TestInnerRect_Original(t *testing.T) {
 
 	assert.Equal(t, innerSize2, innerSize1)
 	assert.Equal(t, innerPos2, innerPos1)
-}
-
-func TestDrawImage_Ratio(t *testing.T) {
-	d := NewGLDriver()
-	win := d.CreateWindow("Test")
-	c := win.Canvas().(*glCanvas)
-
-	img := canvas.NewImageFromResource(theme.FyneLogo())
-	img.Resize(fyne.NewSize(10, 10))
-	c.newGlImageTexture(img)
-	assert.Equal(t, float32(1.0), c.aspects[img])
-}
-
-func TestDrawImage_Ratio2(t *testing.T) {
-	d := NewGLDriver()
-	win := d.CreateWindow("Test")
-	c := win.Canvas().(*glCanvas)
-
-	// make sure we haven't used the visual ratio
-	img := canvas.NewImageFromResource(theme.FyneLogo())
-	img.Resize(fyne.NewSize(20, 10))
-	c.newGlImageTexture(img)
-	assert.Equal(t, float32(1.0), c.aspects[img])
 }
