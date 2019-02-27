0570f9b0abd66c51bb3aac82b3a9a711ba0e8d82
Andy Williams <andy@andy.xyz>
2019-02-27 09:56:37 +0000

    Fix format, updated my git hooks

--- driver/gl/gl_test.go
+++ driver/gl/gl_test.go
@@ -3,8 +3,8 @@
 package gl
 
 import (
-	"testing"
 	"runtime"
+	"testing"
 
 	"fyne.io/fyne"
 	"fyne.io/fyne/canvas"
@@ -16,24 +16,24 @@ func init() {
 }
 
 func TestDrawImage_Ratio(t *testing.T) {
-//	d := NewGLDriver()
-//	win := d.CreateWindow("Test")
-//	c := win.Canvas().(*glCanvas)
+	//	d := NewGLDriver()
+	//	win := d.CreateWindow("Test")
+	//	c := win.Canvas().(*glCanvas)
 
 	img := canvas.NewImageFromResource(theme.FyneLogo())
 	img.Resize(fyne.NewSize(10, 10))
-//	c.newGlImageTexture(img)
-//	assert.Equal(t, float32(1.0), c.aspects[img])
+	//	c.newGlImageTexture(img)
+	//	assert.Equal(t, float32(1.0), c.aspects[img])
 }
 
 func TestDrawImage_Ratio2(t *testing.T) {
-//	d := NewGLDriver()
-//	win := d.CreateWindow("Test")
-//	c := win.Canvas().(*glCanvas)
+	//	d := NewGLDriver()
+	//	win := d.CreateWindow("Test")
+	//	c := win.Canvas().(*glCanvas)
 
 	// make sure we haven't used the visual ratio
 	img := canvas.NewImageFromResource(theme.FyneLogo())
 	img.Resize(fyne.NewSize(20, 10))
-//	c.newGlImageTexture(img)
-//	assert.Equal(t, float32(1.0), c.aspects[img])
+	//	c.newGlImageTexture(img)
+	//	assert.Equal(t, float32(1.0), c.aspects[img])
 }
