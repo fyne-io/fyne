3572f54126c1e9e02c52db5ae4667bba05038b80
Andy Williams <andy@andy.xyz>
2019-03-16 15:11:54 +0000

    Refactor and improve error test

--- log_test.go
+++ log_test.go
@@ -12,13 +12,12 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestLogError(t *testing.T) {
+func bufferLog(t *testing.T, reason string, err error) []string {
 	buf := bytes.NewBuffer([]byte{})
 	write := bufio.NewWriter(buf)
 
 	log.SetOutput(write)
-	err := errors.New("dummy error")
-	LogError("Testing errors", err)
+	LogError(reason, err)
 	log.SetOutput(os.Stdout)
 
 	err = write.Flush()
@@ -26,23 +25,24 @@ func TestLogError(t *testing.T) {
 		t.Error(err)
 	}
 	output := strings.TrimSpace(buf.String())
+	return strings.Split(output, "\n")
+}
 
-	assert.Equal(t, 3, len(strings.Split(output, "\n")))
+func TestLogError(t *testing.T) {
+	err := errors.New("dummy error")
+	output := bufferLog(t, "Testing errors", err)
+
+	assert.Equal(t, 3, len(output))
+	assert.True(t, strings.Contains(output[0], "Testing errors"))
+	assert.True(t, strings.Contains(output[1], "Cause"))
+	assert.True(t, strings.Contains(output[1], "dummy"))
+	assert.True(t, strings.Contains(output[2], "At"))
 }
 
 func TestLogErrorNoErr(t *testing.T) {
-	buf := bytes.NewBuffer([]byte{})
-	write := bufio.NewWriter(buf)
+	output := bufferLog(t, "Testing errors", nil)
 
-	log.SetOutput(write)
-	LogError("Testing errors", nil)
-	log.SetOutput(os.Stdout)
-
-	err := write.Flush()
-	if err != nil {
-		t.Error(err)
-	}
-	output := strings.TrimSpace(buf.String())
-
-	assert.Equal(t, 2, len(strings.Split(output, "\n")))
+	assert.Equal(t, 2, len(output))
+	assert.True(t, strings.Contains(output[0], "Testing errors"))
+	assert.True(t, strings.Contains(output[1], "At"))
 }
