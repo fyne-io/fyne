b7f4509f456e447e8735f8d46557c515eb967721
steveoc <steveoc64@gmail.com>
2019-03-28 00:36:50 +1000

    make line thread safe

--- canvas/line.go
+++ canvas/line.go
@@ -3,6 +3,7 @@ package canvas
 import (
 	"image/color"
 	"math"
+	"sync"
 
 	"fyne.io/fyne"
 )
@@ -11,6 +12,7 @@ import (
 // Lines are special as they can have a negative width or height to indicate
 // an inverse slope (i.e. slope up vs down).
 type Line struct {
+	sync.RWMutex
 	Position1 fyne.Position // The current top-left position of the Line
 	Position2 fyne.Position // The current bottomright position of the Line
 	Hidden    bool          // Is this Line currently hidden
@@ -21,25 +23,33 @@ type Line struct {
 
 // Size returns the current size of bounding box for this line object
 func (l *Line) Size() fyne.Size {
+	l.RLock()
+	defer l.RUnlock()
 	return fyne.NewSize(int(math.Abs(float64(l.Position2.X)-float64(l.Position1.X))),
 		int(math.Abs(float64(l.Position2.Y)-float64(l.Position1.Y))))
 }
 
 // Resize sets a new bottom-right position for the line object
 func (l *Line) Resize(size fyne.Size) {
+	l.Lock()
 	l.Position2 = fyne.NewPos(l.Position1.X+size.Width, l.Position1.Y+size.Height)
+	l.Unlock()
 }
 
 // Position gets the current top-left position of this line object, relative to it's parent / canvas
 func (l *Line) Position() fyne.Position {
+	l.RLock()
+	defer l.RUnlock()
 	return fyne.NewPos(fyne.Min(l.Position1.X, l.Position2.X), fyne.Min(l.Position1.Y, l.Position2.Y))
 }
 
 // Move the line object to a new position, relative to it's parent / canvas
 func (l *Line) Move(pos fyne.Position) {
+	l.Lock()
 	size := l.Size()
 	l.Position1 = pos
 	l.Position2 = fyne.NewPos(l.Position1.X+size.Width, l.Position1.Y+size.Height)
+	l.Unlock()
 }
 
 // MinSize for a Line simply returns Size{1, 1} as there is no
@@ -50,19 +60,25 @@ func (l *Line) MinSize() fyne.Size {
 
 // Visible returns true if this line// Show will set this circle to be visible is visible, false otherwise
 func (l *Line) Visible() bool {
+	l.RLock()
+	defer l.RUnlock()
 	return !l.Hidden
 }
 
 // Show will set this line to be visible
 func (l *Line) Show() {
+	l.Lock()
 	l.Hidden = false
+	l.Unlock()
 
 	Refresh(l)
 }
 
 // Hide will set this line to not be visible
 func (l *Line) Hide() {
+	l.Lock()
 	l.Hidden = true
+	l.Unlock()
 
 	Refresh(l)
 }
