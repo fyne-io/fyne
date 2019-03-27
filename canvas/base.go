f33c1cc177d11e2f86b79867c951f9238692a209
steveoc <steveoc64@gmail.com>
2019-03-27 23:07:25 +1000

    data races and locks all over the place

--- canvas/base.go
+++ canvas/base.go
@@ -1,9 +1,13 @@
 // Package canvas contains all of the primitive CanvasObjects that make up a Fyne GUI
 package canvas // import "fyne.io/fyne/canvas"
 
-import "fyne.io/fyne"
+import (
+	"fyne.io/fyne"
+	"sync"
+)
 
 type baseObject struct {
+	sync.RWMutex
 	size     fyne.Size     // The current size of the Rectangle
 	position fyne.Position // The current position of the Rectangle
 	Hidden   bool          // Is this object currently hidden
@@ -13,26 +17,36 @@ type baseObject struct {
 
 // CurrentSize returns the current size of this rectangle object
 func (r *baseObject) Size() fyne.Size {
+	r.RLock()
+	defer r.RUnlock()
 	return r.size
 }
 
 // Resize sets a new size for the rectangle object
 func (r *baseObject) Resize(size fyne.Size) {
+	r.Lock()
+	defer r.Unlock()
 	r.size = size
 }
 
 // CurrentPosition gets the current position of this rectangle object, relative to it's parent / canvas
 func (r *baseObject) Position() fyne.Position {
+	r.RLock()
+	defer r.RUnlock()
 	return r.position
 }
 
 // Move the rectangle object to a new position, relative to it's parent / canvas
 func (r *baseObject) Move(pos fyne.Position) {
+	r.Lock()
+	defer r.Unlock()
 	r.position = pos
 }
 
 // MinSize returns the specified minimum size, if set, or {1, 1} otherwise
 func (r *baseObject) MinSize() fyne.Size {
+	r.RLock()
+	defer r.RUnlock()
 	if r.min.Width == 0 && r.min.Height == 0 {
 		return fyne.NewSize(1, 1)
 	}
@@ -42,21 +56,29 @@ func (r *baseObject) MinSize() fyne.Size {
 
 // SetMinSize specifies the smallest size this object should be
 func (r *baseObject) SetMinSize(size fyne.Size) {
+	r.Lock()
+	defer r.Unlock()
 	r.min = size
 }
 
 // IsVisible returns true if this object is visible, false otherwise
 func (r *baseObject) Visible() bool {
+	r.RLock()
+	defer r.RUnlock()
 	return !r.Hidden
 }
 
 // Show will set this object to be visible
 func (r *baseObject) Show() {
+	r.Lock()
+	defer r.Unlock()
 	r.Hidden = false
 }
 
 // Hide will set this object to not be visible
 func (r *baseObject) Hide() {
+	r.Lock()
+	defer r.Unlock()
 	r.Hidden = true
 }
 
