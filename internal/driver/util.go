7d9b3e9a433e4d7858eaa81fe8b09dce0888ff52
Tilo Prütz <tilo@infopark.de>
2019-05-23 22:47:19 +0200

    adjust func names for driver.WalkObjectTree

--- internal/driver/util.go
+++ internal/driver/util.go
@@ -7,19 +7,19 @@ import (
 
 // WalkObjectTree will walk an object tree executing the passed functions following the following
 // rules:
-// - fBeforeChildren is called for the start obj before traversing its children
+// - beforeChildren is called for the start obj before traversing its children
 // - the obj's children are traversed by calling walkObjects on each of them
-// - fAfterChildren is called for the obj after traversing the obj's children
+// - afterChildren is called for the obj after traversing the obj's children
 // The walk can be aborted by returning true in one of the functions:
-// - if fBeforeChildren returns true, further traversing is stopped immediatly, the after function
+// - if beforeChildren returns true, further traversing is stopped immediatly, the after function
 //   will not be called for the obj where the walk stopped, however, it will be called for all its
 //   parents
 // - if a walk has been stopped, the after function is called with the third argument set to true
 func WalkObjectTree(
 	obj fyne.CanvasObject,
 	pos fyne.Position,
-	fBeforeChildren func(fyne.CanvasObject, fyne.Position) bool,
-	fAfterChildren func(fyne.CanvasObject, fyne.Position, bool),
+	beforeChildren func(fyne.CanvasObject, fyne.Position) bool,
+	afterChildren func(fyne.CanvasObject, fyne.Position, bool),
 ) bool {
 	var children []fyne.CanvasObject
 	var offset fyne.Position
@@ -34,22 +34,22 @@ func WalkObjectTree(
 		offset = pos
 	}
 
-	if fBeforeChildren != nil {
-		if fBeforeChildren(obj, offset) {
+	if beforeChildren != nil {
+		if beforeChildren(obj, offset) {
 			return true
 		}
 	}
 
 	cancelled := false
 	for _, child := range children {
-		if WalkObjectTree(child, offset, fBeforeChildren, fAfterChildren) {
+		if WalkObjectTree(child, offset, beforeChildren, afterChildren) {
 			cancelled = true
 			break
 		}
 	}
 
-	if fAfterChildren != nil {
-		fAfterChildren(obj, offset, cancelled)
+	if afterChildren != nil {
+		afterChildren(obj, offset, cancelled)
 	}
 	return cancelled
 }
