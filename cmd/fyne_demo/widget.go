86e6f02a73bdea170771343255300336fb1cc7c1
Andy Williams <andy@andy.xyz>
2019-05-25 18:01:10 +0100

    Add disabled widgets to fyne_demo

--- cmd/fyne_demo/widget.go
+++ cmd/fyne_demo/widget.go
@@ -11,10 +11,14 @@ import (
 )
 
 func makeButtonTab() fyne.Widget {
+	disabled := widget.NewButton("Disabled", func() {})
+	disabled.Disable()
+
 	return widget.NewVBox(
 		widget.NewLabel("Text label"),
 		widget.NewButton("Text button", func() { fmt.Println("tapped text button") }),
 		widget.NewButtonWithIcon("With icon", theme.ConfirmIcon(), func() { fmt.Println("tapped icon button") }),
+		disabled,
 	)
 }
 
@@ -22,14 +26,20 @@ func makeInputTab() fyne.Widget {
 	entry := widget.NewEntry()
 	entry.SetPlaceHolder("Entry")
 
+	disabledCheck := widget.NewCheck("Disabled check", func(bool) {})
+	disabledCheck.Disable()
 	radio := widget.NewRadio([]string{"Radio Item 1", "Radio Item 2"}, func(s string) { fmt.Println("selected", s) })
 	radio.Horizontal = true
+	disabledRadio := widget.NewRadio([]string{"Disabled radio"}, func(string) {})
+	disabledRadio.Disable()
 
 	return widget.NewVBox(
 		entry,
 		widget.NewSelect([]string{"Option 1", "Option 2", "Option 3"}, func(s string) { fmt.Println("selected", s) }),
 		widget.NewCheck("Check", func(on bool) { fmt.Println("checked", on) }),
+		disabledCheck,
 		radio,
+		disabledRadio,
 	)
 }
 
