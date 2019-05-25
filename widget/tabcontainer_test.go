90dd7f021ec4901ffc3e2d20f38d2c3289faffdf
Tilo Prütz <tilo@infopark.de>
2019-05-25 08:57:29 +0200

    make tab container tests use driver.WalkObjectTree for tree inspection
    This moves the test into the `widget_test` package, because `driver`
    depends on `widget`.
    This has the advantage that no internal interface is tested.
    
    The position computation for the layout test doesn't modify objects
    anymore.
    
    The `ApplyTheme` test is now named correctly (it tests the renderer)
    and no longer relies on the internal structure of the renderer.

--- widget/tabcontainer_test.go
+++ widget/tabcontainer_test.go
@@ -1,7 +1,10 @@
-package widget
+package widget_test
 
 import (
 	"testing"
+
+	"fyne.io/fyne/internal/driver"
+	"fyne.io/fyne/widget"
 
 	"fyne.io/fyne"
 	"fyne.io/fyne/canvas"
@@ -10,25 +13,25 @@ import (
 )
 
 func TestTabContainer_CurrentTabIndex(t *testing.T) {
-	tabs := NewTabContainer(&TabItem{Text: "Test", Content: NewLabel("Test")})
+	tabs := widget.NewTabContainer(widget.NewTabItem("Test", widget.NewLabel("Test")))
 
 	assert.Equal(t, 1, len(tabs.Items))
 	assert.Equal(t, 0, tabs.CurrentTabIndex())
 }
 
 func TestTabContainer_CurrentTab(t *testing.T) {
-	tab1 := &TabItem{Text: "Test1", Content: NewLabel("Test1")}
-	tab2 := &TabItem{Text: "Test2", Content: NewLabel("Test2")}
-	tabs := NewTabContainer(tab1, tab2)
+	tab1 := widget.NewTabItem("Test1", widget.NewLabel("Test1"))
+	tab2 := widget.NewTabItem("Test2", widget.NewLabel("Test2"))
+	tabs := widget.NewTabContainer(tab1, tab2)
 
 	assert.Equal(t, 2, len(tabs.Items))
 	assert.Equal(t, tab1, tabs.CurrentTab())
 }
 
 func TestTabContainer_SelectTab(t *testing.T) {
-	tab1 := &TabItem{Text: "Test1", Content: NewLabel("Test1")}
-	tab2 := &TabItem{Text: "Test2", Content: NewLabel("Test2")}
-	tabs := NewTabContainer(tab1, tab2)
+	tab1 := widget.NewTabItem("Test1", widget.NewLabel("Test1"))
+	tab2 := widget.NewTabItem("Test2", widget.NewLabel("Test2"))
+	tabs := widget.NewTabContainer(tab1, tab2)
 
 	assert.Equal(t, 2, len(tabs.Items))
 	assert.Equal(t, tab1, tabs.CurrentTab())
@@ -38,8 +41,8 @@ func TestTabContainer_SelectTab(t *testing.T) {
 }
 
 func TestTabContainer_SelectTabIndex(t *testing.T) {
-	tabs := NewTabContainer(&TabItem{Text: "Test1", Content: NewLabel("Test1")},
-		&TabItem{Text: "Test2", Content: NewLabel("Test2")})
+	tabs := widget.NewTabContainer(widget.NewTabItem("Test1", widget.NewLabel("Test1")),
+		widget.NewTabItem("Test2", widget.NewLabel("Test2")))
 
 	assert.Equal(t, 2, len(tabs.Items))
 	assert.Equal(t, 0, tabs.CurrentTabIndex())
@@ -48,69 +51,77 @@ func TestTabContainer_SelectTabIndex(t *testing.T) {
 	assert.Equal(t, 1, tabs.CurrentTabIndex())
 }
 
-func TestTabContainer_ApplyTheme(t *testing.T) {
-	tabs := NewTabContainer(&TabItem{Text: "Test1", Content: NewLabel("Test1")})
-	underline := Renderer(tabs).(*tabContainerRenderer).line
-	barColor := underline.FillColor
+func TestTabContainerRenderer_ApplyTheme(t *testing.T) {
+	tabs := widget.NewTabContainer(widget.NewTabItem("Test1", widget.NewLabel("Test1")))
+	var underline *canvas.Rectangle
+	driver.WalkObjectTree(tabs, fyne.NewPos(0, 0), func(o fyne.CanvasObject, offset fyne.Position) bool {
+		if u, ok := o.(*canvas.Rectangle); ok {
+			underline = u
+			return true
+		}
+		return false
+	}, nil)
+
+	r := widget.Renderer(tabs)
+
+	fyne.CurrentApp().Settings().SetTheme(theme.DarkTheme())
+	r.ApplyTheme()
+	darkColor := underline.FillColor
 
 	fyne.CurrentApp().Settings().SetTheme(theme.LightTheme())
-	Renderer(tabs).ApplyTheme()
-	assert.NotEqual(t, barColor, underline.FillColor)
+	r.ApplyTheme()
+	lightColor := underline.FillColor
+
+	assert.NotEqual(t, darkColor, lightColor)
 }
 
 func TestTabContainerRenderer_Layout(t *testing.T) {
-	tabs := NewTabContainer(
-		NewTabItemWithIcon("Text1", theme.CancelIcon(), canvas.NewCircle(theme.BackgroundColor())),
-		NewTabItemWithIcon("Text2", theme.ConfirmIcon(), canvas.NewCircle(theme.BackgroundColor())),
+	tabs := widget.NewTabContainer(
+		widget.NewTabItemWithIcon("Text1", theme.CancelIcon(), canvas.NewCircle(theme.BackgroundColor())),
+		widget.NewTabItemWithIcon("Text2", theme.ConfirmIcon(), canvas.NewCircle(theme.BackgroundColor())),
 	)
 
-	r := Renderer(tabs)
+	r := widget.Renderer(tabs)
 	r.Layout(fyne.NewSize(100, 100))
 
-	images, texts := collectImagesAndTexts(r.Objects(), fyne.NewPos(0, 0))
+	type dim struct {
+		fyne.Position
+		fyne.Size
+	}
+	images := []dim{}
+	texts := []dim{}
+	driver.WalkObjectTree(tabs, fyne.NewPos(0, 0), func(o fyne.CanvasObject, offset fyne.Position) bool {
+		switch o.(type) {
+		case *canvas.Image:
+			images = append(images, dim{Position: o.Position().Add(offset), Size: o.Size()})
+		case *canvas.Text:
+			texts = append(texts, dim{Position: o.Position().Add(offset), Size: o.Size()})
+		}
+		return false
+	}, nil)
 
 	if assert.Len(t, images, 2) && assert.Len(t, texts, 2) {
 		img1 := images[0]
 		img2 := images[1]
-		if img1.Position().X > img2.Position().X {
+		if img1.X > img2.X {
 			img2, img1 = img1, img2
 		}
 		text1 := texts[0]
 		text2 := texts[1]
-		if text1.Position().X > text2.Position().X {
+		if text1.X > text2.X {
 			text2, text1 = text1, text2
 		}
 
-		text1Left := text1.Position().X
-		img1Right := img1.Position().X + img1.Size().Width
+		text1Left := text1.X
+		img1Right := img1.X + img1.Width
 		assert.Equal(t, img1Right, text1Left)
 
-		text1Right := text1Left + text1.Size().Width
-		img2Left := img2.Position().X
+		text1Right := text1Left + text1.Width
+		img2Left := img2.X
 		assert.Equal(t, 20, img2Left-text1Right)
 
-		img2Right := img2Left + img2.Size().Width
-		text2Left := text2.Position().X
+		img2Right := img2Left + img2.Width
+		text2Left := text2.X
 		assert.Equal(t, img2Right, text2Left)
 	}
-}
-
-func collectImagesAndTexts(objects []fyne.CanvasObject, offset fyne.Position) ([]*canvas.Image, []*canvas.Text) {
-	images := []*canvas.Image{}
-	texts := []*canvas.Text{}
-	for _, o := range objects {
-		switch o.(type) {
-		case fyne.Widget:
-			i, l := collectImagesAndTexts(Renderer(o.(fyne.Widget)).Objects(), o.Position())
-			images = append(images, i...)
-			texts = append(texts, l...)
-		case *canvas.Image:
-			o.Move(o.Position().Add(offset))
-			images = append(images, o.(*canvas.Image))
-		case *canvas.Text:
-			o.Move(o.Position().Add(offset))
-			texts = append(texts, o.(*canvas.Text))
-		}
-	}
-	return images, texts
 }
