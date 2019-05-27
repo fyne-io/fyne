28f641008ac021c5c035404db596b25e9c7050fe
Charles Corbett <nafredy@gmail.com>
2019-05-27 14:43:34 -0700

    was duplicating d mult

--- canvas/gradient.go
+++ canvas/gradient.go
@@ -1,9 +1,11 @@
 package canvas
 
 import (
+	"fmt"
 	"image"
 	"image/color"
 	"image/draw"
+	"os"
 )
 
 // Gradient describes a gradient between two colors
@@ -31,23 +33,29 @@ type GradientColor struct {
 
 func (gc *GradientColor) linearGradient(w, h, x, y int) *color.RGBA {
 	//d := float64(x) / float64(w) // horizontal
-	d := float64(x) / float64(w)
-	//d := float64(y) / float64(h) // top down
+	//d := float64(w) / float64(x)
+	d := float64(y) / float64(h) // top down
+	if y == 100 {
+		os.Exit(0)
+	}
 
 	aR, aG, aB, aA := gc.Start.RGBA()
 	bR, bG, bB, bA := gc.End.RGBA()
 
-	dR := d * (float64(bR) - float64(aR))
-	dG := d * (float64(bG) - float64(aG))
-	dB := d * (float64(bB) - float64(aB))
-	dA := d * (float64(bA) - float64(aA))
+	dR := (float64(bR) - float64(aR))
+	dG := (float64(bG) - float64(aG))
+	dB := (float64(bB) - float64(aB))
+	dA := (float64(bA) - float64(aA))
 
 	//fmt.Printf("[%d][%d] || dR: %f | dG: %f | dB: %f | dA: %f\n", x, y, dR, dG, dB, dA)
 
+	fmt.Printf("(%d)[%d] / [%d] = %f\n", x, y, h, d)
+	fmt.Printf("%d + (%f * %f) =  %d\n\n", aA, d, dA, uint8(float64(aA)+d*dA))
+
 	pixel := &color.RGBA{
 		R: uint8(float64(aR) + d*dR),
-		G: uint8(float64(aG) + d*dG),
 		B: uint8(float64(aB) + d*dB),
+		G: uint8(float64(aG) + d*dG),
 		A: uint8(float64(aA) + d*dA),
 	}
 
@@ -76,6 +84,8 @@ func NewRectangleGradient(start color.Color, end color.Color) *Gradient {
 				rect := image.Rect(0, 0, w, h)
 				pix.img = image.NewRGBA(rect)
 			}
+
+			fmt.Printf("W: %d | H: %d\n", w, h)
 
 			for x := 0; x < w; x++ {
 				for y := 0; y < h; y++ {
