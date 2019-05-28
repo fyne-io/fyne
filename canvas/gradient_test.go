f10be12b389bb591f3e7da7104d3949087149646
Charles Corbett <nafredy@gmail.com>
2019-05-27 23:39:09 -0700

    test coverage for gradients

--- canvas/gradient_test.go
+++ canvas/gradient_test.go
@@ -1,18 +1,157 @@
 package canvas
 
-/*
-func TestNewRectangleLinearGradient(t *testing.T) {
-	grad := NewRectangleLinearGradient(color.Black, color.Transparent)
-	img := grad.Generator(5, 5)
+import (
+	"image/color"
+	"reflect"
+	"testing"
 
-	_, _, _, a := img.At(0, 0).RGBA()
-	assert.Equal(t, a, 65535)
+	"github.com/stretchr/testify/assert"
+)
 
-	for x := 0; x < 5; x++ {
-		for y := 0; y < 5; y++ {
-			t.Error(img.At(x, y).RGBA())
-		}
+// This is a terribly designed test but I don't know
+// a better way to determine the idempotency
+// of the image generation
+
+func TestNewRectangleGradient(t *testing.T) {
+	var test = []struct {
+		Name       string
+		StartColor color.Color
+		EndColor   color.Color
+		Alignment  GradientDirection
+		Result     [][]color.RGBA64
+	}{
+		{"Black to Transparent Horizontal", color.Black, color.Transparent, HORIZONTAL, [][]color.RGBA64{
+			{
+				{R: 0, G: 0, B: 0, A: 65535},
+				{R: 0, G: 0, B: 0, A: 65535},
+				{R: 0, G: 0, B: 0, A: 65535},
+				{R: 0, G: 0, B: 0, A: 65535},
+				{R: 0, G: 0, B: 0, A: 65535},
+			},
+			{
+				{R: 0, G: 0, B: 0, A: 52428},
+				{R: 0, G: 0, B: 0, A: 52428},
+				{R: 0, G: 0, B: 0, A: 52428},
+				{R: 0, G: 0, B: 0, A: 52428},
+				{R: 0, G: 0, B: 0, A: 52428},
+			},
+			{
+				{R: 0, G: 0, B: 0, A: 39321},
+				{R: 0, G: 0, B: 0, A: 39321},
+				{R: 0, G: 0, B: 0, A: 39321},
+				{R: 0, G: 0, B: 0, A: 39321},
+				{R: 0, G: 0, B: 0, A: 39321},
+			},
+			{
+				{R: 0, G: 0, B: 0, A: 26214},
+				{R: 0, G: 0, B: 0, A: 26214},
+				{R: 0, G: 0, B: 0, A: 26214},
+				{R: 0, G: 0, B: 0, A: 26214},
+				{R: 0, G: 0, B: 0, A: 26214},
+			},
+			{
+				{R: 0, G: 0, B: 0, A: 13107},
+				{R: 0, G: 0, B: 0, A: 13107},
+				{R: 0, G: 0, B: 0, A: 13107},
+				{R: 0, G: 0, B: 0, A: 13107},
+				{R: 0, G: 0, B: 0, A: 13107},
+			}},
+		},
+		{"Black to Transparent Vertical", color.Black, color.Transparent, VERTICAL, [][]color.RGBA64{
+			{
+				{R: 0, G: 0, B: 0, A: 65535},
+				{R: 0, G: 0, B: 0, A: 52428},
+				{R: 0, G: 0, B: 0, A: 39321},
+				{R: 0, G: 0, B: 0, A: 26214},
+				{R: 0, G: 0, B: 0, A: 13107},
+			},
+			{
+				{R: 0, G: 0, B: 0, A: 65535},
+				{R: 0, G: 0, B: 0, A: 52428},
+				{R: 0, G: 0, B: 0, A: 39321},
+				{R: 0, G: 0, B: 0, A: 26214},
+				{R: 0, G: 0, B: 0, A: 13107},
+			},
+			{
+				{R: 0, G: 0, B: 0, A: 65535},
+				{R: 0, G: 0, B: 0, A: 52428},
+				{R: 0, G: 0, B: 0, A: 39321},
+				{R: 0, G: 0, B: 0, A: 26214},
+				{R: 0, G: 0, B: 0, A: 13107},
+			},
+			{
+				{R: 0, G: 0, B: 0, A: 65535},
+				{R: 0, G: 0, B: 0, A: 52428},
+				{R: 0, G: 0, B: 0, A: 39321},
+				{R: 0, G: 0, B: 0, A: 26214},
+				{R: 0, G: 0, B: 0, A: 13107},
+			},
+			{
+				{R: 0, G: 0, B: 0, A: 65535},
+				{R: 0, G: 0, B: 0, A: 52428},
+				{R: 0, G: 0, B: 0, A: 39321},
+				{R: 0, G: 0, B: 0, A: 26214},
+				{R: 0, G: 0, B: 0, A: 13107},
+			}},
+		},
+		{"Black to Transparent Circular", color.Black, color.Transparent, CIRCULAR, [][]color.RGBA64{
+			{
+				{R: 0, G: 0, B: 0, A: 65021},
+				{R: 0, G: 0, B: 0, A: 65021},
+				{R: 0, G: 0, B: 0, A: 65021},
+				{R: 0, G: 0, B: 0, A: 65021},
+				{R: 0, G: 0, B: 0, A: 65021},
+			},
+			{
+				{R: 0, G: 0, B: 0, A: 65021},
+				{R: 0, G: 0, B: 0, A: 65278},
+				{R: 0, G: 0, B: 0, A: 65278},
+				{R: 0, G: 0, B: 0, A: 65278},
+				{R: 0, G: 0, B: 0, A: 65021},
+			},
+			{
+				{R: 0, G: 0, B: 0, A: 65021},
+				{R: 0, G: 0, B: 0, A: 65278},
+				{R: 0, G: 0, B: 0, A: 65535},
+				{R: 0, G: 0, B: 0, A: 65278},
+				{R: 0, G: 0, B: 0, A: 65021},
+			},
+			{
+				{R: 0, G: 0, B: 0, A: 65021},
+				{R: 0, G: 0, B: 0, A: 65278},
+				{R: 0, G: 0, B: 0, A: 65278},
+				{R: 0, G: 0, B: 0, A: 65278},
+				{R: 0, G: 0, B: 0, A: 65021},
+			},
+			{
+				{R: 0, G: 0, B: 0, A: 65021},
+				{R: 0, G: 0, B: 0, A: 65021},
+				{R: 0, G: 0, B: 0, A: 65021},
+				{R: 0, G: 0, B: 0, A: 65021},
+				{R: 0, G: 0, B: 0, A: 65021},
+			}},
+		},
+	}
+
+	for _, tt := range test {
+		t.Run(tt.Name, func(t *testing.T) {
+			grad := NewRectangleGradient(
+				GradientStartColor(tt.StartColor),
+				GradientEndColor(tt.EndColor),
+				GradientAlignment(tt.Alignment),
+			)
+			img := grad.Generator(5, 5)
+
+			for x := 0; x < 5; x++ {
+				for y := 0; y < 5; y++ {
+					//t.Error(img.At(x, y).RGBA())
+					//t.Error(tt.Result)
+					r, g, b, a := img.At(x, y).RGBA()
+					built := color.RGBA64{R: uint16(r), G: uint16(g), B: uint16(b), A: uint16(a)}
+					assert.True(t, reflect.DeepEqual(built, tt.Result[x][y]))
+				}
+			}
+		})
 	}
 
 }
-*/
