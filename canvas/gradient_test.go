e41ed499cd1f7599aa8264e4b0168a9927feee86
Andy Williams <andy@andy.xyz>
2019-06-27 17:30:36 +0100

    Fix test assert argument order

--- canvas/gradient_test.go
+++ canvas/gradient_test.go
@@ -11,47 +11,47 @@ func TestNewHorizontalGradient(t *testing.T) {
 	horizontal := NewHorizontalGradient(color.Black, color.Transparent)
 
 	img := horizontal.Generate(50, 5)
-	assert.Equal(t, img.At(0, 0), color.RGBA{0, 0, 0, 0xff})
+	assert.Equal(t, color.RGBA{0, 0, 0, 0xff}, img.At(0, 0))
 	for i := 0; i < 5; i++ {
-		assert.Equal(t, img.At(25, i), color.RGBA{0, 0, 0, 0x7f})
+		assert.Equal(t, color.RGBA{0, 0, 0, 0x7f}, img.At(25, i))
 	}
-	assert.Equal(t, img.At(50, 0), color.RGBA{0, 0, 0, 0x00})
+	assert.Equal(t, color.RGBA{0, 0, 0, 0x00}, img.At(50, 0))
 }
 
 func TestNewVerticalGradient(t *testing.T) {
 	vertical := NewVerticalGradient(color.Black, color.Transparent)
 	imgVert := vertical.Generate(5, 50)
-	assert.Equal(t, imgVert.At(0, 0), color.RGBA{0, 0, 0, 0xff})
+	assert.Equal(t, color.RGBA{0, 0, 0, 0xff}, imgVert.At(0, 0))
 	for i := 0; i < 5; i++ {
-		assert.Equal(t, imgVert.At(i, 25), color.RGBA{0, 0, 0, 0x7f})
+		assert.Equal(t, color.RGBA{0, 0, 0, 0x7f}, imgVert.At(i, 25))
 	}
-	assert.Equal(t, imgVert.At(50, 0), color.RGBA{0, 0, 0, 0x00})
+	assert.Equal(t, color.RGBA{0, 0, 0, 0x00}, imgVert.At(50, 0))
 }
 
 func TestNewRadialGradient(t *testing.T) {
 	circle := NewRadialGradient(color.Black, color.Transparent)
 	imgCircle := circle.Generate(10, 10)
-	assert.Equal(t, imgCircle.At(5, 5), color.RGBA{0, 0, 0, 0xff})
-	assert.Equal(t, imgCircle.At(4, 5), color.RGBA{0, 0, 0, 0xcc})
-	assert.Equal(t, imgCircle.At(3, 5), color.RGBA{0, 0, 0, 0x99})
-	assert.Equal(t, imgCircle.At(2, 5), color.RGBA{0, 0, 0, 0x66})
-	assert.Equal(t, imgCircle.At(1, 5), color.RGBA{0, 0, 0, 0x33})
+	assert.Equal(t, color.RGBA{0, 0, 0, 0xff}, imgCircle.At(5, 5))
+	assert.Equal(t, color.RGBA{0, 0, 0, 0xcc}, imgCircle.At(4, 5))
+	assert.Equal(t, color.RGBA{0, 0, 0, 0x99}, imgCircle.At(3, 5))
+	assert.Equal(t, color.RGBA{0, 0, 0, 0x66}, imgCircle.At(2, 5))
+	assert.Equal(t, color.RGBA{0, 0, 0, 0x33}, imgCircle.At(1, 5))
 
 	circle.CenterOffsetX = 0.1
 	circle.CenterOffsetY = 0.1
 	imgCircleOffset := circle.Generate(10, 10)
-	assert.Equal(t, imgCircleOffset.At(5, 5), color.RGBA{0, 0, 0, 0xc3})
-	assert.Equal(t, imgCircleOffset.At(4, 5), color.RGBA{0, 0, 0, 0xa0})
-	assert.Equal(t, imgCircleOffset.At(3, 5), color.RGBA{0, 0, 0, 0x79})
-	assert.Equal(t, imgCircleOffset.At(2, 5), color.RGBA{0, 0, 0, 0x50})
-	assert.Equal(t, imgCircleOffset.At(1, 5), color.RGBA{0, 0, 0, 0x26})
+	assert.Equal(t, color.RGBA{0, 0, 0, 0xc3}, imgCircleOffset.At(5, 5))
+	assert.Equal(t, color.RGBA{0, 0, 0, 0xa0}, imgCircleOffset.At(4, 5))
+	assert.Equal(t, color.RGBA{0, 0, 0, 0x79}, imgCircleOffset.At(3, 5))
+	assert.Equal(t, color.RGBA{0, 0, 0, 0x50}, imgCircleOffset.At(2, 5))
+	assert.Equal(t, color.RGBA{0, 0, 0, 0x26}, imgCircleOffset.At(1, 5))
 
 	circle.CenterOffsetX = -0.1
 	circle.CenterOffsetY = -0.1
 	imgCircleOffset = circle.Generate(10, 10)
-	assert.Equal(t, imgCircleOffset.At(5, 5), color.RGBA{0, 0, 0, 0xc3})
-	assert.Equal(t, imgCircleOffset.At(4, 5), color.RGBA{0, 0, 0, 0xd5})
-	assert.Equal(t, imgCircleOffset.At(3, 5), color.RGBA{0, 0, 0, 0xc3})
-	assert.Equal(t, imgCircleOffset.At(2, 5), color.RGBA{0, 0, 0, 0xa0})
-	assert.Equal(t, imgCircleOffset.At(1, 5), color.RGBA{0, 0, 0, 0x79})
+	assert.Equal(t, color.RGBA{0, 0, 0, 0xc3}, imgCircleOffset.At(5, 5))
+	assert.Equal(t, color.RGBA{0, 0, 0, 0xd5}, imgCircleOffset.At(4, 5))
+	assert.Equal(t, color.RGBA{0, 0, 0, 0xc3}, imgCircleOffset.At(3, 5))
+	assert.Equal(t, color.RGBA{0, 0, 0, 0xa0}, imgCircleOffset.At(2, 5))
+	assert.Equal(t, color.RGBA{0, 0, 0, 0x79}, imgCircleOffset.At(1, 5))
 }
