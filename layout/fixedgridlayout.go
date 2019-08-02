9d53446c57ea076cc1085b0de27b604f1aa3ba71
Stephen M Houston <smhouston88@gmail.com>
2019-08-02 15:46:51 -0500

    Fix Fixed Grid's Min Size Calculations To Be Accurate. (#379)
    This minimum size is the cell width and the cell height times rows required plus padding

--- layout/fixedgridlayout.go
+++ layout/fixedgridlayout.go
@@ -12,15 +12,19 @@ var _ fyne.Layout = (*fixedGridLayout)(nil)
 
 type fixedGridLayout struct {
 	CellSize fyne.Size
+	colCount int
+	rowCount int
 }
 
 // Layout is called to pack all child objects into a specified size.
 // For a FixedGridLayout this will attempt to lay all the child objects in a row
 // and wrap to a new row if the size is not large enough.
 func (g *fixedGridLayout) Layout(objects []fyne.CanvasObject, size fyne.Size) {
-	cols := 1
+	g.colCount = 1
+	g.rowCount = 1
+
 	if size.Width > g.CellSize.Width {
-		cols = int(math.Floor(float64(size.Width+theme.Padding()) / float64(g.CellSize.Width+theme.Padding())))
+		g.colCount = int(math.Floor(float64(size.Width) / float64(g.CellSize.Width+theme.Padding())))
 	}
 
 	x, y := 0, 0
@@ -28,9 +32,10 @@ func (g *fixedGridLayout) Layout(objects []fyne.CanvasObject, size fyne.Size) {
 		child.Move(fyne.NewPos(x, y))
 		child.Resize(g.CellSize)
 
-		if (i+1)%cols == 0 {
+		if (i+1)%g.colCount == 0 {
 			x = 0
 			y += g.CellSize.Height + theme.Padding()
+			g.rowCount++
 		} else {
 			x += g.CellSize.Width + theme.Padding()
 		}
@@ -39,13 +44,14 @@ func (g *fixedGridLayout) Layout(objects []fyne.CanvasObject, size fyne.Size) {
 
 // MinSize finds the smallest size that satisfies all the child objects.
 // For a FixedGridLayout this is simply the specified cellsize as a single column
-// layout has no padding. The returned sie does not take into account the number
-// of child objects as this layout re-flows dynamically.
+// layout has no padding. The returned size does not take into account the number
+// of columns as this layout re-flows dynamically.
 func (g *fixedGridLayout) MinSize(objects []fyne.CanvasObject) fyne.Size {
-	return g.CellSize
+	return fyne.NewSize((g.CellSize.Width),
+		((g.CellSize.Height * g.rowCount) + ((g.rowCount - 1) * theme.Padding())))
 }
 
 // NewFixedGridLayout returns a new FixedGridLayout instance
 func NewFixedGridLayout(size fyne.Size) fyne.Layout {
-	return &fixedGridLayout{size}
+	return &fixedGridLayout{size, 1, 1}
 }
