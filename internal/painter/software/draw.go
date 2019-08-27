42ba7959ff0366b44c39acf6076f7b6296ca470a
Andy Williams <andy@andy.xyz>
2019-08-27 13:29:29 +0100

    Actually scale the contents of our software renderer

--- internal/painter/software/draw.go
+++ internal/painter/software/draw.go
@@ -6,9 +6,11 @@ import (
 
 	"fyne.io/fyne"
 	"fyne.io/fyne/canvas"
+	"fyne.io/fyne/internal"
 	"fyne.io/fyne/internal/painter"
 	"fyne.io/fyne/theme"
 	"fyne.io/fyne/widget"
+
 	"github.com/goki/freetype"
 	"github.com/goki/freetype/truetype"
 	"golang.org/x/image/font"
@@ -16,18 +18,19 @@ import (
 
 func drawImage(c fyne.Canvas, img *canvas.Image, pos fyne.Position, frame fyne.Size, base *image.RGBA) {
 	bounds := img.Size()
-	width := bounds.Width   //textureScaleInt(c, bounds.Width)
-	height := bounds.Height //textureScaleInt(c, bounds.Height)
+	width := internal.ScaleInt(c, bounds.Width)
+	height := internal.ScaleInt(c, bounds.Height)
 	tex := painter.PaintImage(img, c, width, height)
 
-	outBounds := image.Rect(pos.X, pos.Y, bounds.Width+pos.X, bounds.Height+pos.Y)
+	scaledX, scaledY := internal.ScaleInt(c, pos.X), internal.ScaleInt(c, pos.Y)
+	outBounds := image.Rect(scaledX, scaledY, scaledX+width, scaledY+height)
 	draw.Draw(base, outBounds, tex, image.ZP, draw.Over)
 }
 
 func drawText(c fyne.Canvas, text *canvas.Text, pos fyne.Position, frame fyne.Size, base *image.RGBA) {
 	bounds := text.MinSize()
-	width := bounds.Width   //textureScaleInt(c, bounds.Width)
-	height := bounds.Height //textureScaleInt(c, bounds.Height)
+	width := internal.ScaleInt(c, bounds.Width)
+	height := internal.ScaleInt(c, bounds.Height)
 	txtImg := image.NewRGBA(image.Rect(0, 0, width, height))
 
 	var opts truetype.Options
@@ -44,16 +47,23 @@ func drawText(c fyne.Canvas, text *canvas.Text, pos fyne.Position, frame fyne.Si
 	d.Dot = freetype.Pt(0, height-face.Metrics().Descent.Ceil())
 	d.DrawString(text.Text)
 
-	imgBounds := image.Rect(pos.X, pos.Y, text.Size().Width+pos.X, text.Size().Height+pos.Y)
+	scaledX, scaledY := internal.ScaleInt(c, pos.X), internal.ScaleInt(c, pos.Y)
+	imgBounds := image.Rect(scaledX, scaledY, scaledX+width, scaledY+height)
 	draw.Draw(base, imgBounds, txtImg, image.ZP, draw.Over)
 }
 
 func drawRectangle(c fyne.Canvas, rect *canvas.Rectangle, pos fyne.Position, frame fyne.Size, base *image.RGBA) {
-	bounds := image.Rect(pos.X, pos.Y, rect.Size().Width+pos.X, rect.Size().Height+pos.Y)
+	scaledWidth := internal.ScaleInt(c, rect.Size().Width)
+	scaledHeight := internal.ScaleInt(c, rect.Size().Height)
+	scaledX, scaledY := internal.ScaleInt(c, pos.X), internal.ScaleInt(c, pos.Y)
+	bounds := image.Rect(scaledX, scaledY, scaledX+scaledWidth, scaledY+scaledHeight)
 	draw.Draw(base, bounds, image.NewUniform(rect.FillColor), image.ZP, draw.Over)
 }
 
 func drawWidget(c fyne.Canvas, wid fyne.Widget, pos fyne.Position, frame fyne.Size, base *image.RGBA) {
-	bounds := image.Rect(pos.X, pos.Y, wid.Size().Width+pos.X, wid.Size().Height+pos.Y)
+	scaledWidth := internal.ScaleInt(c, wid.Size().Width)
+	scaledHeight := internal.ScaleInt(c, wid.Size().Height)
+	scaledX, scaledY := internal.ScaleInt(c, pos.X), internal.ScaleInt(c, pos.Y)
+	bounds := image.Rect(scaledX, scaledY, scaledX+scaledWidth, scaledY+scaledHeight)
 	draw.Draw(base, bounds, image.NewUniform(widget.Renderer(wid).BackgroundColor()), image.ZP, draw.Over)
 }
