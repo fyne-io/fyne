57a0e9b5a14308e9d3cd8f66834f70d9c742242c
tayadelmar <dubinina.taisiia@gmail.com>
2019-09-02 17:36:27 +0200

    widget/entry.go: hide popUp on entry.Hide()  - destroy popUp on entyRenderer.Destroy()  - removed re-focus the entry, because the implementation moved to popUp menu

--- widget/entry.go
+++ widget/entry.go
@@ -217,6 +217,12 @@ func (e *entryRenderer) Objects() []fyne.CanvasObject {
 }
 
 func (e *entryRenderer) Destroy() {
+	if e.entry.popUp != nil {
+		c := fyne.CurrentApp().Driver().CanvasForObject(e.entry)
+		c.SetOverlay(nil)
+		Renderer(e.entry.popUp).Destroy()
+		e.entry.popUp = nil
+	}
 }
 
 // Declare conformity with interfaces
@@ -252,6 +258,7 @@ type Entry struct {
 
 	// selecting indicates whether the cursor has moved since it was at the selection start location
 	selecting bool
+	popUp     *PopUp
 	// TODO: Add OnSelectChanged
 }
 
@@ -287,6 +294,9 @@ func (e *Entry) Show() {
 func (e *Entry) Hide() {
 	if e.focused {
 		fyne.CurrentApp().Driver().CanvasForObject(e).Focus(nil)
+	}
+	if e.popUp != nil {
+		e.popUp.Hide()
 	}
 	e.hide(e)
 }
@@ -489,18 +499,15 @@ func (e *Entry) TappedSecondary(pe *fyne.PointEvent) {
 	c := fyne.CurrentApp().Driver().CanvasForObject(e)
 
 	item := fyne.NewMenuItem("Paste", func() {
-		// re-focus the entry
-		fyne.CurrentApp().Driver().CanvasForObject(e).Focus(e)
-
 		clipboard := fyne.CurrentApp().Driver().AllWindows()[0].Clipboard()
 		e.pasteFromClipboard(clipboard)
 	})
-	popUp := NewPopUpMenu(fyne.NewMenu("", item), c)
+	e.popUp = NewPopUpMenu(fyne.NewMenu("", item), c)
 
 	entryPos := fyne.CurrentApp().Driver().AbsolutePositionForObject(e)
 	popUpPos := entryPos.Add(fyne.NewPos(pe.Position.X, e.Size().Height))
 
-	popUp.Move(popUpPos)
+	e.popUp.Move(popUpPos)
 }
 
 // MouseDown called on mouse click, this triggers a mouse click which can move the cursor,
