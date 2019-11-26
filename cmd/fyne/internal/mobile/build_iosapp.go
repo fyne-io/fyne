b5e5da0b5038ec0127bc2f6ee66ff604a97e5efc
Andy Williams <andy@andy.xyz>
2019-11-26 17:10:34 +0000

    Don't manipulate the appID, we are passing it on the command line

--- cmd/fyne/internal/mobile/build_iosapp.go
+++ cmd/fyne/internal/mobile/build_iosapp.go
@@ -32,8 +32,7 @@ func goIOSBuild(pkg *build.Package, bundleID string, archs []string) (map[string
 
 	infoplist := new(bytes.Buffer)
 	if err := infoplistTmpl.Execute(infoplist, infoplistTmplData{
-		// TODO: better bundle id.
-		BundleID: bundleID + "." + productName,
+		BundleID: bundleID,
 		Name:     strings.Title(path.Base(pkg.ImportPath)),
 	}); err != nil {
 		return nil, err
