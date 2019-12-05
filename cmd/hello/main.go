3454d5f7b4216c508c043fb5e7ec1899422ed4f1
lucor <lu.corbo@gmail.com>
2019-12-05 23:32:31 +0100

    Fix typo

--- cmd/hello/main.go
+++ cmd/hello/main.go
@@ -11,7 +11,7 @@ func main() {
 
 	w := a.NewWindow("Hello")
 	w.SetContent(widget.NewVBox(
-		widget.NewPasswordRevealEntry(),
+		widget.NewLabel("Hello Fyne!"),
 		widget.NewButton("Quit", func() {
 			a.Quit()
 		}),
