39cc33f9b2b88ef48f1f75b6295df4022b712718
CC <cerisara@loria.fr>
2020-02-14 18:22:52 +0100

    test 0 is better

--- internal/driver/gomobile/driver.go
+++ internal/driver/gomobile/driver.go
@@ -140,7 +140,7 @@ func (d *mobileDriver) Run() {
 			case paint.Event:
                                 // this is a fix to prevent the window to appear as a "blank screen" on android
                                 // on some android phones, we may get a 0x0 size
-                                if currentSize.WidthPx<10 {
+                                if currentSize.WidthPx==0 {
                                     continue
                                 }
 				if d.glctx == nil || e.External {
