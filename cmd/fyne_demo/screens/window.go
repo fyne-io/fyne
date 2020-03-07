10b175c0193af3b1615f938a6d80ce9d372352a7
Tilo Prütz <tilo@pruetz.net>
2020-03-07 23:05:20 +0100

    SelectEntry in example app

--- cmd/fyne_demo/screens/window.go
+++ cmd/fyne_demo/screens/window.go
@@ -68,7 +68,9 @@ func DialogScreen(win fyne.Window) fyne.CanvasObject {
 			sel := widget.NewSelect([]string{"Option A", "Option B", "Option C"}, func(o string) {
 				fmt.Println("Selected", o)
 			})
-			content := widget.NewVBox(entry, sel)
+			selEntry := widget.NewSelectEntry([]string{"Option A", "Option B", "Option C"})
+			selEntry.PlaceHolder = "Type or select"
+			content := widget.NewVBox(entry, sel, selEntry)
 			dialog.ShowCustom("Custom dialog", "Done", content, win)
 		}),
 	)
