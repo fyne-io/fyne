718619bd2e4e96ee408eac26351c3c6a4479ea21
Andy Williams <andy@andy.xyz>
2020-03-09 21:30:09 +0000

    Oops, this should have applied before push

--- dialog/file_windows_test.go
+++ dialog/file_windows_test.go
@@ -1,13 +1,18 @@
 package dialog
 
-import "github.com/stretchr/testify/assert"
+import (
+	"testing"
+
+	"fyne.io/fyne/widget"
+	"github.com/stretchr/testify/assert"
+)
 
 func TestListDrives(t *testing.T) {
 	driveLetters := listDrives()
 	assert.GreaterOrEqual(t, len(driveLetters), 1)
 
 	if driveLetters[0] != "C:" && driveLetters[0] != "A:" {
-		t.Error("Unexpected lowest drive letter, "+driveLetters[0])
+		t.Error("Unexpected lowest drive letter, " + driveLetters[0])
 	}
 }
 
