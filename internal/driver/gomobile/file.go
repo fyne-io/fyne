7afb4606aca9a25203250dce6211e9e0e2920ba3
Andy Williams <andy@andy.xyz>
2020-03-13 20:34:28 +0000

    Missed doc line

--- internal/driver/gomobile/file.go
+++ internal/driver/gomobile/file.go
@@ -6,6 +6,7 @@ type hasPicker interface {
 	ShowFileOpenPicker(callback func(string))
 }
 
+// ShowFileOpenPicker loads the native file open dialog and returns the chosen file path via the callback func.
 func ShowFileOpenPicker(callback func(string)) {
 	if a, ok := fyne.CurrentApp().Driver().(*mobileDriver).app.(hasPicker); ok {
 		a.ShowFileOpenPicker(callback)
