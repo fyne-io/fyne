838f7083e3b2ced325914fb12fa3dbe74b69c1ea
Tilo Prütz <tilo@pruetz.net>
2020-04-05 08:52:09 +0200

    fix shadowRenderer's handling of objects

---
+++ widget/shadowing_renderer_test.go
@@ -0,0 +1,36 @@
+package widget
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"fyne.io/fyne"
+)
+
+func TestShadowingRenderer_Objects(t *testing.T) {
+	tests := map[string]struct {
+		level                elevationLevel
+		wantPrependedObjects []fyne.CanvasObject
+	}{
+		"with shadow": {
+			12,
+			[]fyne.CanvasObject{newShadow(shadowAround, 12)},
+		},
+		"without shadow": {
+			0,
+			[]fyne.CanvasObject{},
+		},
+	}
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			objects := []fyne.CanvasObject{NewLabel("A"), NewLabel("B")}
+			r := newShadowingRenderer(objects, tt.level)
+			assert.Equal(t, append(tt.wantPrependedObjects, objects...), r.Objects())
+
+			otherObjects := []fyne.CanvasObject{NewLabel("X"), NewLabel("Y")}
+			r.setObjects(otherObjects)
+			assert.Equal(t, append(tt.wantPrependedObjects, otherObjects...), r.Objects())
+		})
+	}
+}
