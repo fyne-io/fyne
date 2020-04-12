b53d6399188e82bc3ea916b50c893fd56883e9a6
obsti8383 <github@florian-probst.de>
2020-04-12 21:23:12 +0200

    Fix #752 by adding additional methods with callbacks for error and information dialog

--- dialog/information.go
+++ dialog/information.go
@@ -7,7 +7,11 @@ import (
 )
 
 func createTextDialog(title, message string, icon fyne.Resource, parent fyne.Window) Dialog {
-	d := newDialog(title, message, icon, nil, parent)
+	return createTextDialogWithCallback(title, message, icon, nil, parent)
+}
+
+func createTextDialogWithCallback(title, message string, icon fyne.Resource, callback func(bool), parent fyne.Window) Dialog {
+	d := newDialog(title, message, icon, callback, parent)
 
 	d.dismiss = &widget.Button{Text: "OK",
 		OnTapped: d.Hide,
@@ -24,14 +28,32 @@ func NewInformation(title, message string, parent fyne.Window) Dialog {
 	return createTextDialog(title, message, theme.InfoIcon(), parent)
 }
 
+// The title is used for the dialog window and message is the content.
+// After creation you should call Show().
+func NewInformationWithCallback(title, message string, callback func(bool), parent fyne.Window) Dialog {
+	return createTextDialogWithCallback(title, message, theme.InfoIcon(), callback, parent)
+}
+
 // ShowInformation shows a dialog over the specified window for user
 // information. The title is used for the dialog window and message is the content.
 func ShowInformation(title, message string, parent fyne.Window) {
 	NewInformation(title, message, parent).Show()
 }
 
+// ShowInformation shows a dialog over the specified window for user
+// information. The title is used for the dialog window and message is the content.
+func ShowInformationWithCallback(title, message string, callback func(bool), parent fyne.Window) {
+	NewInformationWithCallback(title, message, callback, parent).Show()
+}
+
 // ShowError shows a dialog over the specified window for an application
 // error. The title and message are extracted from the provided error.
 func ShowError(err error, parent fyne.Window) {
 	createTextDialog("Error", err.Error(), theme.WarningIcon(), parent).Show()
+}
+
+// ShowError shows a dialog over the specified window for an application
+// error. The title and message are extracted from the provided error.
+func ShowErrorWithCallback(err error, callback func(bool), parent fyne.Window) {
+	createTextDialogWithCallback("Error", err.Error(), theme.WarningIcon(), callback, parent).Show()
 }
