69c13baf48ec156d158137ecb048758c4b48eb05
Andy Williams <andy@andy.xyz>
2020-04-15 10:49:46 +0100

    Fix missed Hidden removal

--- widget/label.go
+++ widget/label.go
@@ -89,7 +89,6 @@ func (l *Label) object() fyne.Widget {
 func (l *Label) CreateRenderer() fyne.WidgetRenderer {
 	l.ExtendBaseWidget(l)
 	l.provider = newTextProvider(l.Text, l)
-	l.provider.Hidden = l.Hidden
 	return l.provider.CreateRenderer()
 }
 
