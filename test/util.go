5a2ab86a408556ec928dc5f1da00ef9408e2c49d
Tilo Prütz <tilo@pruetz.net>
2020-04-19 08:13:31 +0200

    test.AssertImagesEqual for image comparison
    This enables golden master tests (e.g. for layout tests):
    
    ```go
    w := test.NewWindowWithPainter(content, software.NewPainter())
    w.Resize(fyne.NewSize(320, 200))
    test.LaidOutObjects(content)
    test.AssertImagesEqual(t, "my_layout.png", fyne.CurrentApp().Driver().CanvasForObject(m).Capture())
    ```
    
    The master doesn’t need to exist. An empty image will be used then.
    The actual image will be exported to disk if the images don’t equal.
    Thus it can be examined and used as new master.

--- test/util.go
+++ test/util.go
@@ -1,9 +1,52 @@
 package test
 
 import (
+	"fmt"
+	"image"
+	"image/draw"
+	"image/png"
+	"os"
+	"path/filepath"
+	"testing"
+
 	"fyne.io/fyne"
 	"fyne.io/fyne/internal/cache"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
+
+// AssertImagesEqual asserts that the master image at given filename (below testdata) and the given image are equal.
+// It fails the test if not. In this case the actual image is written to disk and reported.
+func AssertImagesEqual(t *testing.T, masterFilename string, got image.Image) {
+	wd, err := os.Getwd()
+	require.NoError(t, err)
+	masterPath := filepath.Join("testdata", masterFilename)
+	var expected image.Image
+	_, err = os.Stat(masterPath)
+	if os.IsNotExist(err) {
+		fmt.Printf("Master image does not exist at %s.\nAssume initial run and use empty image for comparison.\n", filepath.Join(wd, masterPath))
+		expected = image.NewRGBA(image.Rectangle{})
+	} else {
+		file, err := os.Open(masterPath)
+		require.NoError(t, err)
+		defer file.Close()
+		raw, _, err := image.Decode(file)
+		require.NoError(t, err)
+		normalized := image.NewRGBA(raw.Bounds())
+		draw.Draw(normalized, normalized.Bounds(), raw, image.Pt(0, 0), draw.Src)
+		expected = normalized
+	}
+	if !assert.Equal(t, expected, got) {
+		path := filepath.Join("testdata/failed", masterFilename)
+		require.NoError(t, os.MkdirAll(filepath.Dir(path), 0755))
+		file, err := os.Create(path)
+		require.NoError(t, err)
+		defer file.Close()
+		require.NoError(t, png.Encode(file, got))
+		fmt.Println("Images were not equal. Actual image written to:", filepath.Join(wd, path))
+	}
+}
 
 // Tap simulates a left mouse click on the specified object.
 func Tap(obj fyne.Tappable) {
