e4752379bf43ffc2a63e7fb3d0cc38ab2b1502ca
Tilo Prütz <tilo@pruetz.net>
2020-04-21 09:07:43 +0200

    make select tests independent from menu implementation
    They don’t assert the menus to be `*PopUp` anymore and use image comparison instead.
    For this to work the test had to be moved into widget_test package due to circular dependencies.
    The superfluous tests for updateSelected were removed.
    This behaviour is already tested with TestClearSelected and TestSetSelected.

--- widget/select_test.go
+++ widget/select_test.go
@@ -1,24 +1,27 @@
-package widget
+package widget_test
 
 import (
 	"testing"
+	"time"
 
 	"fyne.io/fyne"
+	"fyne.io/fyne/internal/painter/software"
 	"fyne.io/fyne/test"
+	"fyne.io/fyne/theme"
+	"fyne.io/fyne/widget"
 
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 )
 
 func TestNewSelect(t *testing.T) {
-	combo := NewSelect([]string{"1", "2"}, func(string) {})
+	combo := widget.NewSelect([]string{"1", "2"}, func(string) {})
 
 	assert.Equal(t, 2, len(combo.Options))
 	assert.Equal(t, "", combo.Selected)
 }
 
 func TestSelect_PlaceHolder(t *testing.T) {
-	combo := NewSelect([]string{"1", "2"}, func(string) {})
+	combo := widget.NewSelect([]string{"1", "2"}, func(string) {})
 	assert.NotEmpty(t, combo.PlaceHolder)
 
 	combo.PlaceHolder = "changed!"
@@ -31,7 +34,7 @@ func TestSelect_ClearSelected(t *testing.T) {
 		opt2     = "2"
 		optClear = ""
 	)
-	combo := NewSelect([]string{opt1, opt2}, func(string) {})
+	combo := widget.NewSelect([]string{opt1, opt2}, func(string) {})
 	assert.NotEmpty(t, combo.PlaceHolder)
 
 	combo.SetSelected(opt1)
@@ -52,7 +55,7 @@ func TestSelect_ClearSelected(t *testing.T) {
 func TestSelect_SetSelected(t *testing.T) {
 	var triggered bool
 	var triggeredValue string
-	combo := NewSelect([]string{"1", "2"}, func(s string) {
+	combo := widget.NewSelect([]string{"1", "2"}, func(s string) {
 		triggered = true
 		triggeredValue = s
 	})
@@ -65,21 +68,21 @@ func TestSelect_SetSelected(t *testing.T) {
 
 func TestSelect_SetSelected_NoChangeOnEmpty(t *testing.T) {
 	var triggered bool
-	combo := NewSelect([]string{"1", "2"}, func(string) { triggered = true })
+	combo := widget.NewSelect([]string{"1", "2"}, func(string) { triggered = true })
 	combo.SetSelected("")
 
 	assert.False(t, triggered)
 }
 
 func TestSelect_SetSelected_Invalid(t *testing.T) {
-	combo := NewSelect([]string{"1", "2"}, func(string) {})
+	combo := widget.NewSelect([]string{"1", "2"}, func(string) {})
 	combo.SetSelected("3")
 
 	assert.Equal(t, "", combo.Selected)
 }
 
 func TestSelect_SetSelected_InvalidReplace(t *testing.T) {
-	combo := NewSelect([]string{"1", "2"}, func(string) {})
+	combo := widget.NewSelect([]string{"1", "2"}, func(string) {})
 	combo.SetSelected("2")
 	combo.SetSelected("3")
 
@@ -88,7 +91,7 @@ func TestSelect_SetSelected_InvalidReplace(t *testing.T) {
 
 func TestSelect_SetSelected_Callback(t *testing.T) {
 	selected := ""
-	combo := NewSelect([]string{"1", "2"}, func(s string) {
+	combo := widget.NewSelect([]string{"1", "2"}, func(s string) {
 		selected = s
 	})
 	combo.SetSelected("2")
@@ -98,7 +101,7 @@ func TestSelect_SetSelected_Callback(t *testing.T) {
 
 func TestSelect_SetSelected_InvalidNoCallback(t *testing.T) {
 	var triggered bool
-	combo := NewSelect([]string{"1", "2"}, func(string) {
+	combo := widget.NewSelect([]string{"1", "2"}, func(string) {
 		triggered = true
 	})
 	combo.SetSelected("")
@@ -106,111 +109,81 @@ func TestSelect_SetSelected_InvalidNoCallback(t *testing.T) {
 	assert.False(t, triggered)
 }
 
-func TestSelect_updateSelected(t *testing.T) {
-	const (
-		opt1 = "1"
-		opt2 = "2"
-	)
-
-	combo := NewSelect([]string{opt1, opt2}, func(string) {})
-
-	combo.updateSelected(opt2)
-
-	assert.Equal(t, opt2, combo.Selected)
-}
-
-func TestSelect_updateSelected_Callback(t *testing.T) {
-	const (
-		opt1 = "1"
-		opt2 = "2"
-	)
-	var (
-		triggered      bool
-		triggeredValue string
-	)
-
-	combo := NewSelect([]string{opt1, opt2}, func(s string) {
-		triggered = true
-		triggeredValue = s
-	})
-
-	combo.updateSelected(opt2)
-
-	assert.True(t, triggered)
-	assert.Equal(t, opt2, triggeredValue)
-}
-
 func TestSelect_Tapped(t *testing.T) {
-	combo := NewSelect([]string{"1", "2"}, func(s string) {})
-	test.Tap(combo)
+	app := test.NewApp()
+	defer test.NewApp()
+	app.Settings().SetTheme(theme.LightTheme())
 
+	combo := widget.NewSelect([]string{"1", "2"}, func(s string) {})
+	w := test.NewWindowWithPainter(combo, software.NewPainter())
+	defer w.Close()
+	w.Resize(fyne.NewSize(200, 150))
+	combo.Resize(combo.MinSize())
+
+	test.Tap(combo)
 	canvas := fyne.CurrentApp().Driver().CanvasForObject(combo)
 	assert.Equal(t, 1, len(canvas.Overlays().List()))
-	over := canvas.Overlays().Top()
-	pos := fyne.CurrentApp().Driver().AbsolutePositionForObject(over)
-
-	cont := over.(*PopUp).Content
-	assert.Equal(t, cont.Position().X, pos.X)
-	assert.Equal(t, cont.Position().Y, pos.Y)
-
-	items := cont.(*Box).Children
-	assert.Equal(t, 2, len(items))
+	test.AssertImageMatches(t, "select_tapped.png", w.Canvas().Capture())
 }
 
 func TestSelect_Tapped_Constrained(t *testing.T) {
-	// fresh app for this test
-	test.NewApp()
-	// don't let our app hang around for too long
+	app := test.NewApp()
 	defer test.NewApp()
+	app.Settings().SetTheme(theme.LightTheme())
 
-	combo := NewSelect([]string{"1", "2"}, func(s string) {})
-	canvas := fyne.CurrentApp().Driver().CanvasForObject(combo)
-	canvas.(test.WindowlessCanvas).Resize(fyne.NewSize(100, 100))
-	canvas.SetContent(combo)
-
+	combo := widget.NewSelect([]string{"1", "2"}, func(s string) {})
+	w := test.NewWindowWithPainter(combo, software.NewPainter())
+	defer w.Close()
+	w.Resize(fyne.NewSize(200, 150))
 	combo.Resize(combo.MinSize())
+
+	canvas := w.Canvas()
 	combo.Move(fyne.NewPos(canvas.Size().Width-10, canvas.Size().Height-10))
 	test.Tap(combo)
-
-	comboPos := fyne.CurrentApp().Driver().AbsolutePositionForObject(combo)
 	assert.Equal(t, 1, len(canvas.Overlays().List()))
-	cont := canvas.Overlays().Top().(*PopUp).Content
-	assert.Less(t, cont.Position().Y, comboPos.Y, "window too small so we render higher up")
-	assert.Less(t, cont.Position().X, comboPos.X, "window too small so we render to the left")
+	test.AssertImageMatches(t, "select_tapped_constrained.png", w.Canvas().Capture())
 }
 
-func TestSelectRenderer_ApplyTheme(t *testing.T) {
-	sel := &Select{}
-	render := test.WidgetRenderer(sel).(*selectRenderer)
+func TestSelect_ChangeTheme(t *testing.T) {
+	app := test.NewApp()
+	defer test.NewApp()
+	app.Settings().SetTheme(theme.LightTheme())
 
-	textSize := render.label.TextSize
-	customTextSize := textSize
+	combo := widget.NewSelect([]string{"1", "2"}, func(s string) {})
+	w := test.NewWindowWithPainter(combo, software.NewPainter())
+	defer w.Close()
+	w.Resize(fyne.NewSize(200, 150))
+	combo.Resize(combo.MinSize())
+	combo.Move(fyne.NewPos(10, 10))
+	test.Tap(combo)
+
+	test.AssertImageMatches(t, "select_theme_initial.png", w.Canvas().Capture())
+
 	test.WithTestTheme(t, func() {
-		render.Refresh()
-		customTextSize = render.label.TextSize
+		combo.Refresh()
+		time.Sleep(100 * time.Millisecond)
+		// Looks weird but the test infrastructure does not adjust min-sizes.
+		test.AssertImageMatches(t, "select_theme_changed.png", w.Canvas().Capture())
 	})
-
-	assert.NotEqual(t, textSize, customTextSize)
 }
 
 func TestSelect_Move(t *testing.T) {
-	// fresh app for this test
 	app := test.NewApp()
-	// don't let our app hang around for too long
 	defer test.NewApp()
+	app.Settings().SetTheme(theme.LightTheme())
 
-	combo := NewSelect([]string{"1", "2"}, nil)
-	canvas := app.Driver().CanvasForObject(combo)
-	canvas.(test.WindowlessCanvas).Resize(fyne.NewSize(100, 100))
-	canvas.SetContent(combo)
+	combo := widget.NewSelect([]string{"1", "2"}, nil)
+	w := test.NewWindowWithPainter(combo, software.NewPainter())
+	defer w.Close()
+	w.Resize(fyne.NewSize(200, 150))
 
 	combo.Resize(combo.MinSize())
 	combo.Move(fyne.NewPos(10, 10))
-	require.Equal(t, fyne.NewPos(10, 10), combo.Position())
+	test.AssertImageMatches(t, "select_move_initial.png", w.Canvas().Capture())
 
 	combo.Tapped(&fyne.PointEvent{})
-	require.Equal(t, fyne.NewPos(10, 39), combo.popUp.innerPos)
+	test.AssertImageMatches(t, "select_move_tapped.png", w.Canvas().Capture())
 
 	combo.Move(fyne.NewPos(20, 20))
-	assert.Equal(t, fyne.NewPos(20, 49), combo.popUp.innerPos)
+	test.AssertImageMatches(t, "select_move_moved.png", w.Canvas().Capture())
 }
