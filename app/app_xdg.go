8298372549f37c1dd87ac3193661483c15222030
Andy Williams <andy@andy.xyz>
2020-04-27 16:58:30 +0100

    Send notifications on unis through D-Bus
    This library is good, we use it in FyneDesk too and the devs are helpful

--- app/app_xdg.go
+++ app/app_xdg.go
@@ -6,10 +6,11 @@
 package app
 
 import (
-	"log"
 	"net/url"
 	"os"
 	"path/filepath"
+
+	"github.com/godbus/dbus/v5"
 
 	"fyne.io/fyne"
 	"fyne.io/fyne/theme"
@@ -25,8 +26,23 @@ func (app *fyneApp) OpenURL(url *url.URL) error {
 	return cmd.Run()
 }
 
-func (app *fyneApp) SendNotification(notify *fyne.Notification) {
-	log.Println("NOT YET IMPLEMENTED") // TODO
+func (app *fyneApp) SendNotification(n *fyne.Notification) {
+	conn, err := dbus.SessionBus() // shared connection, don't close
+	if err != nil {
+		fyne.LogError("Unable to connect to session D-Bus", err)
+		return
+	}
+
+	appName := ""       // considered fyne.CurrentApp().UniqueID() but really would need a prettier name
+	appIcon := ""       // TODO in the future extract icon from app to cache and pass path
+	timeout := int32(0) // we don't support this yet
+
+	obj := conn.Object("org.freedesktop.Notifications", "/org/freedesktop/Notifications")
+	call := obj.Call("org.freedesktop.Notifications.Notify", 0, appName, uint32(0),
+		appIcon, n.Title, n.Content, []string{}, map[string]dbus.Variant{}, timeout)
+	if call.Err != nil {
+		fyne.LogError("Failed to send message to bus", call.Err)
+	}
 }
 
 func rootConfigDir() string {
--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 	github.com/fyne-io/mobile v0.0.2-0.20200421111809-d9d371319719
 	github.com/go-gl/gl v0.0.0-20190320180904-bf2b1f2f34d7
 	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20200222043503-6f7a984d4dc4
+	github.com/godbus/dbus/v5 v5.0.3
 	github.com/goki/freetype v0.0.0-20181231101311-fa8a33aabaff
 	github.com/jackmordaunt/icns v0.0.0-20181231085925-4f16af745526
 	github.com/josephspurrier/goversioninfo v0.0.0-20200309025242-14b0ab84c6ca
