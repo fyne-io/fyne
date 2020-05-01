dc712891657bdd0672d2341280eaaf24b366b973
Tilo Prütz <tilo@pruetz.net>
2020-05-01 19:00:06 +0200

    move internal/app tests into app_test package

--- internal/app/focus_test.go
+++ internal/app/focus_test.go
@@ -1,134 +1,65 @@
-package app
+package app_test
 
 import (
 	"testing"
 
+	"fyne.io/fyne/internal/app"
 	"fyne.io/fyne/test"
 	"fyne.io/fyne/widget"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestFocusManager_nextInChain(t *testing.T) {
-	c := test.NewCanvas()
-	entry1 := widget.NewEntry()
-	entry2 := widget.NewEntry()
-	c.SetContent(widget.NewVBox(entry1, entry2))
-
-	manager := NewFocusManager(c)
-	next := manager.nextInChain(entry1)
-	assert.Equal(t, entry2, next)
-}
-
-func TestFocusManager_nextInChain_Nil(t *testing.T) {
-	c := test.NewCanvas()
-	entry1 := widget.NewEntry()
-	entry2 := widget.NewEntry()
-	c.SetContent(widget.NewVBox(entry1, entry2))
-
-	manager := NewFocusManager(c)
-	next := manager.nextInChain(nil)
-	assert.Equal(t, entry1, next)
-}
-
-func TestFocusManager_nextInChain_Hidden(t *testing.T) {
-	c := test.NewCanvas()
-	entry1 := widget.NewEntry()
-	check := widget.NewCheck("test", func(bool) {})
-	check.Hide()
-	entry2 := widget.NewEntry()
-	c.SetContent(widget.NewVBox(entry1, check, entry2))
-
-	manager := NewFocusManager(c)
-	next := manager.nextInChain(entry1)
-	assert.Equal(t, entry2, next)
-}
-
-func TestFocusManager_nextInChain_Disableable(t *testing.T) {
-	c := test.NewCanvas()
-	entry1 := widget.NewEntry()
-	check := widget.NewCheck("test", func(bool) {})
-	check.Disable()
-	entry2 := widget.NewEntry()
-	c.SetContent(widget.NewVBox(entry1, check, entry2))
-
-	manager := NewFocusManager(c)
-	next := manager.nextInChain(entry1)
-	assert.Equal(t, entry2, next)
-}
-
 func TestFocusManager_FocusNext(t *testing.T) {
 	c := test.NewCanvas()
 	entry1 := widget.NewEntry()
+	hidden := widget.NewCheck("test", func(bool) {})
+	hidden.Hide()
 	entry2 := widget.NewEntry()
-	c.SetContent(widget.NewVBox(entry1, entry2))
+	disabled := widget.NewCheck("test", func(bool) {})
+	disabled.Disable()
+	entry3 := widget.NewEntry()
+	c.SetContent(widget.NewVBox(entry1, hidden, entry2, disabled, entry3))
 
-	manager := NewFocusManager(c)
-	c.Focus(entry1)
+	manager := app.NewFocusManager(c)
+	assert.Nil(t, c.Focused())
+
+	manager.FocusNext(nil)
 	assert.Equal(t, entry1, c.Focused())
 
 	manager.FocusNext(entry1)
 	assert.Equal(t, entry2, c.Focused())
-}
 
-func TestFocusManager_previousInChain(t *testing.T) {
-	c := test.NewCanvas()
-	entry1 := widget.NewEntry()
-	entry2 := widget.NewEntry()
-	c.SetContent(widget.NewVBox(entry1, entry2))
+	manager.FocusNext(entry2)
+	assert.Equal(t, entry3, c.Focused())
 
-	manager := NewFocusManager(c)
-	previous := manager.previousInChain(entry2)
-	assert.Equal(t, entry1, previous)
-}
-
-func TestFocusManager_previousInChain_Nil(t *testing.T) {
-	c := test.NewCanvas()
-	entry1 := widget.NewEntry()
-	entry2 := widget.NewEntry()
-	c.SetContent(widget.NewVBox(entry1, entry2))
-
-	manager := NewFocusManager(c)
-	previous := manager.previousInChain(nil)
-	assert.Equal(t, entry2, previous)
-}
-
-func TestFocusManager_previousInChain_Hidden(t *testing.T) {
-	c := test.NewCanvas()
-	entry1 := widget.NewEntry()
-	check := widget.NewCheck("test", func(bool) {})
-	check.Hide()
-	entry2 := widget.NewEntry()
-	c.SetContent(widget.NewVBox(entry1, check, entry2))
-
-	manager := NewFocusManager(c)
-	next := manager.previousInChain(entry2)
-	assert.Equal(t, entry1, next)
-}
-
-func TestFocusManager_previousInChain_Disableable(t *testing.T) {
-	c := test.NewCanvas()
-	entry1 := widget.NewEntry()
-	check := widget.NewCheck("test", func(bool) {})
-	check.Disable()
-	entry2 := widget.NewEntry()
-	c.SetContent(widget.NewVBox(entry1, check, entry2))
-
-	manager := NewFocusManager(c)
-	next := manager.previousInChain(entry2)
-	assert.Equal(t, entry1, next)
+	manager.FocusNext(entry3)
+	assert.Equal(t, entry1, c.Focused())
 }
 
 func TestFocusManager_FocusPrevious(t *testing.T) {
 	c := test.NewCanvas()
 	entry1 := widget.NewEntry()
+	hidden := widget.NewCheck("test", func(bool) {})
+	hidden.Hide()
 	entry2 := widget.NewEntry()
-	c.SetContent(widget.NewVBox(entry1, entry2))
+	disabled := widget.NewCheck("test", func(bool) {})
+	disabled.Disable()
+	entry3 := widget.NewEntry()
+	c.SetContent(widget.NewVBox(entry1, hidden, entry2, disabled, entry3))
 
-	manager := NewFocusManager(c)
-	c.Focus(entry2)
+	manager := app.NewFocusManager(c)
+	assert.Nil(t, c.Focused())
+
+	manager.FocusPrevious(nil)
+	assert.Equal(t, entry3, c.Focused())
+
+	manager.FocusPrevious(entry3)
 	assert.Equal(t, entry2, c.Focused())
 
 	manager.FocusPrevious(entry2)
 	assert.Equal(t, entry1, c.Focused())
+
+	manager.FocusPrevious(entry1)
+	assert.Equal(t, entry3, c.Focused())
 }
