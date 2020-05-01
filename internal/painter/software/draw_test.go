07ddfa4d5ecf71a0373bb2ea98528c0e28e511a3
Andy Williams <andy@andy.xyz>
2020-05-01 21:45:30 +0100

    And add tests for existing image work

--- internal/painter/software/draw_test.go
+++ internal/painter/software/draw_test.go
@@ -35,6 +35,34 @@ func TestDrawImage(t *testing.T) {
 	test.AssertImageMatches(t, "draw_image_default.png", target)
 }
 
+func TestDrawImage_StretchX(t *testing.T) {
+	target := image.NewRGBA(image.Rect(0, 0, 100, 50))
+	img := canvas.NewImageFromImage(makeTestImage())
+	img.Resize(fyne.NewSize(100, 50))
+
+	drawImage(test.Canvas(), img, fyne.NewPos(0, 0), fyne.NewSize(100, 50), target)
+	test.AssertImageMatches(t, "draw_image_stretchx.png", target)
+}
+
+func TestDrawImage_StretchY(t *testing.T) {
+	target := image.NewRGBA(image.Rect(0, 0, 50, 100))
+	img := canvas.NewImageFromImage(makeTestImage())
+	img.Resize(fyne.NewSize(50, 100))
+
+	drawImage(test.Canvas(), img, fyne.NewPos(0, 0), fyne.NewSize(50, 100), target)
+	test.AssertImageMatches(t, "draw_image_stretchy.png", target)
+}
+
+func TestDrawImage_Contain(t *testing.T) {
+	target := image.NewRGBA(image.Rect(0, 0, 50, 50))
+	img := canvas.NewImageFromImage(makeTestImage())
+	img.FillMode = canvas.ImageFillContain
+	img.Resize(fyne.NewSize(50, 50))
+
+	drawImage(test.Canvas(), img, fyne.NewPos(0, 0), fyne.NewSize(50, 50), target)
+	test.AssertImageMatches(t, "draw_image_default.png", target)
+}
+
 func TestDrawImage_ContainX(t *testing.T) {
 	target := image.NewRGBA(image.Rect(0, 0, 100, 50))
 	img := canvas.NewImageFromImage(makeTestImage())
