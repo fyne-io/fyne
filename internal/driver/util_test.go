b2973aad8e0a2d1e2cd7608e81cd53838ed04cf3
Tilo Prütz <tilo@pruetz.net>
2020-05-07 09:05:19 +0200

    driver/util_test: sort methods and imports

--- internal/driver/util_test.go
+++ internal/driver/util_test.go
@@ -4,68 +4,15 @@ import (
 	"image/color"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
-
 	"fyne.io/fyne"
 	"fyne.io/fyne/canvas"
 	"fyne.io/fyne/internal/driver"
 	"fyne.io/fyne/layout"
 	_ "fyne.io/fyne/test"
 	"fyne.io/fyne/widget"
+
+	"github.com/stretchr/testify/assert"
 )
-
-func TestWalkVisibleObjectTree(t *testing.T) {
-	rect := canvas.NewRectangle(color.White)
-	rect.SetMinSize(fyne.NewSize(100, 100))
-	child := canvas.NewRectangle(color.Black)
-	child.Hide()
-	base := widget.NewHBox(rect, child)
-
-	walked := 0
-	driver.WalkVisibleObjectTree(base, func(object fyne.CanvasObject, position fyne.Position, clippingPos fyne.Position, clippingSize fyne.Size) bool {
-		walked++
-		return false
-	}, nil)
-
-	assert.Equal(t, 2, walked)
-}
-
-func TestWalkWholeObjectTree(t *testing.T) {
-	rect := canvas.NewRectangle(color.White)
-	rect.SetMinSize(fyne.NewSize(100, 100))
-	child := canvas.NewRectangle(color.Black)
-	child.Hide()
-	base := widget.NewHBox(rect, child)
-
-	walked := 0
-	driver.WalkCompleteObjectTree(base, func(object fyne.CanvasObject, position fyne.Position, clippingPos fyne.Position, clippingSize fyne.Size) bool {
-		walked++
-		return false
-	}, nil)
-
-	assert.Equal(t, 3, walked)
-}
-
-func TestWalkVisibleObjectTree_Clip(t *testing.T) {
-	rect := canvas.NewRectangle(color.White)
-	rect.SetMinSize(fyne.NewSize(100, 100))
-	child := canvas.NewRectangle(color.Black)
-	base := fyne.NewContainerWithLayout(layout.NewGridLayout(1), rect, widget.NewScrollContainer(child))
-
-	clipPos := fyne.NewPos(0, 0)
-	clipSize := rect.MinSize()
-
-	driver.WalkVisibleObjectTree(base, func(object fyne.CanvasObject, position fyne.Position, clippingPos fyne.Position, clippingSize fyne.Size) bool {
-		if _, ok := object.(*widget.ScrollContainer); ok {
-			clipPos = clippingPos
-			clipSize = clippingSize
-		}
-		return false
-	}, nil)
-
-	assert.Equal(t, fyne.NewPos(0, 104), clipPos)
-	assert.Equal(t, fyne.NewSize(100, 100), clipSize)
-}
 
 func TestAbsolutePositionForObject(t *testing.T) {
 	t1r1c1 := widget.NewLabel("row 1 col 1")
@@ -163,4 +110,57 @@ func TestAbsolutePositionForObject(t *testing.T) {
 			assert.Equal(t, tt.want, driver.AbsolutePositionForObject(tt.object, trees))
 		})
 	}
+}
+
+func TestWalkVisibleObjectTree(t *testing.T) {
+	rect := canvas.NewRectangle(color.White)
+	rect.SetMinSize(fyne.NewSize(100, 100))
+	child := canvas.NewRectangle(color.Black)
+	child.Hide()
+	base := widget.NewHBox(rect, child)
+
+	walked := 0
+	driver.WalkVisibleObjectTree(base, func(object fyne.CanvasObject, position fyne.Position, clippingPos fyne.Position, clippingSize fyne.Size) bool {
+		walked++
+		return false
+	}, nil)
+
+	assert.Equal(t, 2, walked)
+}
+
+func TestWalkVisibleObjectTree_Clip(t *testing.T) {
+	rect := canvas.NewRectangle(color.White)
+	rect.SetMinSize(fyne.NewSize(100, 100))
+	child := canvas.NewRectangle(color.Black)
+	base := fyne.NewContainerWithLayout(layout.NewGridLayout(1), rect, widget.NewScrollContainer(child))
+
+	clipPos := fyne.NewPos(0, 0)
+	clipSize := rect.MinSize()
+
+	driver.WalkVisibleObjectTree(base, func(object fyne.CanvasObject, position fyne.Position, clippingPos fyne.Position, clippingSize fyne.Size) bool {
+		if _, ok := object.(*widget.ScrollContainer); ok {
+			clipPos = clippingPos
+			clipSize = clippingSize
+		}
+		return false
+	}, nil)
+
+	assert.Equal(t, fyne.NewPos(0, 104), clipPos)
+	assert.Equal(t, fyne.NewSize(100, 100), clipSize)
+}
+
+func TestWalkWholeObjectTree(t *testing.T) {
+	rect := canvas.NewRectangle(color.White)
+	rect.SetMinSize(fyne.NewSize(100, 100))
+	child := canvas.NewRectangle(color.Black)
+	child.Hide()
+	base := widget.NewHBox(rect, child)
+
+	walked := 0
+	driver.WalkCompleteObjectTree(base, func(object fyne.CanvasObject, position fyne.Position, clippingPos fyne.Position, clippingSize fyne.Size) bool {
+		walked++
+		return false
+	}, nil)
+
+	assert.Equal(t, 3, walked)
 }
