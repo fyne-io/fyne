cc221836eb9d62980e411b2cf2f6a9bd976f3b2a
German Laullon <glaullon@vmware.com>
2020-05-11 22:31:17 +0200

    sample app to test image rendering

---
+++ cmd/img/main.go
@@ -0,0 +1,35 @@
+// Package main loads a very basic Hello World graphical application
+package main
+
+import (
+	"fyne.io/fyne"
+	"fyne.io/fyne/app"
+	"fyne.io/fyne/canvas"
+	"fyne.io/fyne/cmd/fyne_demo/data"
+	"fyne.io/fyne/layout"
+	"fyne.io/fyne/widget"
+)
+
+func main() {
+	a := app.New()
+
+	hello := widget.NewLabel("Hello Fyne!")
+	logo := canvas.NewImageFromResource(data.FyneScene)
+	quit := widget.NewButton("Quit", func() {
+		a.Quit()
+	})
+
+	logo.FillMode = canvas.ImageFillContain
+	logo.ScalingFilter = canvas.LinearFilter
+	logo.SetMinSize(fyne.NewSize(100, 100))
+
+	w := a.NewWindow("Hello")
+
+	w.SetContent(fyne.NewContainerWithLayout(layout.NewBorderLayout(hello, quit, nil, nil),
+		hello,
+		logo,
+		quit,
+	))
+
+	w.ShowAndRun()
+}
