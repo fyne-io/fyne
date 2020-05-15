130909feb119959ec0f625beb9c0bd0f3d352cc6
Andy Williams <andy@andywilliams.me>
2020-05-15 09:43:37 +0100

    Remove helper constructors until we see if they are needed

--- storage/file.go
+++ storage/file.go
@@ -10,20 +10,8 @@ func OpenFileFromURI(uri fyne.URI) (fyne.FileReadCloser, error) {
 	return fyne.CurrentApp().Driver().FileReaderForURI(uri)
 }
 
-// OpenFileFromURIString loads a file read stream from a simple resource identifier.
-// This is a helper for apps that have persisted URI.String() and want to re-use.
-func OpenFileFromURIString(uri string) (fyne.FileReadCloser, error) {
-	return OpenFileFromURI(NewURI(uri))
-}
-
 // SaveFileToURI loads a file write stream to a resource identifier.
 // This is mostly provided so that file references can be saved using their URI and written to again later.
 func SaveFileToURI(uri fyne.URI) (fyne.FileWriteCloser, error) {
 	return fyne.CurrentApp().Driver().FileWriterForURI(uri)
-}
-
-// SaveFileToURIString loads a file write stream to a simple resource identifier.
-// This is a helper for apps that have persisted URI.String() and want to re-use.
-func SaveFileToURIString(uri string) (fyne.FileWriteCloser, error) {
-	return SaveFileToURI(NewURI(uri))
 }
