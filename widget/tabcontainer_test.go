f6abe9eb4d2dc8511a7e260d12448688f99927c6
Tilo Prütz <tilo@pruetz.net>
2020-05-17 09:08:00 +0200

    sort tab container tests

--- widget/tabcontainer_test.go
+++ widget/tabcontainer_test.go
@@ -12,77 +12,33 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestTabContainer_Empty(t *testing.T) {
-	tabs := NewTabContainer()
-	assert.Equal(t, 0, len(tabs.Items))
-	assert.Equal(t, -1, tabs.CurrentTabIndex())
-	assert.Nil(t, tabs.CurrentTab())
-	min := tabs.MinSize()
-	assert.Equal(t, 4, min.Height)
-	assert.Equal(t, 0, min.Width)
+func TestTabContainer_ApplyTheme(t *testing.T) {
+	fyne.CurrentApp().Settings().SetTheme(theme.DarkTheme())
+	item := &TabItem{Text: "Test1", Content: NewLabel("Test1")}
+	tabs := NewTabContainer(item)
+	underline := test.WidgetRenderer(tabs).(*tabContainerRenderer).line
+	barColor := underline.FillColor
+
+	fyne.CurrentApp().Settings().SetTheme(theme.LightTheme())
+	tabs.Refresh()
+	assert.NotEqual(t, barColor, underline.FillColor)
+
+	tabButton := tabs.makeButton(item)
+	render := test.WidgetRenderer(tabButton).(*tabButtonRenderer)
+
+	textSize := render.label.TextSize
+	customTextSize := textSize
+	test.WithTestTheme(t, func() {
+		render.Refresh()
+		customTextSize = render.label.TextSize
+	})
+
+	assert.NotEqual(t, textSize, customTextSize)
 }
 
-func TestTabContainer_Resize_Empty(t *testing.T) {
-	tabs := NewTabContainer()
-	tabs.Resize(fyne.NewSize(10, 10))
-	size := tabs.Size()
-	assert.Equal(t, 10, size.Height)
-	assert.Equal(t, 10, size.Width)
-}
-
-func TestTabContainer_CurrentTabIndex(t *testing.T) {
-	tabs := NewTabContainer(&TabItem{Text: "Test", Content: NewLabel("Test")})
-
-	assert.Equal(t, 1, len(tabs.Items))
-	assert.Equal(t, 0, tabs.CurrentTabIndex())
-}
-
-func TestTabContainer_CurrentTab(t *testing.T) {
-	tab1 := &TabItem{Text: "Test1", Content: NewLabel("Test1")}
-	tab2 := &TabItem{Text: "Test2", Content: NewLabel("Test2")}
-	tabs := NewTabContainer(tab1, tab2)
-
-	assert.Equal(t, 2, len(tabs.Items))
-	assert.Equal(t, tab1, tabs.CurrentTab())
-}
-
-func TestTabContainer_SelectTab(t *testing.T) {
-	tab1 := &TabItem{Text: "Test1", Content: NewLabel("Test1")}
-	tab2 := &TabItem{Text: "Test2", Content: NewLabel("Test2")}
-	tabs := NewTabContainer(tab1, tab2)
-
-	assert.Equal(t, 2, len(tabs.Items))
-	assert.Equal(t, tab1, tabs.CurrentTab())
-
-	var selectedTab *TabItem
-	tabs.OnChanged = func(tab *TabItem) {
-		selectedTab = tab
-	}
-	tabs.SelectTab(tab2)
-	assert.Equal(t, tab2, tabs.CurrentTab())
-	assert.Equal(t, tab2, selectedTab)
-
-	tabs.OnChanged = func(tab *TabItem) {
-		assert.Fail(t, "unexpected tab selection")
-	}
-	tabs.SelectTab(NewTabItem("Test3", NewLabel("Test3")))
-	assert.Equal(t, tab2, tabs.CurrentTab())
-}
-
-func TestTabContainer_SelectTabIndex(t *testing.T) {
-	tabs := NewTabContainer(&TabItem{Text: "Test1", Content: NewLabel("Test1")},
-		&TabItem{Text: "Test2", Content: NewLabel("Test2")})
-
-	assert.Equal(t, 2, len(tabs.Items))
-	assert.Equal(t, 0, tabs.CurrentTabIndex())
-
-	var selectedTab *TabItem
-	tabs.OnChanged = func(tab *TabItem) {
-		selectedTab = tab
-	}
-	tabs.SelectTabIndex(1)
-	assert.Equal(t, 1, tabs.CurrentTabIndex())
-	assert.Equal(t, tabs.Items[1], selectedTab)
+func TestTabContainer_ButtonBackgroundColor(t *testing.T) {
+	r := test.WidgetRenderer(&tabButton{})
+	assert.Equal(t, theme.BackgroundColor(), r.BackgroundColor())
 }
 
 func TestTabContainer_ChangeItemContent(t *testing.T) {
@@ -109,110 +65,66 @@ func TestTabContainer_ChangeItemContent(t *testing.T) {
 	assert.False(t, newUnselected.Visible())
 }
 
-func TestTabContainer_SetTabLocation(t *testing.T) {
-	tab1 := NewTabItem("Test1", NewLabel("Test1"))
-	tab2 := NewTabItem("Test2", NewLabel("Test2"))
-	tab3 := NewTabItem("Test3", NewLabel("Test3"))
-	tabs := NewTabContainer(tab1, tab2, tab3)
-	r := test.WidgetRenderer(tabs).(*tabContainerRenderer)
+func TestTabContainer_CurrentTab(t *testing.T) {
+	tab1 := &TabItem{Text: "Test1", Content: NewLabel("Test1")}
+	tab2 := &TabItem{Text: "Test2", Content: NewLabel("Test2")}
+	tabs := NewTabContainer(tab1, tab2)
 
-	buttons := r.tabBar.Objects
-	require.Len(t, buttons, 3)
-	content := tabs.Items[0].Content
-
-	tabs.SetTabLocation(TabLocationLeading)
-	tabs.Resize(r.MinSize())
-	assert.Equal(t, fyne.NewPos(0, 0), r.tabBar.Position())
-	assert.Equal(t, fyne.NewPos(r.tabBar.MinSize().Width+theme.Padding(), 0), content.Position())
-	assert.Equal(t, fyne.NewPos(r.tabBar.MinSize().Width, 0), r.line.Position())
-	assert.Equal(t, fyne.NewSize(theme.Padding(), tabs.MinSize().Height), r.line.Size())
-	y := 0
-	for _, button := range buttons {
-		assert.Equal(t, fyne.NewPos(0, y), button.Position())
-		y += button.Size().Height
-		y += theme.Padding()
-	}
-
-	tabs.SetTabLocation(TabLocationBottom)
-	tabs.Resize(r.MinSize())
-	assert.Equal(t, fyne.NewPos(0, content.MinSize().Height+theme.Padding()), r.tabBar.Position())
-	assert.Equal(t, fyne.NewPos(0, 0), content.Position())
-	assert.Equal(t, fyne.NewPos(0, content.Size().Height), r.line.Position())
-	assert.Equal(t, fyne.NewSize(tabs.MinSize().Width, theme.Padding()), r.line.Size())
-	x := 0
-	for _, button := range buttons {
-		assert.Equal(t, fyne.NewPos(x, 0), button.Position())
-		x += button.Size().Width
-		x += theme.Padding()
-	}
-
-	tabs.SetTabLocation(TabLocationTrailing)
-	tabs.Resize(r.MinSize())
-	assert.Equal(t, fyne.NewPos(content.Size().Width+theme.Padding(), 0), r.tabBar.Position())
-	assert.Equal(t, fyne.NewPos(0, 0), content.Position())
-	assert.Equal(t, fyne.NewPos(content.Size().Width, 0), r.line.Position())
-	assert.Equal(t, fyne.NewSize(theme.Padding(), tabs.MinSize().Height), r.line.Size())
-	y = 0
-	for _, button := range buttons {
-		assert.Equal(t, fyne.NewPos(0, y), button.Position())
-		y += button.Size().Height
-		y += theme.Padding()
-	}
-
-	tabs.SetTabLocation(TabLocationTop)
-	tabs.Resize(r.MinSize())
-	assert.Equal(t, fyne.NewPos(0, 0), r.tabBar.Position())
-	assert.Equal(t, fyne.NewPos(0, r.tabBar.MinSize().Height+theme.Padding()), content.Position())
-	assert.Equal(t, fyne.NewPos(0, r.tabBar.MinSize().Height), r.line.Position())
-	assert.Equal(t, fyne.NewSize(tabs.MinSize().Width, theme.Padding()), r.line.Size())
-	x = 0
-	for _, button := range buttons {
-		assert.Equal(t, fyne.NewPos(x, 0), button.Position())
-		x += button.Size().Width
-		x += theme.Padding()
-	}
+	assert.Equal(t, 2, len(tabs.Items))
+	assert.Equal(t, tab1, tabs.CurrentTab())
 }
 
-func TestTabContainer_Tapped(t *testing.T) {
-	tabs := NewTabContainer(
-		NewTabItem("Test1", NewLabel("Test1")),
-		NewTabItem("Test2", NewLabel("Test2")),
-		NewTabItem("Test3", NewLabel("Test3")),
-	)
+func TestTabContainer_CurrentTabIndex(t *testing.T) {
+	tabs := NewTabContainer(&TabItem{Text: "Test", Content: NewLabel("Test")})
+
+	assert.Equal(t, 1, len(tabs.Items))
+	assert.Equal(t, 0, tabs.CurrentTabIndex())
+}
+
+func TestTabContainer_DynamicTabs(t *testing.T) {
+	item := NewTabItem("Text1", canvas.NewCircle(theme.BackgroundColor()))
+	tabs := NewTabContainer(item)
 	r := test.WidgetRenderer(tabs).(*tabContainerRenderer)
 
-	tab1 := r.tabBar.Objects[0].(*tabButton)
-	tab2 := r.tabBar.Objects[1].(*tabButton)
-	tab3 := r.tabBar.Objects[2].(*tabButton)
-	require.Equal(t, 0, tabs.CurrentTabIndex())
-	require.Equal(t, theme.PrimaryColor(), test.WidgetRenderer(tab1).BackgroundColor())
+	appendedItem := NewTabItem("Text2", canvas.NewCircle(theme.BackgroundColor()))
 
-	tab2.Tapped(&fyne.PointEvent{})
-	assert.Equal(t, 1, tabs.CurrentTabIndex())
-	require.Equal(t, theme.BackgroundColor(), test.WidgetRenderer(tab1).BackgroundColor())
-	require.Equal(t, theme.PrimaryColor(), test.WidgetRenderer(tab2).BackgroundColor())
-	require.Equal(t, theme.BackgroundColor(), test.WidgetRenderer(tab3).BackgroundColor())
-	assert.False(t, tabs.Items[0].Content.Visible())
-	assert.True(t, tabs.Items[1].Content.Visible())
-	assert.False(t, tabs.Items[2].Content.Visible())
+	tabs.Append(appendedItem)
+	assert.Equal(t, len(tabs.Items), 2)
+	assert.Equal(t, len(r.tabBar.Objects), 2)
+	assert.Equal(t, tabs.Items[1].Text, appendedItem.Text)
 
-	tab3.Tapped(&fyne.PointEvent{})
-	assert.Equal(t, 2, tabs.CurrentTabIndex())
-	require.Equal(t, theme.BackgroundColor(), test.WidgetRenderer(tab1).BackgroundColor())
-	require.Equal(t, theme.BackgroundColor(), test.WidgetRenderer(tab2).BackgroundColor())
-	require.Equal(t, theme.PrimaryColor(), test.WidgetRenderer(tab3).BackgroundColor())
-	assert.False(t, tabs.Items[0].Content.Visible())
-	assert.False(t, tabs.Items[1].Content.Visible())
-	assert.True(t, tabs.Items[2].Content.Visible())
+	tabs.RemoveIndex(1)
+	assert.Equal(t, len(tabs.Items), 1)
+	assert.Equal(t, len(r.tabBar.Objects), 1)
+	assert.Equal(t, tabs.Items[0].Text, item.Text)
 
-	tab1.Tapped(&fyne.PointEvent{})
-	assert.Equal(t, 0, tabs.CurrentTabIndex())
-	require.Equal(t, theme.PrimaryColor(), test.WidgetRenderer(tab1).BackgroundColor())
-	require.Equal(t, theme.BackgroundColor(), test.WidgetRenderer(tab2).BackgroundColor())
-	require.Equal(t, theme.BackgroundColor(), test.WidgetRenderer(tab3).BackgroundColor())
-	assert.True(t, tabs.Items[0].Content.Visible())
-	assert.False(t, tabs.Items[1].Content.Visible())
-	assert.False(t, tabs.Items[2].Content.Visible())
+	tabs.Append(appendedItem)
+	tabs.Remove(tabs.Items[0])
+	assert.Equal(t, len(tabs.Items), 1)
+	assert.Equal(t, len(r.tabBar.Objects), 1)
+	assert.Equal(t, tabs.Items[0].Text, appendedItem.Text)
+
+	appendedItem3 := NewTabItem("Text3", canvas.NewCircle(theme.BackgroundColor()))
+	appendedItem4 := NewTabItem("Text4", canvas.NewCircle(theme.BackgroundColor()))
+	appendedItem5 := NewTabItem("Text5", canvas.NewCircle(theme.BackgroundColor()))
+	tabs.Append(appendedItem3)
+	tabs.Append(appendedItem4)
+	tabs.Append(appendedItem5)
+	assert.Equal(t, 4, len(tabs.Items))
+	assert.Equal(t, 4, len(r.tabBar.Objects))
+	assert.Equal(t, "Text3", tabs.Items[1].Text)
+	assert.Equal(t, "Text4", tabs.Items[2].Text)
+	assert.Equal(t, "Text5", tabs.Items[3].Text)
+}
+
+func TestTabContainer_Empty(t *testing.T) {
+	tabs := NewTabContainer()
+	assert.Equal(t, 0, len(tabs.Items))
+	assert.Equal(t, -1, tabs.CurrentTabIndex())
+	assert.Nil(t, tabs.CurrentTab())
+	min := tabs.MinSize()
+	assert.Equal(t, 4, min.Height)
+	assert.Equal(t, 0, min.Width)
 }
 
 func TestTabContainer_Hidden_AsChild(t *testing.T) {
@@ -231,28 +143,15 @@ func TestTabContainer_Hidden_AsChild(t *testing.T) {
 	assert.True(t, c2.Visible())
 }
 
-func TestTabContainer_ApplyTheme(t *testing.T) {
-	fyne.CurrentApp().Settings().SetTheme(theme.DarkTheme())
-	item := &TabItem{Text: "Test1", Content: NewLabel("Test1")}
-	tabs := NewTabContainer(item)
-	underline := test.WidgetRenderer(tabs).(*tabContainerRenderer).line
-	barColor := underline.FillColor
+func TestTabContainer_HoveredButton(t *testing.T) {
+	b := &tabButton{}
+	r := test.WidgetRenderer(b)
+	require.Equal(t, theme.BackgroundColor(), r.BackgroundColor())
 
-	fyne.CurrentApp().Settings().SetTheme(theme.LightTheme())
-	tabs.Refresh()
-	assert.NotEqual(t, barColor, underline.FillColor)
-
-	tabButton := tabs.makeButton(item)
-	render := test.WidgetRenderer(tabButton).(*tabButtonRenderer)
-
-	textSize := render.label.TextSize
-	customTextSize := textSize
-	test.WithTestTheme(t, func() {
-		render.Refresh()
-		customTextSize = render.label.TextSize
-	})
-
-	assert.NotEqual(t, textSize, customTextSize)
+	b.MouseIn(&desktop.MouseEvent{})
+	assert.Equal(t, theme.HoverColor(), r.BackgroundColor())
+	b.MouseOut()
+	assert.Equal(t, theme.BackgroundColor(), r.BackgroundColor())
 }
 
 func TestTabContainer_Layout(t *testing.T) {
@@ -413,56 +312,51 @@ func TestTabContainer_Layout(t *testing.T) {
 	}
 }
 
-func TestTabContainer_DynamicTabs(t *testing.T) {
-	item := NewTabItem("Text1", canvas.NewCircle(theme.BackgroundColor()))
-	tabs := NewTabContainer(item)
-	r := test.WidgetRenderer(tabs).(*tabContainerRenderer)
-
-	appendedItem := NewTabItem("Text2", canvas.NewCircle(theme.BackgroundColor()))
-
-	tabs.Append(appendedItem)
-	assert.Equal(t, len(tabs.Items), 2)
-	assert.Equal(t, len(r.tabBar.Objects), 2)
-	assert.Equal(t, tabs.Items[1].Text, appendedItem.Text)
-
-	tabs.RemoveIndex(1)
-	assert.Equal(t, len(tabs.Items), 1)
-	assert.Equal(t, len(r.tabBar.Objects), 1)
-	assert.Equal(t, tabs.Items[0].Text, item.Text)
-
-	tabs.Append(appendedItem)
-	tabs.Remove(tabs.Items[0])
-	assert.Equal(t, len(tabs.Items), 1)
-	assert.Equal(t, len(r.tabBar.Objects), 1)
-	assert.Equal(t, tabs.Items[0].Text, appendedItem.Text)
-
-	appendedItem3 := NewTabItem("Text3", canvas.NewCircle(theme.BackgroundColor()))
-	appendedItem4 := NewTabItem("Text4", canvas.NewCircle(theme.BackgroundColor()))
-	appendedItem5 := NewTabItem("Text5", canvas.NewCircle(theme.BackgroundColor()))
-	tabs.Append(appendedItem3)
-	tabs.Append(appendedItem4)
-	tabs.Append(appendedItem5)
-	assert.Equal(t, 4, len(tabs.Items))
-	assert.Equal(t, 4, len(r.tabBar.Objects))
-	assert.Equal(t, "Text3", tabs.Items[1].Text)
-	assert.Equal(t, "Text4", tabs.Items[2].Text)
-	assert.Equal(t, "Text5", tabs.Items[3].Text)
+func TestTabContainer_Resize_Empty(t *testing.T) {
+	tabs := NewTabContainer()
+	tabs.Resize(fyne.NewSize(10, 10))
+	size := tabs.Size()
+	assert.Equal(t, 10, size.Height)
+	assert.Equal(t, 10, size.Width)
 }
 
-func TestTabContainer_HoveredButton(t *testing.T) {
-	b := &tabButton{}
-	r := test.WidgetRenderer(b)
-	require.Equal(t, theme.BackgroundColor(), r.BackgroundColor())
+func TestTabContainer_SelectTab(t *testing.T) {
+	tab1 := &TabItem{Text: "Test1", Content: NewLabel("Test1")}
+	tab2 := &TabItem{Text: "Test2", Content: NewLabel("Test2")}
+	tabs := NewTabContainer(tab1, tab2)
 
-	b.MouseIn(&desktop.MouseEvent{})
-	assert.Equal(t, theme.HoverColor(), r.BackgroundColor())
-	b.MouseOut()
-	assert.Equal(t, theme.BackgroundColor(), r.BackgroundColor())
+	assert.Equal(t, 2, len(tabs.Items))
+	assert.Equal(t, tab1, tabs.CurrentTab())
+
+	var selectedTab *TabItem
+	tabs.OnChanged = func(tab *TabItem) {
+		selectedTab = tab
+	}
+	tabs.SelectTab(tab2)
+	assert.Equal(t, tab2, tabs.CurrentTab())
+	assert.Equal(t, tab2, selectedTab)
+
+	tabs.OnChanged = func(tab *TabItem) {
+		assert.Fail(t, "unexpected tab selection")
+	}
+	tabs.SelectTab(NewTabItem("Test3", NewLabel("Test3")))
+	assert.Equal(t, tab2, tabs.CurrentTab())
 }
 
-func TestTabContainer_ButtonBackgroundColor(t *testing.T) {
-	r := test.WidgetRenderer(&tabButton{})
-	assert.Equal(t, theme.BackgroundColor(), r.BackgroundColor())
+func TestTabContainer_SelectTabIndex(t *testing.T) {
+	tabs := NewTabContainer(&TabItem{Text: "Test1", Content: NewLabel("Test1")},
+		&TabItem{Text: "Test2", Content: NewLabel("Test2")})
+
+	assert.Equal(t, 2, len(tabs.Items))
+	assert.Equal(t, 0, tabs.CurrentTabIndex())
+
+	var selectedTab *TabItem
+	tabs.OnChanged = func(tab *TabItem) {
+		selectedTab = tab
+	}
+	tabs.SelectTabIndex(1)
+	assert.Equal(t, 1, tabs.CurrentTabIndex())
+	assert.Equal(t, tabs.Items[1], selectedTab)
 }
 
 func TestTabContainer_SetButtonText(t *testing.T) {
@@ -480,4 +374,110 @@ func TestTabContainer_SetButtonText(t *testing.T) {
 	item.Text = "Replace"
 	tabs.Refresh()
 	assert.Equal(t, "Replace", renderer.label.Text)
+}
+
+func TestTabContainer_SetTabLocation(t *testing.T) {
+	tab1 := NewTabItem("Test1", NewLabel("Test1"))
+	tab2 := NewTabItem("Test2", NewLabel("Test2"))
+	tab3 := NewTabItem("Test3", NewLabel("Test3"))
+	tabs := NewTabContainer(tab1, tab2, tab3)
+	r := test.WidgetRenderer(tabs).(*tabContainerRenderer)
+
+	buttons := r.tabBar.Objects
+	require.Len(t, buttons, 3)
+	content := tabs.Items[0].Content
+
+	tabs.SetTabLocation(TabLocationLeading)
+	tabs.Resize(r.MinSize())
+	assert.Equal(t, fyne.NewPos(0, 0), r.tabBar.Position())
+	assert.Equal(t, fyne.NewPos(r.tabBar.MinSize().Width+theme.Padding(), 0), content.Position())
+	assert.Equal(t, fyne.NewPos(r.tabBar.MinSize().Width, 0), r.line.Position())
+	assert.Equal(t, fyne.NewSize(theme.Padding(), tabs.MinSize().Height), r.line.Size())
+	y := 0
+	for _, button := range buttons {
+		assert.Equal(t, fyne.NewPos(0, y), button.Position())
+		y += button.Size().Height
+		y += theme.Padding()
+	}
+
+	tabs.SetTabLocation(TabLocationBottom)
+	tabs.Resize(r.MinSize())
+	assert.Equal(t, fyne.NewPos(0, content.MinSize().Height+theme.Padding()), r.tabBar.Position())
+	assert.Equal(t, fyne.NewPos(0, 0), content.Position())
+	assert.Equal(t, fyne.NewPos(0, content.Size().Height), r.line.Position())
+	assert.Equal(t, fyne.NewSize(tabs.MinSize().Width, theme.Padding()), r.line.Size())
+	x := 0
+	for _, button := range buttons {
+		assert.Equal(t, fyne.NewPos(x, 0), button.Position())
+		x += button.Size().Width
+		x += theme.Padding()
+	}
+
+	tabs.SetTabLocation(TabLocationTrailing)
+	tabs.Resize(r.MinSize())
+	assert.Equal(t, fyne.NewPos(content.Size().Width+theme.Padding(), 0), r.tabBar.Position())
+	assert.Equal(t, fyne.NewPos(0, 0), content.Position())
+	assert.Equal(t, fyne.NewPos(content.Size().Width, 0), r.line.Position())
+	assert.Equal(t, fyne.NewSize(theme.Padding(), tabs.MinSize().Height), r.line.Size())
+	y = 0
+	for _, button := range buttons {
+		assert.Equal(t, fyne.NewPos(0, y), button.Position())
+		y += button.Size().Height
+		y += theme.Padding()
+	}
+
+	tabs.SetTabLocation(TabLocationTop)
+	tabs.Resize(r.MinSize())
+	assert.Equal(t, fyne.NewPos(0, 0), r.tabBar.Position())
+	assert.Equal(t, fyne.NewPos(0, r.tabBar.MinSize().Height+theme.Padding()), content.Position())
+	assert.Equal(t, fyne.NewPos(0, r.tabBar.MinSize().Height), r.line.Position())
+	assert.Equal(t, fyne.NewSize(tabs.MinSize().Width, theme.Padding()), r.line.Size())
+	x = 0
+	for _, button := range buttons {
+		assert.Equal(t, fyne.NewPos(x, 0), button.Position())
+		x += button.Size().Width
+		x += theme.Padding()
+	}
+}
+
+func TestTabContainer_Tapped(t *testing.T) {
+	tabs := NewTabContainer(
+		NewTabItem("Test1", NewLabel("Test1")),
+		NewTabItem("Test2", NewLabel("Test2")),
+		NewTabItem("Test3", NewLabel("Test3")),
+	)
+	r := test.WidgetRenderer(tabs).(*tabContainerRenderer)
+
+	tab1 := r.tabBar.Objects[0].(*tabButton)
+	tab2 := r.tabBar.Objects[1].(*tabButton)
+	tab3 := r.tabBar.Objects[2].(*tabButton)
+	require.Equal(t, 0, tabs.CurrentTabIndex())
+	require.Equal(t, theme.PrimaryColor(), test.WidgetRenderer(tab1).BackgroundColor())
+
+	tab2.Tapped(&fyne.PointEvent{})
+	assert.Equal(t, 1, tabs.CurrentTabIndex())
+	require.Equal(t, theme.BackgroundColor(), test.WidgetRenderer(tab1).BackgroundColor())
+	require.Equal(t, theme.PrimaryColor(), test.WidgetRenderer(tab2).BackgroundColor())
+	require.Equal(t, theme.BackgroundColor(), test.WidgetRenderer(tab3).BackgroundColor())
+	assert.False(t, tabs.Items[0].Content.Visible())
+	assert.True(t, tabs.Items[1].Content.Visible())
+	assert.False(t, tabs.Items[2].Content.Visible())
+
+	tab3.Tapped(&fyne.PointEvent{})
+	assert.Equal(t, 2, tabs.CurrentTabIndex())
+	require.Equal(t, theme.BackgroundColor(), test.WidgetRenderer(tab1).BackgroundColor())
+	require.Equal(t, theme.BackgroundColor(), test.WidgetRenderer(tab2).BackgroundColor())
+	require.Equal(t, theme.PrimaryColor(), test.WidgetRenderer(tab3).BackgroundColor())
+	assert.False(t, tabs.Items[0].Content.Visible())
+	assert.False(t, tabs.Items[1].Content.Visible())
+	assert.True(t, tabs.Items[2].Content.Visible())
+
+	tab1.Tapped(&fyne.PointEvent{})
+	assert.Equal(t, 0, tabs.CurrentTabIndex())
+	require.Equal(t, theme.PrimaryColor(), test.WidgetRenderer(tab1).BackgroundColor())
+	require.Equal(t, theme.BackgroundColor(), test.WidgetRenderer(tab2).BackgroundColor())
+	require.Equal(t, theme.BackgroundColor(), test.WidgetRenderer(tab3).BackgroundColor())
+	assert.True(t, tabs.Items[0].Content.Visible())
+	assert.False(t, tabs.Items[1].Content.Visible())
+	assert.False(t, tabs.Items[2].Content.Visible())
 }
