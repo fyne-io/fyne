dd104fc6b1cf799f09759f8bd02e5cfe38805534
German Laullon <laullon@gmail.com>
2020-05-19 14:50:09 +0200

    Update image.go

--- canvas/image.go
+++ canvas/image.go
@@ -50,7 +50,8 @@ type Image struct {
 
 	Translucency float64    // Set a translucency value > 0.0 to fade the image
 	FillMode     ImageFill  // Specify how the image should expand to fill or fit the available space
-	ScaleMode    ImageScale // Specify how type of scaling applied to the image
+	ScaleMode    ImageScale // Specify the type of scaling interpolation applied to the image
+
 }
 
 // Alpha is a convenience function that returns the alpha value for an image
