50f223b0f32d08c9ffc22d28fba76de5b7601571
Andy Williams <andy@andywilliams.me>
2020-05-22 18:52:56 +0100

    Also test minsize form render

--- widget/form_test.go
+++ widget/form_test.go
@@ -80,6 +80,23 @@ func TestForm_AddRemoveButton(t *testing.T) {
 	assert.Equal(t, 5, sscount, "tapping new cancel should decr ssount from 11 down to 5")
 }
 
+func TestForm_Renderer(t *testing.T) {
+	app := test.NewApp()
+	defer test.NewApp()
+	app.Settings().SetTheme(theme.LightTheme())
+
+	form := &Form{
+		Items: []*FormItem{
+			{Text: "test1", Widget: NewEntry()},
+			{Text: "test2", Widget: NewEntry()},
+		},
+		OnSubmit: func() {}, OnCancel: func() {}}
+	w := test.NewWindow(form)
+	defer w.Close()
+
+	test.AssertImageMatches(t, "form_initial.png", w.Canvas().Capture())
+}
+
 func TestForm_ChangeTheme(t *testing.T) {
 	app := test.NewApp()
 	defer test.NewApp()
