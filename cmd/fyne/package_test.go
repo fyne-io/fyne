06712271f562fb58ca1f7f977509202c36bbafdd
mrjrieke <joel@belowtheroot>
2020-06-04 07:21:30 -0700

    Adding test.

--- cmd/fyne/package_test.go
+++ cmd/fyne/package_test.go
@@ -16,3 +16,14 @@ func Test_calculateExeName(t *testing.T) {
 	nonModulesApp := calculateExeName("testdata", "linux")
 	assert.Equal(t, "testdata", nonModulesApp)
 }
+
+func Test_isMobile(t *testing.T) {
+	oslist := []string{"darwin", "linux", "windows", "android/arm", "android/386", "android", "ios"}
+	osIsMobile := []bool{false, false, false, true, true, true, true}
+	p := &packager{os: "", install: false, srcDir: ""}
+
+	for i, os := range oslist {
+		p.os = os
+		assert.Equal(t, p.isMobile(), osIsMobile[i])
+	}
+}
