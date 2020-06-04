bef3ff2d3e37b2823f837553c4375c1d2400cfdf
Andy Williams <andy@andy.xyz>
2020-06-04 16:44:52 +0100

    File...Closer -> URI...Closer

--- uri.go
+++ uri.go
@@ -5,17 +5,17 @@ import (
 	"io"
 )
 
-// FileReadCloser represents a cross platform data stream from a file or provider of data.
+// URIReadCloser represents a cross platform data stream from a file or provider of data.
 // It may refer to an item on a filesystem or data in another application that we have access to.
-type FileReadCloser interface {
+type URIReadCloser interface {
 	io.ReadCloser
 	Name() string
 	URI() URI
 }
 
-// FileWriteCloser represents a cross platform data writer for a file resource.
+// URIWriteCloser represents a cross platform data writer for a file resource.
 // This will normally refer to a local file resource.
-type FileWriteCloser interface {
+type URIWriteCloser interface {
 	io.WriteCloser
 	Name() string
 	URI() URI
