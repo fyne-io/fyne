39291aa298fbf0224220a8c0d959c4a3f85836a7
Andy Williams <andy@andy.xyz>
2020-06-10 10:29:06 +0100

    Make line numbers more readable, update whitespace text colour on theme change

--- widget/textgrid.go
+++ widget/textgrid.go
@@ -302,7 +302,7 @@ func (t *textGridRenderer) refreshGrid() {
 				x++
 			}
 			for c := 0; c < len(lineStr); c++ {
-				t.setCellRune(lineStr[c], x, TextGridStyleWhitespace, rowStyle) // line numbers
+				t.setCellRune(lineStr[c], x, TextGridStyleDefault, rowStyle) // line numbers
 				i++
 				x++
 			}
@@ -402,6 +402,7 @@ func (t *textGridRenderer) MinSize() fyne.Size {
 }
 
 func (t *textGridRenderer) Refresh() {
+	TextGridStyleWhitespace = &CustomTextGridStyle{FGColor: theme.ButtonColor()}
 	t.updateGridSize(t.text.size)
 	t.refreshGrid()
 }
