593e42b2c4ea658718a4f10306fbaa2f6cabe574
Andy Williams <andy@andy.xyz>
2020-07-23 19:07:00 +0100

    Let's not assume widgets are topleft

--- layout/maxlayout.go
+++ layout/maxlayout.go
@@ -12,8 +12,10 @@ type maxLayout struct {
 // Layout is called to pack all child objects into a specified size.
 // For MaxLayout this sets all children to the full size passed.
 func (m *maxLayout) Layout(objects []fyne.CanvasObject, size fyne.Size) {
+	topLeft := fyne.NewPos(0, 0)
 	for _, child := range objects {
 		child.Resize(size)
+		child.Move(topLeft)
 	}
 }
 
