476875dd8b936be19b802235bf0e3e358eaded63
Kevin Gentile <kgentile@protonmail.com>
2020-09-06 09:15:38 -0400

    implement RemoveShortcut

--- shortcut_test.go
+++ shortcut_test.go
@@ -15,6 +15,22 @@ func TestShortcutHandler_AddShortcut(t *testing.T) {
 	assert.Equal(t, 2, len(handle.entry))
 }
 
+func TestShortcutHandler_RemoveShortut(t *testing.T) {
+	handle := &ShortcutHandler{}
+	handle.AddShortcut(&ShortcutCopy{}, func(shortcut Shortcut) {})
+	handle.AddShortcut(&ShortcutPaste{}, func(shortcut Shortcut) {})
+
+	assert.Equal(t, 2, len(handle.entry))
+
+	handle.RemoveShortcut(&ShortcutCopy{})
+
+	assert.Equal(t, 1, len(handle.entry))
+
+	handle.RemoveShortcut(&ShortcutPaste{})
+
+	assert.Equal(t, 0, len(handle.entry))
+}
+
 func TestShortcutHandler_HandleShortcut(t *testing.T) {
 	handle := &ShortcutHandler{}
 	cutCalled, copyCalled, pasteCalled := false, false, false
