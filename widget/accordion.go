77e8f5527095f3e875a68143ec0c0c9007241ee7
Andy Williams <andy@andy.xyz>
2020-09-11 23:19:34 +0100

    Revert "Update accordion to use button style"
    This reverts commit 571a27a43eb13becec955d8b71fb441ea1586254.

--- widget/accordion.go
+++ widget/accordion.go
@@ -194,6 +194,7 @@ func (r *accordionRenderer) updateObjects() {
 		h.Alignment = ButtonAlignLeading
 		h.IconPlacement = ButtonIconLeadingText
 		h.Hidden = false
+		h.HideShadow = true
 		h.Text = ai.Title
 		index := i // capture
 		h.OnTapped = func() {
