85a4cc6fd46282ad470fd2d71a93d5873b04ca2b
okratitan <smhouston88@gmail.com>
2020-09-14 14:08:41 -0500

    If item.Size() is 0 use item.MinSize()

--- widget/list.go
+++ widget/list.go
@@ -348,8 +348,12 @@ type listItemRenderer struct {
 // MinSize calculates the minimum size of a listItem.
 // This is based on the size of the status indicator and the size of the child object.
 func (li *listItemRenderer) MinSize() (size fyne.Size) {
-	size = fyne.NewSize(li.item.child.Size().Width+theme.Padding()*2,
-		li.item.child.Size().Height+theme.Padding()*2)
+	itemSize := li.item.child.Size()
+	if itemSize.IsZero() {
+		itemSize = li.item.MinSize()
+	}
+	size = fyne.NewSize(itemSize.Width+theme.Padding()*3,
+		itemSize.Height+theme.Padding()*2)
 	return
 }
 
