7fc46bdb5a7dfe63d8eeca4cb9808f8cc073da87
Andy Williams <andy@andywilliams.me>
2020-09-15 10:25:45 +0100

    Revert accidental command changes from this PR

--- cmd/fyne/install.go
+++ cmd/fyne/install.go
@@ -7,7 +7,6 @@ import (
 	"os"
 	"path/filepath"
 	"runtime"
-	"strings"
 
 	"fyne.io/fyne"
 )
@@ -16,14 +15,13 @@ import (
 var _ command = (*installer)(nil)
 
 type installer struct {
-	installDir, srcDir, icon, os string
-	packager                     *packager
+	installDir, srcDir, icon string
+	packager                 *packager
 }
 
 func (i *installer) addFlags() {
 	flag.StringVar(&i.installDir, "installDir", "", "A specific location to install to, rather than the OS default")
 	flag.StringVar(&i.icon, "icon", "Icon.png", "The name of the application icon file")
-	flag.StringVar(&i.os, "os", "", "The operating system to target (android, android/arm, android/arm64, android/amd64, android/386, darwin, ios, linux, windows)")
 }
 
 func (i *installer) printHelp(indent string) {
@@ -33,63 +31,34 @@ func (i *installer) printHelp(indent string) {
 }
 
 func (i *installer) validate() error {
-	if i.os == "" {
-		i.os = runtime.GOOS
-	}
-
 	i.packager = &packager{os: runtime.GOOS, install: true, srcDir: i.srcDir}
 	i.packager.icon = i.icon
-	i.packager.os = i.os
 	return i.packager.validate()
 }
 
 func (i *installer) install() error {
 	p := i.packager
 
-	switch i.os {
-	}
-
-	switch i.os {
-	case "darwin":
-		if i.installDir == "" {
+	if i.installDir == "" {
+		switch runtime.GOOS {
+		case "darwin":
 			i.installDir = "/Applications"
-		}
-	case "linux":
-		if i.installDir == "" {
+		case "linux":
 			i.installDir = "/" // the tarball contains the structure starting at usr/local
-		}
-	case "windows":
-		if i.installDir == "" {
+		case "windows":
 			i.installDir = filepath.Join(os.Getenv("ProgramFiles"), p.name)
+			err := runAsAdminWindows("mkdir", "\"\""+filepath.Join(os.Getenv("ProgramFiles"), p.name)+"\"\"")
+			if err != nil {
+				fyne.LogError("Failed to run as windows administrator", err)
+				return err
+			}
+		default:
+			return errors.New("Unsupported target operating system \"" + p.os + "\"")
 		}
-		err := runAsAdminWindows("mkdir", "\"\""+i.installDir+"\"\"")
-		if err != nil {
-			fyne.LogError("Failed to run as windows administrator", err)
-			return err
-		}
-	case "android/arm", "android/arm64", "android/amd64", "android/386", "android", "ios":
-	default:
-		return errors.New("Unsupported target operating system \"" + p.os + "\"")
 	}
 
 	p.dir = i.installDir
-	err := p.doPackage()
-	if err == nil {
-		if strings.Index(i.os, "android") == 0 {
-			err = i.installAndroid()
-		} else if i.os == "ios" {
-			err = i.installIOS()
-		}
-	}
-	return err
-}
-
-func (i *installer) installAndroid() error {
-	return nil
-}
-
-func (i *installer) installIOS() error {
-	return nil
+	return p.doPackage()
 }
 
 func (i *installer) run(args []string) {
