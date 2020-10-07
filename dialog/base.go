aa280252ef7e112783953d6ab52f7513cc35fbaa
okratitan <smhouston88@gmail.com>
2020-10-07 11:05:17 -0500

    Increase horizontal padding and update test images

--- dialog/base.go
+++ dialog/base.go
@@ -120,8 +120,8 @@ func (d *dialog) Layout(obj []fyne.CanvasObject, size fyne.Size) {
 	obj[0].Move(fyne.NewPos(size.Width-iconHeight+theme.Padding(), -theme.Padding()))
 
 	// content (text)
-	obj[2].Move(fyne.NewPos(0, d.label.MinSize().Height+theme.Padding()))
-	obj[2].Resize(fyne.NewSize(d.bg.Size().Width, d.bg.Size().Height-(theme.Padding()+btnMin.Height)))
+	obj[2].Move(fyne.NewPos(theme.Padding(), d.label.MinSize().Height+theme.Padding()))
+	obj[2].Resize(fyne.NewSize(d.bg.Size().Width-(theme.Padding()*4), d.bg.Size().Height-(theme.Padding()+btnMin.Height)))
 
 	// buttons
 	obj[3].Resize(btnMin)
