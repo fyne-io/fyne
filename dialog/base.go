a7b74d902c908ca8665f294b172ca5b7f2e33198
Stephen M Houston <smhouston88@gmail.com>
2020-10-07 16:59:02 -0500

    Fix dialog content to expand instead of use min size. (#1367)
    * Fix dialog content to expand instead of use min size.
    
    * Dialog already provides horizontal padding on its edges.
    
    * Increase horizontal padding and update test images
    
    * Update to use const width padding for horizontal padding
    
    * Center the text in the label in the base test
    
    * Make content sizing code read clearer
    
    * Remove extra padding and improve code readability for sizing
    
    * Use padHeight instead of theme.Padding() between label and content
    
    * Update color dialog images to work with dialog changes
    
    * I think this file slipped in by accident. Not sure what it is.

--- dialog/base.go
+++ dialog/base.go
@@ -108,10 +108,9 @@ func (d *dialog) Show() {
 }
 
 func (d *dialog) Layout(obj []fyne.CanvasObject, size fyne.Size) {
-	d.bg.Move(fyne.NewPos(-theme.Padding(), -theme.Padding()))
-	d.bg.Resize(size.Add(fyne.NewSize(theme.Padding()*2, theme.Padding()*2)))
+	d.bg.Move(fyne.NewPos(0, 0))
+	d.bg.Resize(size)
 
-	textMin := obj[2].MinSize()
 	btnMin := obj[3].MinSize().Union(obj[3].Size())
 
 	// icon
@@ -119,21 +118,23 @@ func (d *dialog) Layout(obj []fyne.CanvasObject, size fyne.Size) {
 	obj[0].Resize(fyne.NewSize(iconHeight, iconHeight))
 	obj[0].Move(fyne.NewPos(size.Width-iconHeight+theme.Padding(), -theme.Padding()))
 
-	// content (text)
-	obj[2].Move(fyne.NewPos(size.Width/2-(textMin.Width/2), size.Height-padHeight-btnMin.Height-textMin.Height-theme.Padding()))
-	obj[2].Resize(fyne.NewSize(textMin.Width, textMin.Height))
-
 	// buttons
 	obj[3].Resize(btnMin)
 	obj[3].Move(fyne.NewPos(size.Width/2-(btnMin.Width/2), size.Height-padHeight-btnMin.Height))
+
+	// content
+	contentStart := d.label.Position().Y + d.label.MinSize().Height + padHeight
+	contentEnd := obj[3].Position().Y - theme.Padding()
+	obj[2].Move(fyne.NewPos((padWidth / 2), d.label.MinSize().Height+padHeight))
+	obj[2].Resize(fyne.NewSize(size.Width-padWidth, contentEnd-contentStart))
 }
 
 func (d *dialog) MinSize(obj []fyne.CanvasObject) fyne.Size {
-	textMin := obj[2].MinSize()
+	contentMin := obj[2].MinSize()
 	btnMin := obj[3].MinSize().Union(obj[3].Size())
 
-	width := fyne.Max(fyne.Max(textMin.Width, btnMin.Width), obj[4].MinSize().Width) + padWidth*2
-	height := textMin.Height + btnMin.Height + d.label.MinSize().Height + theme.Padding() + padHeight*2
+	width := fyne.Max(fyne.Max(contentMin.Width, btnMin.Width), obj[4].MinSize().Width) + padWidth
+	height := contentMin.Height + btnMin.Height + d.label.MinSize().Height + theme.Padding() + padHeight*2
 
 	return fyne.NewSize(width, height)
 }
