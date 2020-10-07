59efd9c0745c1a26d9a2ab32924c75f196bcc0ed
okratitan <smhouston88@gmail.com>
2020-10-07 11:22:36 -0500

    Update to use const width padding for horizontal padding

--- dialog/base.go
+++ dialog/base.go
@@ -120,8 +120,8 @@ func (d *dialog) Layout(obj []fyne.CanvasObject, size fyne.Size) {
 	obj[0].Move(fyne.NewPos(size.Width-iconHeight+theme.Padding(), -theme.Padding()))
 
 	// content (text)
-	obj[2].Move(fyne.NewPos(theme.Padding(), d.label.MinSize().Height+theme.Padding()))
-	obj[2].Resize(fyne.NewSize(d.bg.Size().Width-(theme.Padding()*4), d.bg.Size().Height-(theme.Padding()+btnMin.Height)))
+	obj[2].Move(fyne.NewPos((padWidth/2)-theme.Padding(), d.label.MinSize().Height+theme.Padding()))
+	obj[2].Resize(fyne.NewSize(d.bg.Size().Width-padWidth, d.bg.Size().Height-(theme.Padding()+btnMin.Height)))
 
 	// buttons
 	obj[3].Resize(btnMin)
