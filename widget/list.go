6eae0528b7e7eca0956d4c1663e8ab3f048d55c2
Stephen M Houston <smhouston88@gmail.com>
2020-10-08 16:46:20 -0500

    List length fix (#1373)
    * Ensure safety around List() function
    
    * Add test for a list with no functions set.
    
    * Cache length to avoid multiple func calls
    
    * Save a line of code

--- widget/list.go
+++ widget/list.go
@@ -91,7 +91,11 @@ func newListRenderer(objects []fyne.CanvasObject, l *List, scroller *ScrollConta
 }
 
 func (l *listRenderer) Layout(size fyne.Size) {
-	if l.list.Length() == 0 {
+	length := 0
+	if f := l.list.Length; f != nil {
+		length = f()
+	}
+	if length == 0 {
 		if len(l.children) > 0 {
 			for _, child := range l.children {
 				l.itemPool.Release(child)
@@ -126,14 +130,14 @@ func (l *listRenderer) Layout(size fyne.Size) {
 	if l.visibleItemCount == 0 {
 		return
 	}
-	min := int(math.Min(float64(l.list.Length()), float64(l.visibleItemCount)))
+	min := int(math.Min(float64(length), float64(l.visibleItemCount)))
 	if len(l.children) > min {
 		for i := len(l.children); i >= min; i-- {
 			l.itemPool.Release(l.children[i-1])
 		}
 		l.children = l.children[:min-1]
 	}
-	for i := len(l.children) + l.firstItemIndex; len(l.children) <= l.visibleItemCount && i < l.list.Length(); i++ {
+	for i := len(l.children) + l.firstItemIndex; len(l.children) <= l.visibleItemCount && i < length; i++ {
 		l.appendItem(i)
 	}
 	l.layout.Objects = l.children
@@ -215,7 +219,14 @@ func (l *listRenderer) scrollDown(offsetChange int) {
 		itemChange = int(math.Floor(float64(offsetChange) / float64(l.list.itemMin.Height)))
 	}
 	l.previousOffsetY = l.list.offsetY
-	for i := 0; i < itemChange && l.lastItemIndex != l.list.Length()-1; i++ {
+	length := 0
+	if f := l.list.Length; f != nil {
+		length = f()
+	}
+	if length == 0 {
+		return
+	}
+	for i := 0; i < itemChange && l.lastItemIndex != length-1; i++ {
 		l.itemPool.Release(l.children[0])
 		l.children = l.children[1:]
 		l.firstItemIndex++
@@ -412,8 +423,11 @@ func (l *listLayout) Layout(objects []fyne.CanvasObject, size fyne.Size) {
 }
 
 func (l *listLayout) MinSize(objects []fyne.CanvasObject) fyne.Size {
-	return fyne.NewSize(l.list.itemMin.Width,
-		l.list.itemMin.Height*l.list.Length())
+	if f := l.list.Length; f != nil {
+		return fyne.NewSize(l.list.itemMin.Width,
+			l.list.itemMin.Height*f())
+	}
+	return fyne.NewSize(0, 0)
 }
 
 func (l *listLayout) appendedItem(objects []fyne.CanvasObject) {
