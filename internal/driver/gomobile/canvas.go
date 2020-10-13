16e40b204147dc02ce27ffcb2afa3c1a15ce1a98
Andy Williams <andy@andywilliams.me>
2020-10-13 21:01:31 +0100

    Fix bad merge

--- internal/driver/gomobile/canvas.go
+++ internal/driver/gomobile/canvas.go
@@ -123,10 +123,10 @@ func (c *mobileCanvas) sizeContent(size fyne.Size) {
 		if p, ok := overlay.(*widget.PopUp); ok {
 			// TODO: remove this when #707 is being addressed.
 			// “Notifies” the PopUp of the canvas size change.
-			size := p.Content.Size().Add(fyne.NewSize(theme.Padding()*2, theme.Padding()*2)).Min(innerSize)
+			size := p.Content.Size().Add(fyne.NewSize(theme.Padding()*2, theme.Padding()*2)).Min(areaSize)
 			p.Resize(size)
 		} else {
-			overlay.Resize(innerSize)
+			overlay.Resize(areaSize)
 			overlay.Move(topLeft)
 		}
 	}
