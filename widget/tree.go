25af4038f520f953478f1001ffa77952ff3b360a
Stuart Scott <stuart.murray.scott@gmail.com>
2020-10-24 11:45:35 -0700

    Minor Tree Field Organization (#1459)

--- widget/tree.go
+++ widget/tree.go
@@ -20,22 +20,22 @@ var _ fyne.Widget = (*Tree)(nil)
 // Each node of the tree must be identified by a Unique ID.
 type Tree struct {
 	BaseWidget
-	Root     string
-	selected string
-	Offset   fyne.Position
+	Root string
 
 	ChildUIDs          func(uid string) (c []string)                         // Return a sorted slice of Children Unique IDs for the given Node Unique ID
-	IsBranch           func(uid string) (ok bool)                            // Return true if the given Unique ID represents a Branch
 	CreateNode         func(branch bool) (o fyne.CanvasObject)               // Return a CanvasObject that can represent a Branch (if branch is true), or a Leaf (if branch is false)
-	UpdateNode         func(uid string, branch bool, node fyne.CanvasObject) // Called to update the given CanvasObject to represent the data at the given Unique ID
-	OnBranchOpened     func(uid string)                                      // Called when a Branch is opened
+	IsBranch           func(uid string) (ok bool)                            // Return true if the given Unique ID represents a Branch
 	OnBranchClosed     func(uid string)                                      // Called when a Branch is closed
+	OnBranchOpened     func(uid string)                                      // Called when a Branch is opened
 	OnSelectionChanged func(uid string)                                      // Called when the Node with the given Unique ID is selected.
+	UpdateNode         func(uid string, branch bool, node fyne.CanvasObject) // Called to update the given CanvasObject to represent the data at the given Unique ID
 
-	scroller      *ScrollContainer
-	open          map[string]bool
 	branchMinSize fyne.Size
 	leafMinSize   fyne.Size
+	offset        fyne.Position
+	open          map[string]bool
+	scroller      *ScrollContainer
+	selected      string
 }
 
 // NewTree returns a new performant tree widget defined by the passed functions.
@@ -110,10 +110,10 @@ func (t *Tree) CreateRenderer() fyne.WidgetRenderer {
 		scroller:     s,
 	}
 	s.onOffsetChanged = func() {
-		if t.Offset == s.Offset {
+		if t.offset == s.Offset {
 			return
 		}
-		t.Offset = s.Offset
+		t.offset = s.Offset
 		c.Refresh()
 	}
 	r.updateMinSizes()
@@ -370,7 +370,7 @@ func (r *treeContentRenderer) Layout(size fyne.Size) {
 	branches := make(map[string]*branch)
 	leaves := make(map[string]*leaf)
 
-	offsetY := r.treeContent.tree.Offset.Y
+	offsetY := r.treeContent.tree.offset.Y
 	viewport := r.treeContent.viewport
 	width := fyne.Max(size.Width, viewport.Width)
 	y := 0
