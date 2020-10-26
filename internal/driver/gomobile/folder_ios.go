58127becaa4b68643a4d964aab4534978a3a8bb1
Andy Williams <andy@andywilliams.me>
2020-10-26 16:04:50 +0000

    Adding iOS ListableURI support YES

--- internal/driver/gomobile/folder_ios.go
+++ internal/driver/gomobile/folder_ios.go
@@ -2,12 +2,41 @@
 
 package gomobile
 
-import "fyne.io/fyne"
+/*
+#cgo CFLAGS: -x objective-c
+#cgo LDFLAGS: -framework Foundation
 
-func uriCanList(fyne.URI) bool {
-	return false
+#import <stdlib.h>
+#import <stdbool.h>
+
+bool iosCanList(const char* url);
+char* iosList(const char* url);
+*/
+import "C"
+import (
+	"strings"
+	"unsafe"
+
+	"fyne.io/fyne"
+	"fyne.io/fyne/storage"
+)
+
+func uriCanList(uri fyne.URI) bool {
+	uriStr := C.CString(uri.String())
+	defer C.free(unsafe.Pointer(uriStr))
+
+	return bool(C.iosCanList(uriStr))
 }
 
-func uriList(fyne.URI) ([]fyne.URI, error) {
-	return nil, nil
+func uriList(uri fyne.URI) ([]fyne.URI, error) {
+	uriStr := C.CString(uri.String())
+	defer C.free(unsafe.Pointer(uriStr))
+
+	str := C.iosList(uriStr)
+	parts := strings.Split(C.GoString(str), "|")
+	var list []fyne.URI
+	for _, part := range parts {
+		list = append(list, storage.NewURI(part))
+	}
+	return list, nil
 }
