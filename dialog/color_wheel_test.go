da2dd9bdc594120ea73fd238ae0821cbdda52ea7
Tilo Prütz <tilo@pruetz.net>
2020-11-01 13:11:27 +0100

    add markup expectation to ColorWheel test

--- dialog/color_wheel_test.go
+++ dialog/color_wheel_test.go
@@ -19,6 +19,17 @@ func Test_colorWheel_Layout(t *testing.T) {
 	window.Resize(wheel.MinSize().Max(fyne.NewSize(100, 100)))
 
 	test.AssertImageMatches(t, "color/wheel_layout.png", window.Canvas().Capture())
+	test.AssertRendersToMarkup(t, `
+		<canvas padded size="128x128">
+			<content>
+				<widget pos="4,4" size="120x120" type="*dialog.colorWheel">
+					<raster size="120x120"/>
+					<line pos="0,60" size="120x0" strokeColor="text"/>
+					<line size="0x120" strokeColor="text"/>
+				</widget>
+			</content>
+		</canvas>
+	`, window.Canvas())
 
 	window.Close()
 }
