73ada3f524e9ea9ed03d31f20c863f96aaba4612
Tilo Prütz <tilo@pruetz.net>
2020-11-05 21:38:53 +0100

    change check test to markup based

--- widget/check_test.go
+++ widget/check_test.go
@@ -19,22 +19,75 @@ func TestCheck_Layout(t *testing.T) {
 		text     string
 		checked  bool
 		disabled bool
+		want     string
 	}{
 		"checked": {
 			text:    "Test",
 			checked: true,
+			want: `
+				<canvas padded size="150x200">
+					<content>
+						<container pos="4,4" size="142x192">
+							<widget pos="36,81" size="70x29" type="*widget.Check">
+								<circle fillColor="background" size="28x28"/>
+								<image pos="4,4" rsc="checkButtonCheckedIcon" size="iconInlineSize"/>
+								<text pos="32,0" size="42x29">Test</text>
+							</widget>
+						</container>
+					</content>
+				</canvas>
+			`,
 		},
 		"unchecked": {
 			text: "Test",
+			want: `
+				<canvas padded size="150x200">
+					<content>
+						<container pos="4,4" size="142x192">
+							<widget pos="36,81" size="70x29" type="*widget.Check">
+								<circle fillColor="background" size="28x28"/>
+								<image pos="4,4" rsc="checkButtonIcon" size="iconInlineSize"/>
+								<text pos="32,0" size="42x29">Test</text>
+							</widget>
+						</container>
+					</content>
+				</canvas>
+			`,
 		},
 		"checked_disabled": {
 			text:     "Test",
 			checked:  true,
 			disabled: true,
+			want: `
+				<canvas padded size="150x200">
+					<content>
+						<container pos="4,4" size="142x192">
+							<widget pos="36,81" size="70x29" type="*widget.Check">
+								<circle fillColor="background" size="28x28"/>
+								<image pos="4,4" rsc="checkButtonCheckedIcon" size="iconInlineSize" themed="disabled"/>
+								<text color="disabled text" pos="32,0" size="42x29">Test</text>
+							</widget>
+						</container>
+					</content>
+				</canvas>
+			`,
 		},
 		"unchecked_disabled": {
 			text:     "Test",
 			disabled: true,
+			want: `
+				<canvas padded size="150x200">
+					<content>
+						<container pos="4,4" size="142x192">
+							<widget pos="36,81" size="70x29" type="*widget.Check">
+								<circle fillColor="background" size="28x28"/>
+								<image pos="4,4" rsc="checkButtonIcon" size="iconInlineSize" themed="disabled"/>
+								<text color="disabled text" pos="32,0" size="42x29">Test</text>
+							</widget>
+						</container>
+					</content>
+				</canvas>
+			`,
 		},
 	} {
 		t.Run(name, func(t *testing.T) {
@@ -49,7 +102,7 @@ func TestCheck_Layout(t *testing.T) {
 			window := test.NewWindow(fyne.NewContainerWithLayout(layout.NewCenterLayout(), check))
 			window.Resize(check.MinSize().Max(fyne.NewSize(150, 200)))
 
-			test.AssertImageMatches(t, "check/layout_"+name+".png", window.Canvas().Capture())
+			test.AssertRendersToMarkup(t, tt.want, window.Canvas())
 
 			window.Close()
 		})
