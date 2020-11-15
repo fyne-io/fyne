0064000bcc1e76e59e50df64612b922f7ac6fdda
Tilo Prütz <tilo@pruetz.net>
2020-11-15 08:39:47 +0100

    OverlaysStack.ListFocusManagers() returns the stack’s focus managers

--- internal/overlay_stack_test.go
+++ internal/overlay_stack_test.go
@@ -7,6 +7,7 @@ import (
 
 	"fyne.io/fyne"
 	"fyne.io/fyne/internal"
+	"fyne.io/fyne/internal/app"
 	"fyne.io/fyne/test"
 	"fyne.io/fyne/widget"
 )
@@ -23,6 +24,7 @@ func TestOverlayStack(t *testing.T) {
 	assert.Empty(t, s.List())
 	assert.Nil(t, s.Top())
 	assert.Nil(t, s.TopFocusManager())
+	assert.Empty(t, s.ListFocusManagers())
 
 	// add one & remove
 	s.Add(o1)
@@ -30,50 +32,61 @@ func TestOverlayStack(t *testing.T) {
 	assert.Equal(t, o1, s.Top())
 	fm := s.TopFocusManager()
 	assert.NotNil(t, fm)
+	assert.Equal(t, []*app.FocusManager{fm}, s.ListFocusManagers())
 	// remove other does nothing
 	s.Remove(o2)
 	assert.Equal(t, []fyne.CanvasObject{o1}, s.List())
 	assert.Equal(t, o1, s.Top())
 	assert.Equal(t, fm, s.TopFocusManager())
+	assert.Equal(t, []*app.FocusManager{fm}, s.ListFocusManagers())
 	// remove the correct one
 	s.Remove(o1)
 	assert.Empty(t, s.List())
 	assert.Nil(t, s.Top())
 	assert.Nil(t, s.TopFocusManager())
+	assert.Empty(t, s.ListFocusManagers())
 
 	// add multiple & remove
 	s.Add(o1)
 	fm1 := s.TopFocusManager()
 	assert.NotNil(t, fm1)
+	assert.Equal(t, []*app.FocusManager{fm1}, s.ListFocusManagers())
 	s.Add(o2)
 	fm2 := s.TopFocusManager()
 	assert.NotNil(t, fm2)
 	assert.NotEqual(t, fm1, fm2)
+	assert.Equal(t, []*app.FocusManager{fm1, fm2}, s.ListFocusManagers())
 	s.Add(o3)
 	fm3 := s.TopFocusManager()
 	assert.NotNil(t, fm3)
 	assert.NotEqual(t, fm2, fm3)
+	assert.Equal(t, []*app.FocusManager{fm1, fm2, fm3}, s.ListFocusManagers())
 	s.Add(o4)
 	fm4 := s.TopFocusManager()
 	assert.NotNil(t, fm4)
 	assert.NotEqual(t, fm3, fm4)
+	assert.Equal(t, []*app.FocusManager{fm1, fm2, fm3, fm4}, s.ListFocusManagers())
 	s.Add(o5)
 	assert.Equal(t, []fyne.CanvasObject{o1, o2, o3, o4, o5}, s.List())
 	assert.Equal(t, o5, s.Top())
 	fm5 := s.TopFocusManager()
 	assert.NotNil(t, fm5)
 	assert.NotEqual(t, fm4, fm5)
+	assert.Equal(t, []*app.FocusManager{fm1, fm2, fm3, fm4, fm5}, s.ListFocusManagers())
 	s.Remove(o5)
 	assert.Equal(t, []fyne.CanvasObject{o1, o2, o3, o4}, s.List())
 	assert.Equal(t, o4, s.Top())
 	assert.Equal(t, fm4, s.TopFocusManager())
+	assert.Equal(t, []*app.FocusManager{fm1, fm2, fm3, fm4}, s.ListFocusManagers())
 	// remove cuts the stack
 	s.Remove(o2)
 	assert.Equal(t, []fyne.CanvasObject{o1}, s.List())
 	assert.Equal(t, o1, s.Top())
 	assert.Equal(t, fm1, s.TopFocusManager())
+	assert.Equal(t, []*app.FocusManager{fm1}, s.ListFocusManagers())
 	s.Remove(o1)
 	assert.Empty(t, s.List())
 	assert.Nil(t, s.Top())
 	assert.Nil(t, s.TopFocusManager())
+	assert.Empty(t, s.ListFocusManagers())
 }
