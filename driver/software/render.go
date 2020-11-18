65cd055506192b97f743b77904cdfb9125536bed
Andy Williams <andy@andywilliams.me>
2020-11-18 14:14:00 +0000

    Add a software driver to underpin playground and embedded
    For anyone that wants to use Fyne but render to image instead of window

---
+++ driver/software/render.go
@@ -0,0 +1,30 @@
+package software
+
+import (
+	"image"
+
+	"fyne.io/fyne"
+	"fyne.io/fyne/internal/app"
+)
+
+// RenderCanvas takes a canvas and renders it to a regular Go image using the provided Theme.
+// This is the same as setting the application theme and then calling Canvas.Capture().
+func RenderCanvas(c fyne.Canvas, t fyne.Theme) image.Image {
+	fyne.CurrentApp().Settings().SetTheme(t)
+	app.ApplyThemeTo(c.Content(), c)
+
+	return c.Capture()
+}
+
+// Render takes a canvas object and renders it to a regular Go image using the provided Theme.
+// The returned image will be set to the object's minimum size.
+// Use the theme.LightTheme() or theme.DarkTheme() to access the builtin themes.
+func Render(obj fyne.CanvasObject, t fyne.Theme) image.Image {
+	c := NewCanvas()
+	c.SetPadded(false)
+	c.SetContent(obj)
+
+	fyne.CurrentApp().Settings().SetTheme(t)
+	app.ApplyThemeTo(obj, c)
+	return c.Capture()
+}
