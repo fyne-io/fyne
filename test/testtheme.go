70548758973d36a53444578e6b3b3bb9b64dee59
Andy Williams <andy@andywilliams.me>
2020-11-26 14:29:37 +0000

    Remove second test-defined theme type

--- test/testtheme.go
+++ test/testtheme.go
@@ -13,65 +13,29 @@ var (
 	blue  = &color.RGBA{R: 0, G: 0, B: 255, A: 255}
 )
 
-const testTextSize = 18
-
-var _ fyne.Theme = testTheme{}
-
-// testTheme is a simple theme variation used for testing that widgets adapt correctly
-type testTheme struct {
-}
-
 // NewTheme returns a new testTheme.
 func NewTheme() fyne.Theme {
-	return &testTheme{}
-}
-
-func (testTheme) Color(c fyne.ThemeColorName, _ fyne.ThemeVariant) color.Color {
-	switch c {
-	case theme.Colors.Background:
-		return red
-	case theme.Colors.Button, theme.Colors.DisabledText:
-		return color.Black
-	case theme.Colors.PlaceHolder, theme.Colors.ScrollBar, theme.Colors.Shadow:
-		return blue
-	case theme.Colors.Primary, theme.Colors.Hover, theme.Colors.Focus:
-		return green
-	default:
-		return color.White
+	return &configurableTheme{
+		background:         red,
+		bold:               theme.DefaultTextItalicFont(),
+		boldItalic:         theme.DefaultTextMonospaceFont(),
+		button:             color.Black,
+		disabledButton:     color.White,
+		disabledText:       color.Black,
+		focus:              green,
+		hover:              green,
+		iconInlineSize:     24,
+		italic:             theme.DefaultTextBoldItalicFont(),
+		monospace:          theme.DefaultTextFont(),
+		padding:            10,
+		placeholder:        blue,
+		primary:            green,
+		regular:            theme.DefaultTextBoldFont(),
+		scrollBar:          blue,
+		scrollBarSize:      10,
+		scrollBarSmallSize: 2,
+		shadow:             blue,
+		text:               color.White,
+		textSize:           18,
 	}
-}
-
-func (testTheme) Size(s fyne.ThemeSizeName) int {
-	switch s {
-	case theme.Sizes.Padding:
-		return 10
-	case theme.Sizes.InlineIcon:
-		return 24
-	case theme.Sizes.ScrollBar:
-		return 10
-	case theme.Sizes.ScrollBarSmall:
-		return 2
-	case theme.Sizes.Text:
-		return testTextSize
-	default:
-		return 0
-	}
-}
-
-func (testTheme) Font(style fyne.TextStyle) fyne.Resource {
-	if style.Monospace {
-		return theme.DefaultTextFont()
-	}
-
-	if style.Bold {
-		if style.Italic {
-			return theme.DefaultTextMonospaceFont()
-		}
-		return theme.DefaultTextItalicFont()
-	}
-	if style.Italic {
-		return theme.DefaultTextBoldItalicFont()
-	}
-
-	return theme.DefaultTextBoldFont()
 }
