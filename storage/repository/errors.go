3c08c793dbaf11dffd55ec7ca500afbee1e47304
Charles Daniels <charles@cdaniels.net>
2021-01-12 17:14:07 -0500

    move URIRootError to repository

--- storage/repository/errors.go
+++ storage/repository/errors.go
@@ -15,3 +15,18 @@ const OperationNotSupportedError operationNotSupportedError = operationNotSuppor
 func (e operationNotSupportedError) Error() string {
 	return string(OperationNotSupportedError)
 }
+
+// Declare conformance with Error interface
+var _ error = URIRootError
+
+type uriRootError string
+
+// URIRootError should be thrown by fyne.URI implementations when the caller
+// attempts to take the parent of the root. This way, downstream code that
+// wants to programmatically walk up a URIs parent's will know when to stop
+// iterating.
+const URIRootError uriRootError = uriRootError("Cannot take the parent of the root element in a URI")
+
+func (e uriRootError) Error() string {
+	return string(URIRootError)
+}
