a4aa5511302da040b88069e18126780b65ef4748
Charles Daniels <charles@cdaniels.net>
2021-01-17 18:23:42 -0500

    add type assertions to gomobile/repository.go

--- internal/driver/gomobile/repository.go
+++ internal/driver/gomobile/repository.go
@@ -6,6 +6,16 @@ import (
 	"fyne.io/fyne"
 )
 
+// declare conformance with repository types
+var _ repository.Repository = (*mobileFileRepo)(nil)
+var _ repository.HierarchicalRepository = (*mobileFileRepo)(nil)
+var _ repository.CopyableRepository = (*mobileFileRepo)(nil)
+var _ repository.MovableRepository = (*mobileFileRepo)(nil)
+var _ repository.ListableRepository = (*mobileFileRepo)(nil)
+
+// TODO add write support (not yet supported on mobile)
+// var _ repository.WriteableRepository = (*mobileFileRepo)(nil)
+
 type mobileFileRepo struct {
 	driver *mobileDriver
 }
@@ -33,4 +43,7 @@ func (m *mobileFileRepo) List(u fyne.URI) ([]fyne.URI, error) {
 	return listURI(u)
 }
 
-// TODO add write support (not yet supported on mobile)
+func (m *mobileFileRepo) CreateListable(u fyne.URI) error {
+	// TODO: implement this
+	return nil
+}
