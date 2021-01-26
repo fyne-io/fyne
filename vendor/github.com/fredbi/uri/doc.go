bbb2a6805f5b2d675672fc6b5be5ea9b5b05a821
Andy Williams <andy@andywilliams.me>
2021-01-26 22:21:03 +0000

    Fix vendoring from release
    Fixes #1862

---
+++ vendor/github.com/fredbi/uri/doc.go
@@ -0,0 +1,13 @@
+// Package uri is meant to be an RFC 3986 compliant URI builder and parser.
+//
+// This is based on the work from ttacon/uri (credits: Trey Tacon).
+//
+// This fork concentrates on RFC 3986 strictness for URI parsing and validation.
+//
+// Reference: https://tools.ietf.org/html/rfc3986
+//
+// Tests have been augmented with test suites of URI validators in other languages:
+// perl, python, scala, .Net.
+//
+// Extra features like MySQL URIs present in the original repo have been removed.
+package uri
