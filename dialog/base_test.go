21ca8bc3f1253ff10c9c6b3e02e9d5cd7609859b
Andy Williams <andy@andywilliams.me>
2021-01-27 16:09:07 +0000

    Missed Resize test

--- dialog/base_test.go
+++ dialog/base_test.go
@@ -4,9 +4,12 @@ import (
 	"image/color"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/test"
+	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
 )
 
@@ -27,4 +30,18 @@ func TestShowCustom_ApplyTheme(t *testing.T) {
 
 	test.ApplyTheme(t, test.NewTheme())
 	test.AssertImageMatches(t, "dialog-custom-ugly.png", w.Canvas().Capture())
+}
+
+func TestShowCustom_Resize(t *testing.T) {
+	w := test.NewWindow(canvas.NewRectangle(color.Transparent))
+	w.Resize(fyne.NewSize(300, 300))
+
+	label := widget.NewLabel("Content")
+	label.Alignment = fyne.TextAlignCenter
+	d := NewCustom("Title", "OK", label, w)
+
+	size := fyne.NewSize(200, 200)
+	d.Resize(size)
+	d.Show()
+	assert.Equal(t, size, d.(*dialog).win.Content.Size().Add(fyne.NewSize(theme.Padding()*2, theme.Padding()*2)))
 }
