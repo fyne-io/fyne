e444f20653557dd92636286a1c2815b416b03420
Jacalz <jacob.alzen@gmail.com>
2021-02-03 19:38:49 +0100

    Only use one of the icons for now

--- cmd/fyne_settings/data/data.go
+++ cmd/fyne_settings/data/data.go
@@ -1,3 +1,4 @@
 package data
 
-//go:generate fyne bundle -package settings -o ../settings/bundled.go assets
+// TODO: Bundle the folder instead of the file when we have language support.
+//go:generate fyne bundle -package settings -o ../settings/bundled.go assets/appearance.svg
