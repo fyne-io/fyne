d45612046d03b68261c697dea1f8cb23329dde0a
Jacalz <jacob.alzen@gmail.com>
2021-02-03 18:41:01 +0100

    Better deprecation text

--- cmd/fyne/commands/command.go
+++ cmd/fyne/commands/command.go
@@ -5,7 +5,8 @@ import "fyne.io/fyne/v2/cmd/fyne/internal/commands"
 
 // Command defines the required functionality to provide a subcommand to the "fyne" tool.
 //
-// Deprecated: Use the functions that return urfave/cli commands instead.
+// Deprecated: Access to the internal cli commands are being removed.
+// Better versions of bundler, installer, packager and releaser will be avaliable in the future.
 type Command interface {
 	AddFlags()
 	PrintHelp(string)
