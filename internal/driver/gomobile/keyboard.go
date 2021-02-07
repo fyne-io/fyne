e0d721114ae55f8bbc3db441c353c89c5b135170
Andy Williams <andy@andywilliams.me>
2021-02-07 12:29:30 +0000

    Don't crash if keyboard controls are invoked too soon
    Fixes #1896

--- internal/driver/gomobile/keyboard.go
+++ internal/driver/gomobile/keyboard.go
@@ -8,12 +8,21 @@ import (
 
 func showVirtualKeyboard(keyboard mobile.KeyboardType) {
 	if driver, ok := fyne.CurrentApp().Driver().(*mobileDriver); ok {
+		if driver.app == nil { // not yet running
+			fyne.LogError("Cannot show keyboard before app is running", nil)
+			return
+		}
+
 		driver.app.ShowVirtualKeyboard(app.KeyboardType(keyboard))
 	}
 }
 
 func hideVirtualKeyboard() {
 	if driver, ok := fyne.CurrentApp().Driver().(*mobileDriver); ok {
+		if driver.app == nil { // not yet running
+			return
+		}
+
 		driver.app.HideVirtualKeyboard()
 	}
 }
