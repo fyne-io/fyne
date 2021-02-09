6a0f967529a418f5bca87122ed5b3f06a554469b
Stuart Scott <stuart.murray.scott@gmail.com>
2021-02-09 10:53:39 -0800

    Support AppTabs.Items until next major

--- container/apptabs.go
+++ container/apptabs.go
@@ -22,6 +22,10 @@ var _ fyne.Widget = (*AppTabs)(nil)
 // Since: 1.4
 type AppTabs struct {
 	baseTabs
+	// Deprecated: 'Items' has been moved into an embedded type.
+	// Most devs using AppTabs will not be affected when this is removed as they can still assign to Items field and call the same public APIs.
+	// The only devs affected would be ones using struct init (a := &AppTabs{Items: ...}) who'll need to change to (a := &AppTabs{}; a.Items = ...)
+	Items []*TabItem
 	// Deprecated: Use `OnSelected func(*TabItem)` instead.
 	OnChanged func(tab *TabItem)
 }
@@ -35,6 +39,7 @@ func NewAppTabs(items ...*TabItem) *AppTabs {
 			BaseWidget: widget.BaseWidget{},
 			current:    -1,
 		},
+		Items: items,
 	}
 	tabs.ExtendBaseWidget(tabs)
 	tabs.SetItems(items)
@@ -43,6 +48,7 @@ func NewAppTabs(items ...*TabItem) *AppTabs {
 
 // CreateRenderer is a private method to Fyne which links this widget to its renderer
 func (t *AppTabs) CreateRenderer() fyne.WidgetRenderer {
+	t.baseTabs.Items = t.Items
 	t.ExtendBaseWidget(t)
 	r := &appTabsRenderer{
 		baseTabsRenderer: baseTabsRenderer{
@@ -58,6 +64,15 @@ func (t *AppTabs) CreateRenderer() fyne.WidgetRenderer {
 	r.updateTabs(1)
 	r.updateIndicator()
 	return r
+}
+
+// Append adds a new TabItem to the end of the tab bar.
+//
+// Deprecated: Moved into an embedded type.
+func (t *AppTabs) Append(item *TabItem) {
+	t.baseTabs.Items = t.Items
+	t.baseTabs.Append(item)
+	t.Items = t.baseTabs.Items
 }
 
 // CurrentTab returns the currently selected TabItem.
@@ -77,10 +92,55 @@ func (t *AppTabs) CurrentTabIndex() int {
 	return t.current
 }
 
+// Hide hides the widget.
+//
+// Implements: fyne.Widget
+//
+// Deprecated: Moved into an embedded type.
+func (t *AppTabs) Hide() {
+	t.baseTabs.Items = t.Items
+	t.baseTabs.Hide()
+}
+
 // MinSize returns the size that this widget should not shrink below
 func (t *AppTabs) MinSize() fyne.Size {
+	t.baseTabs.Items = t.Items
 	t.ExtendBaseWidget(t)
 	return t.BaseWidget.MinSize()
+}
+
+// Remove tab by value.
+//
+// Deprecated: Moved into an embedded type.
+func (t *AppTabs) Remove(item *TabItem) {
+	t.baseTabs.Items = t.Items
+	t.baseTabs.Remove(item)
+	t.Items = t.baseTabs.Items
+}
+
+// RemoveIndex removes tab by index.
+//
+// Deprecated: Moved into an embedded type.
+func (t *AppTabs) RemoveIndex(index int) {
+	t.baseTabs.Items = t.Items
+	t.baseTabs.RemoveIndex(index)
+	t.Items = t.baseTabs.Items
+}
+
+// Select sets the specified TabItem to be selected and its content visible.
+//
+// Deprecated: Moved into an embedded type.
+func (t *AppTabs) Select(item *TabItem) {
+	t.baseTabs.Items = t.Items
+	t.baseTabs.Select(item)
+}
+
+// SelectIndex sets the TabItem at the specific index to be selected and its content visible.
+//
+// Deprecated: Moved into an embedded type.
+func (t *AppTabs) SelectIndex(index int) {
+	t.baseTabs.Items = t.Items
+	t.baseTabs.SelectIndex(index)
 }
 
 // SelectTab sets the specified TabItem to be selected and its content visible.
@@ -110,8 +170,33 @@ func (t *AppTabs) SelectTabIndex(index int) {
 	}
 }
 
+// Selection returns the currently selected TabItem.
+//
+// Deprecated: Moved into an embedded type.
+func (t *AppTabs) Selection() *TabItem {
+	t.baseTabs.Items = t.Items
+	return t.baseTabs.Selection()
+}
+
+// SelectionIndex returns the index of the currently selected TabItem.
+//
+// Deprecated: Moved into an embedded type.
+func (t *AppTabs) SelectionIndex() int {
+	t.baseTabs.Items = t.Items
+	return t.baseTabs.SelectionIndex()
+}
+
+// SetItems sets the container’s items and refreshes.
+//
+// Deprecated: Moved into an embedded type.
+func (t *AppTabs) SetItems(items []*TabItem) {
+	t.baseTabs.SetItems(items)
+	t.Items = t.baseTabs.Items
+}
+
 // SetTabLocation sets the location of the tab bar
 func (t *AppTabs) SetTabLocation(l TabLocation) {
+	t.baseTabs.Items = t.Items
 	// Mobile has limited screen space, so don't put app tab bar on long edges
 	if d := fyne.CurrentDevice(); d.IsMobile() {
 		if o := d.Orientation(); fyne.IsVertical(o) {
@@ -125,6 +210,14 @@ func (t *AppTabs) SetTabLocation(l TabLocation) {
 		}
 	}
 	t.baseTabs.SetTabLocation(l)
+}
+
+// Show this widget, if it was previously hidden
+//
+// Deprecated: Moved into an embedded type.
+func (t *AppTabs) Show() {
+	t.baseTabs.Items = t.Items
+	t.baseTabs.Show()
 }
 
 // Declare conformity with WidgetRenderer interface.
@@ -163,7 +256,7 @@ func (r *appTabsRenderer) MinSize() fyne.Size {
 
 func (r *appTabsRenderer) Objects() []fyne.CanvasObject {
 	objects := r.baseTabsRenderer.Objects()
-	if i, is := r.appTabs.current, r.appTabs.Items; i >= 0 && i < len(is) {
+	if i, is := r.appTabs.current, r.appTabs.baseTabs.Items; i >= 0 && i < len(is) {
 		objects = append(objects, is[i].Content)
 	}
 	return objects
@@ -182,8 +275,8 @@ func (r *appTabsRenderer) buildOverflowTabsButton() (overflow *widget.Button) {
 		// Show pop up containing all tabs which did not fit in the tab bar
 
 		var items []*fyne.MenuItem
-		for i := len(r.bar.Objects[0].(*fyne.Container).Objects); i < len(r.appTabs.Items); i++ {
-			item := r.appTabs.Items[i]
+		for i := len(r.bar.Objects[0].(*fyne.Container).Objects); i < len(r.appTabs.baseTabs.Items); i++ {
+			item := r.appTabs.baseTabs.Items[i]
 			// FIXME MenuItem doesn't support icons (#1752)
 			// FIXME MenuItem can't show if it is the currently selected tab (#1753)
 			items = append(items, fyne.NewMenuItem(item.Text, func() {
@@ -218,7 +311,7 @@ func (r *appTabsRenderer) buildTabButtons(count int) *fyne.Container {
 	}
 
 	for i := 0; i < count; i++ {
-		item := r.appTabs.Items[i]
+		item := r.appTabs.baseTabs.Items[i]
 		button, ok := r.buttonCache[item]
 		if !ok {
 			button = &tabButton{
@@ -284,7 +377,7 @@ func (r *appTabsRenderer) updateIndicator() {
 }
 
 func (r *appTabsRenderer) updateTabs(max int) {
-	tabCount := len(r.appTabs.Items)
+	tabCount := len(r.appTabs.baseTabs.Items)
 
 	// Set overflow action
 	if tabCount < max {
