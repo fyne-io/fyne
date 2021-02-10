68fcc28c3d98c113e782618e18bb7405723799f6
Andy Williams <andy@andywilliams.me>
2021-02-10 18:16:00 +0000

    Fix possible app crashes on Windows

--- internal/driver/gomobile/keyboard_test.go
+++ internal/driver/gomobile/keyboard_test.go
@@ -1,6 +1,10 @@
 package gomobile
 
-import "testing"
+import (
+	"testing"
+
+	_ "fyne.io/fyne/v2/test"
+)
 
 func TestDevice_HideVirtualKeyboard_BeforeRun(t *testing.T) {
 	hideVirtualKeyboard() // should not crash!
