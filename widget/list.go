791ecf4c805df0d5e2ae578b544842f2fdca4dc8
FPabl0 <f.pablo1@hotmail.com>
2021-02-09 20:10:50 -0500

    handle situation in widget.List when the items length is lower than the current visible ones, fixes 1864

--- widget/list.go
+++ widget/list.go
@@ -207,6 +207,13 @@ func (l *listRenderer) Layout(size fyne.Size) {
 		return
 	}
 	min := int(fyne.Min(float32(length), float32(l.visibleItemCount)))
+	if length < len(l.children) {
+		l.firstItemIndex = 0
+		l.list.offsetY = 0
+		l.visibleItemCount = min
+		// TODO should the list scroll to top if length is lower than the children length?
+		l.scroller.ScrollToTop()
+	}
 	if len(l.children) > min {
 		for i := len(l.children); i >= min; i-- {
 			l.itemPool.Release(l.children[i-1])
