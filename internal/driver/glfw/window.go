5ceb286d47492f5659793f198d47361098c69153
Andy Williams <andy@andy.xyz>
2021-02-11 21:21:12 +0000

    Keep track of windows closing without removing context
    Fixes #1604

--- internal/driver/glfw/window.go
+++ internal/driver/glfw/window.go
@@ -51,6 +51,7 @@ type window struct {
 	viewLock   sync.RWMutex
 	createLock sync.Once
 	decorate   bool
+	closing    bool
 	fixedSize  bool
 
 	cursor       desktop.Cursor
@@ -273,7 +274,7 @@ func (w *window) fitContent() {
 		return
 	}
 
-	if w.viewport == nil {
+	if w.viewport == nil || w.closing {
 		return
 	}
 
@@ -405,7 +406,7 @@ func (w *window) doShow() {
 }
 
 func (w *window) Hide() {
-	if w.viewport == nil {
+	if w.closing {
 		return
 	}
 
@@ -423,10 +424,11 @@ func (w *window) Hide() {
 }
 
 func (w *window) Close() {
-	if w.viewport == nil {
+	if w.closing {
 		return
 	}
 
+	w.closing = true
 	w.viewport.SetShouldClose(true)
 
 	w.canvas.walkTrees(nil, func(node *renderCacheNode) {
@@ -449,7 +451,7 @@ func (w *window) ShowAndRun() {
 
 // Clipboard returns the system clipboard
 func (w *window) Clipboard() fyne.Clipboard {
-	if w.viewport == nil {
+	if w.view() == nil {
 		return nil
 	}
 
@@ -1176,6 +1178,9 @@ func (w *window) focused(_ *glfw.Window, isFocused bool) {
 }
 
 func (w *window) RunWithContext(f func()) {
+	if w.closing {
+		return
+	}
 	w.viewport.MakeContextCurrent()
 
 	f()
@@ -1190,7 +1195,7 @@ func (w *window) RescaleContext() {
 }
 
 func (w *window) rescaleOnMain() {
-	if w.viewport == nil {
+	if w.viewport == nil || w.closing {
 		return
 	}
 	w.fitContent()
@@ -1354,7 +1359,7 @@ func (w *window) create() {
 }
 
 func (w *window) doShowAgain() {
-	if w.viewport == nil {
+	if w.closing {
 		return
 	}
 
@@ -1376,6 +1381,9 @@ func (w *window) view() *glfw.Window {
 	w.viewLock.RLock()
 	defer w.viewLock.RUnlock()
 
+	if w.closing {
+		return nil
+	}
 	return w.viewport
 }
 
