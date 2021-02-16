7414f5b90eae28683fb70980c4f4ceed6d1a959e
FPabl0 <f.pablo1@hotmail.com>
2021-02-16 01:56:06 -0500

    added themedBackground widget to use it as dialog background, added and updated dialog's tests

--- dialog/base.go
+++ dialog/base.go
@@ -40,7 +40,7 @@ type dialog struct {
 	desiredSize fyne.Size
 
 	win            *widget.PopUp
-	bg             *canvas.Rectangle
+	bg             *themedBackground
 	content, label fyne.CanvasObject
 	dismiss        *widget.Button
 	parent         fyne.Window
@@ -121,7 +121,6 @@ func (d *dialog) Show() {
 }
 
 func (d *dialog) Refresh() {
-	d.applyTheme()
 	d.win.Refresh()
 }
 
@@ -165,12 +164,6 @@ func (d *dialog) SetOnClosed(closed func()) {
 	}
 }
 
-func (d *dialog) applyTheme() {
-	r, g, b, _ := theme.BackgroundColor().RGBA()
-	bg := &color.NRGBA{R: uint8(r), G: uint8(g), B: uint8(b), A: 230}
-	d.bg.FillColor = bg
-}
-
 func (d *dialog) hideWithResponse(resp bool) {
 	d.win.Hide()
 	if d.callback != nil {
@@ -179,7 +172,7 @@ func (d *dialog) hideWithResponse(resp bool) {
 }
 
 func (d *dialog) setButtons(buttons fyne.CanvasObject) {
-	d.bg = canvas.NewRectangle(theme.BackgroundColor())
+	d.bg = newThemedBackground()
 	d.label = widget.NewLabelWithStyle(d.title, fyne.TextAlignLeading, fyne.TextStyle{Bold: true})
 
 	var content fyne.CanvasObject
@@ -228,6 +221,45 @@ func newButtonList(buttons ...*widget.Button) fyne.CanvasObject {
 	return list
 }
 
+// ===============================================================
+// ThemedBackground
+// ===============================================================
+
+type themedBackground struct {
+	widget.BaseWidget
+}
+
+func newThemedBackground() *themedBackground {
+	t := &themedBackground{}
+	t.ExtendBaseWidget(t)
+	return t
+}
+
+func (t *themedBackground) CreateRenderer() fyne.WidgetRenderer {
+	t.ExtendBaseWidget(t)
+	rect := canvas.NewRectangle(theme.BackgroundColor())
+	return &themedBackgroundRenderer{rect, []fyne.CanvasObject{rect}}
+}
+
+type themedBackgroundRenderer struct {
+	rect    *canvas.Rectangle
+	objects []fyne.CanvasObject
+}
+
+func (renderer *themedBackgroundRenderer) Objects() []fyne.CanvasObject { return renderer.objects }
+func (renderer *themedBackgroundRenderer) Destroy()                     {}
+func (renderer *themedBackgroundRenderer) Layout(size fyne.Size)        { renderer.rect.Resize(size) }
+func (renderer *themedBackgroundRenderer) MinSize() fyne.Size           { return renderer.rect.MinSize() }
+func (renderer *themedBackgroundRenderer) Refresh() {
+	r, g, b, _ := theme.BackgroundColor().RGBA()
+	bg := &color.NRGBA{R: uint8(r), G: uint8(g), B: uint8(b), A: 230}
+	renderer.rect.FillColor = bg
+}
+
+// ===============================================================
+// DialogLayout
+// ===============================================================
+
 type dialogLayout struct {
 	d *dialog
 }
@@ -236,7 +268,7 @@ func (l *dialogLayout) Layout(obj []fyne.CanvasObject, size fyne.Size) {
 	l.d.bg.Move(fyne.NewPos(0, 0))
 	l.d.bg.Resize(size)
 
-	btnMin := obj[3].MinSize().Max(obj[3].Size())
+	btnMin := obj[3].MinSize()
 
 	// icon
 	iconHeight := padHeight*2 + l.d.label.MinSize().Height*2 - theme.Padding()
@@ -256,7 +288,7 @@ func (l *dialogLayout) Layout(obj []fyne.CanvasObject, size fyne.Size) {
 
 func (l *dialogLayout) MinSize(obj []fyne.CanvasObject) fyne.Size {
 	contentMin := obj[2].MinSize()
-	btnMin := obj[3].MinSize().Max(obj[3].Size())
+	btnMin := obj[3].MinSize()
 
 	width := fyne.Max(fyne.Max(contentMin.Width, btnMin.Width), obj[4].MinSize().Width) + padWidth
 	height := contentMin.Height + btnMin.Height + l.d.label.MinSize().Height + theme.Padding() + padHeight*2
