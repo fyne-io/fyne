9e51376b60df8c0b7940dfb7fa89e62cc30b0b0e
TehSphinX <tehsphinx@web.de>
2021-02-20 15:22:36 +0100

    undo reorder

--- menu.go
+++ menu.go
@@ -15,10 +15,10 @@ func NewMenu(label string, items ...*MenuItem) *Menu {
 // MenuItem is a single item within any menu, it contains a display Label and Action function that is called when tapped.
 type MenuItem struct {
 	ChildMenu   *Menu
-	Label       string
 	IsSeparator bool
 	// Since: 2.1
 	IsQuit bool
+	Label  string
 	Action func()
 }
 
