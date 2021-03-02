4b63512a2f8450fa1f0ed6b91ec44142a3f8263a
FPabl0 <f.pablo1@hotmail.com>
2021-03-02 02:06:00 -0500

    track draggable object position to avoid moving it to (0,0) when the drag event goes out of the widget

--- internal/driver/gomobile/canvas.go
+++ internal/driver/gomobile/canvas.go
@@ -40,12 +40,13 @@ type mobileCanvas struct {
 	onTypedKey  func(event *fyne.KeyEvent)
 	shortcut    fyne.ShortcutHandler
 
-	inited         bool
-	lastTapDown    map[int]time.Time
-	lastTapDownPos map[int]fyne.Position
-	dragging       fyne.Draggable
-	refreshQueue   chan fyne.CanvasObject
-	minSizeCache   map[fyne.CanvasObject]fyne.Size
+	inited                bool
+	lastTapDown           map[int]time.Time
+	lastTapDownPos        map[int]fyne.Position
+	dragging              fyne.Draggable
+	dragStart, dragOffset fyne.Position
+	refreshQueue          chan fyne.CanvasObject
+	minSizeCache          map[fyne.CanvasObject]fyne.Size
 
 	touchTapCount   int
 	touchCancelFunc context.CancelFunc
@@ -389,8 +390,9 @@ func (c *mobileCanvas) tapDown(pos fyne.Position, tapID int) {
 
 func (c *mobileCanvas) tapMove(pos fyne.Position, tapID int,
 	dragCallback func(fyne.Draggable, *fyne.DragEvent)) {
-	deltaX := pos.X - c.lastTapDownPos[tapID].X
-	deltaY := pos.Y - c.lastTapDownPos[tapID].Y
+	previousPos := c.lastTapDownPos[tapID]
+	deltaX := pos.X - previousPos.X
+	deltaY := pos.Y - previousPos.Y
 
 	if c.dragging == nil && (math.Abs(float64(deltaX)) < tapMoveThreshold && math.Abs(float64(deltaY)) < tapMoveThreshold) {
 		return
@@ -420,13 +422,17 @@ func (c *mobileCanvas) tapMove(pos fyne.Position, tapID int,
 	if c.dragging == nil {
 		if drag, ok := co.(fyne.Draggable); ok {
 			c.dragging = drag
+			c.dragStart = co.Position()
+			c.dragOffset = previousPos.Subtract(pos)
 		} else {
 			return
 		}
 	}
 
 	ev := new(fyne.DragEvent)
-	ev.Position = objPos
+	draggedObjDelta := c.dragStart.Subtract(c.dragging.(fyne.CanvasObject).Position())
+	ev.AbsolutePosition = pos
+	ev.Position = pos.Subtract(c.dragOffset).Add(draggedObjDelta)
 	ev.Dragged = fyne.Delta{DX: deltaX, DY: deltaY}
 
 	dragCallback(c.dragging, ev)
