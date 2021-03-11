d1d2a58a6a1c4ba5c691c3e9c9bd0e1486bd80e8
FPabl0 <f.pablo1@hotmail.com>
2021-03-11 02:00:16 -0500

    added missing test case to TestEntry_Password_Extended_CreateRenderer

--- widget/entry_password_extend_test.go
+++ widget/entry_password_extend_test.go
@@ -14,6 +14,9 @@ type extendEntry struct {
 }
 
 func TestEntry_Password_Extended_CreateRenderer(t *testing.T) {
+	a := test.NewApp()
+	defer test.NewApp()
+	w := a.NewWindow("")
 	entry := &extendEntry{}
 	entry.ExtendBaseWidget(entry)
 	entry.Password = true
@@ -23,9 +26,12 @@ func TestEntry_Password_Extended_CreateRenderer(t *testing.T) {
 	p := test.WidgetRenderer(r).(*entryContentRenderer).provider
 	texts := test.WidgetRenderer(p).(*textRenderer).texts
 
+	w.SetContent(entry)
+
 	test.Type(entry, "Pass")
 	assert.Equal(t, passwordChar+passwordChar+passwordChar+passwordChar, texts[0].Text)
 	assert.NotNil(t, entry.ActionItem)
 	test.Tap(entry.ActionItem.(*passwordRevealer))
 	assert.Equal(t, "Pass", texts[0].Text)
+	assert.Equal(t, entry, w.Canvas().Focused())
 }
