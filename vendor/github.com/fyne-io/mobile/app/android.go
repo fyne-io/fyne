e93f96b0797b04e7553a228341da75e85f175c78
Andy Williams <andy@andywilliams.me>
2021-03-22 22:50:36 +0000

    Update vendor for mobile dep

--- vendor/github.com/fyne-io/mobile/app/android.go
+++ vendor/github.com/fyne-io/mobile/app/android.go
@@ -35,8 +35,8 @@ package app
 #include <pthread.h>
 #include <stdlib.h>
 
-EGLDisplay display;
-EGLSurface surface;
+extern EGLDisplay display;
+extern EGLSurface surface;
 
 char* createEGLSurface(ANativeWindow* window);
 char* destroyEGLSurface();
