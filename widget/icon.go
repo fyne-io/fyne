c527d0fe547f225f9bf557d1ae4d2e4e50843da7
Andy Williams <andy@andywilliams.me>
2021-04-12 19:47:49 +0100

    Re-use the underling image in Icon widget

--- widget/icon.go
+++ widget/icon.go
@@ -9,6 +9,8 @@ import (
 
 type iconRenderer struct {
 	widget.BaseRenderer
+	raster *canvas.Image
+
 	image *Icon
 }
 
@@ -26,14 +28,15 @@ func (i *iconRenderer) Layout(size fyne.Size) {
 }
 
 func (i *iconRenderer) Refresh() {
-	if i.image.Resource != i.image.cachedRes {
-		i.image.propertyLock.RLock()
-		i.updateObjects()
-		i.image.cachedRes = i.image.Resource
-		i.image.propertyLock.RUnlock()
+	if i.image.Resource == i.image.cachedRes {
+		return
 	}
 
-	i.Layout(i.image.Size())
+	i.image.propertyLock.RLock()
+	i.raster.Resource = i.image.Resource
+	i.image.cachedRes = i.image.Resource
+	i.image.propertyLock.RUnlock()
+
 	canvas.Refresh(i.image.super())
 }
 
@@ -58,7 +61,6 @@ type Icon struct {
 // SetResource updates the resource rendered in this icon widget
 func (i *Icon) SetResource(res fyne.Resource) {
 	i.Resource = res
-	i.cachedRes = nil
 	i.Refresh()
 }
 
@@ -73,8 +75,12 @@ func (i *Icon) CreateRenderer() fyne.WidgetRenderer {
 	i.ExtendBaseWidget(i)
 	i.propertyLock.RLock()
 	defer i.propertyLock.RUnlock()
-	r := &iconRenderer{image: i}
-	r.updateObjects()
+
+	img := canvas.NewImageFromResource(i.Resource)
+	img.FillMode = canvas.ImageFillContain
+	r := &iconRenderer{image: i, raster: img}
+	r.SetObjects([]fyne.CanvasObject{img})
+	i.cachedRes = i.Resource
 	return r
 }
 
