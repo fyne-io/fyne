b142ab7960531321b4ab83052e46db8f6fc0f17d
LazyHacker <github@lazyhacker.com>
2021-04-24 10:43:36 -0700

    Clarified wording per review comments.

--- dialog/folder.go
+++ dialog/folder.go
@@ -21,7 +21,7 @@ func NewFolderOpen(callback func(fyne.ListableURI, error), parent fyne.Window) *
 
 // ShowFolderOpen creates and shows a file dialog allowing the user to choose a
 // folder to open. The callback function will run when the dialog closes.  The
-// URI will be nil when nothing is selected or cancelled.
+// URI will be nil when the user cancels or when nothing is selected.
 //
 // The dialog will appear over the window specified.
 //
