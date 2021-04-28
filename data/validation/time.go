dc65e480680a34afd2bdb2e381d7e0d5332b4b16
Jacalz <jacob.alzen@gmail.com>
2021-04-28 20:40:56 +0200

    Add back the reference time

--- data/validation/time.go
+++ data/validation/time.go
@@ -8,7 +8,8 @@ import (
 
 // NewTime creates a new validator that verifies times using time.Parse.
 // The validator will return nil if valid, otherwise returns an error with a reason text.
-// See time.Parse() for reference time format: https://pkg.go.dev/time#Parse
+// The reference time for the format: Mon Jan 2 15:04:05 -0700 MST 2006.
+// See time.Parse() for more information about the reference time: https://pkg.go.dev/time#Parse
 //
 // Since: 2.1
 func NewTime(format string) fyne.StringValidator {
