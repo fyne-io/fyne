6ed26884d09150b45e75cd2d9d516a656fac543a
Andy Williams <andy@andy.xyz>
2021-05-01 15:12:22 +0100

    Show external bind addition to the extended time.Time binding

--- data/binding/binditems_extend_test.go
+++ data/binding/binditems_extend_test.go
@@ -7,6 +7,40 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestBindTime(t *testing.T) {
+	val := time.Now()
+	f := bindTime(&val)
+	v, err := f.Get()
+	assert.Nil(t, err)
+	assert.Equal(t, val.Unix(), v.Unix())
+
+	called := false
+	fn := NewDataListener(func() {
+		called = true
+	})
+	f.AddListener(fn)
+	waitForItems()
+	assert.True(t, called)
+
+	newTime := val.Add(time.Hour)
+	called = false
+	err = f.Set(newTime)
+	assert.Nil(t, err)
+	waitForItems()
+	assert.Equal(t, newTime.Unix(), val.Unix())
+	assert.True(t, called)
+
+	newTime = newTime.Add(time.Minute)
+	called = false
+	val = newTime
+	f.Reload()
+	waitForItems()
+	assert.True(t, called)
+	v, err = f.Get()
+	assert.Nil(t, err)
+	assert.Equal(t, newTime.Unix(), v.Unix())
+}
+
 func TestNewTime(t *testing.T) {
 	f := newTime()
 	v, err := f.Get()
@@ -23,6 +57,11 @@ func TestNewTime(t *testing.T) {
 
 type timeBinding struct {
 	Int
+	src *time.Time
+}
+
+func bindTime(t *time.Time) *timeBinding {
+	return &timeBinding{Int: NewInt(), src: t}
 }
 
 func newTime() *timeBinding {
@@ -30,11 +69,27 @@ func newTime() *timeBinding {
 }
 
 func (t *timeBinding) Get() (time.Time, error) {
+	if t.src != nil {
+		return *t.src, nil
+	}
+
 	i, err := t.Int.Get()
 	return time.Unix(int64(i), 0), err
 }
 
+func (t *timeBinding) Reload() error {
+	if t.src == nil {
+		return nil
+	}
+
+	return t.Set(*t.src)
+}
+
 func (t *timeBinding) Set(time time.Time) error {
+	if t.src != nil {
+		*t.src = time
+	}
+
 	i := time.Unix()
 	return t.Int.Set(int(i))
 }
