d18fa2d49623c7242e4a3f569e753d4d35fb92db
Andy Williams <andy@fyne.io>
2021-05-06 16:36:09 +0100

    Now we have standard padded widgets we don't need padding in collections

--- widget/table_test.go
+++ widget/table_test.go
@@ -112,7 +112,7 @@ func TestTable_MinSize(t *testing.T) {
 		},
 		"large": {
 			fyne.NewSize(100, 100),
-			fyne.NewSize(100+3*theme.Padding(), 100+3*theme.Padding()),
+			fyne.NewSize(100+theme.Padding(), 100+theme.Padding()),
 		},
 	} {
 		t.Run(name, func(t *testing.T) {
@@ -284,9 +284,9 @@ func TestTable_SetColumnWidth(t *testing.T) {
 	renderer := test.WidgetRenderer(table).(*tableRenderer)
 	cellRenderer := test.WidgetRenderer(renderer.scroll.Content.(*tableCells))
 	cellRenderer.Refresh()
-	assert.Equal(t, 8, len(cellRenderer.Objects()))
+	assert.Equal(t, 10, len(cellRenderer.Objects()))
 	assert.Equal(t, float32(32), cellRenderer.(*tableCellsRenderer).Objects()[0].Size().Width)
-	cell1Offset := theme.SeparatorThicknessSize() + theme.Padding()*3
+	cell1Offset := theme.SeparatorThicknessSize()
 	assert.Equal(t, float32(32)+cell1Offset, cellRenderer.(*tableCellsRenderer).Objects()[1].Position().X)
 
 	table.SetColumnWidth(0, 16)
@@ -311,7 +311,7 @@ func TestTable_ShowVisible(t *testing.T) {
 	renderer := test.WidgetRenderer(table).(*tableRenderer)
 	cellRenderer := test.WidgetRenderer(renderer.scroll.Content.(*tableCells))
 	cellRenderer.Refresh()
-	assert.Equal(t, 8, len(cellRenderer.Objects()))
+	assert.Equal(t, 10, len(cellRenderer.Objects()))
 }
 
 func TestTable_SeparatorThicknessZero_NotPanics(t *testing.T) {
