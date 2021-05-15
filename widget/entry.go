08f6a2a671c4296cba77dc07b112d19a04fd6ff6
FPabl0 <f.pablo1@hotmail.com>
2021-05-15 02:49:14 -0500

    fix notEmpty validator behavior in widget.Entry

--- widget/entry.go
+++ widget/entry.go
@@ -58,6 +58,7 @@ type Entry struct {
 
 	cursorAnim *entryCursorAnimation
 
+	dirty       bool
 	focused     bool
 	text        *textProvider
 	placeholder *textProvider
@@ -132,7 +133,7 @@ func (e *Entry) Bind(data binding.String) {
 		val, err := data.Get()
 		if err != nil {
 			convertErr = err
-			e.SetValidationError(e.Validate())
+			e.Validate()
 			return
 		}
 		e.Text = val
@@ -146,7 +147,7 @@ func (e *Entry) Bind(data binding.String) {
 
 	e.OnChanged = func(s string) {
 		convertErr = data.Set(s)
-		e.SetValidationError(e.Validate())
+		e.Validate()
 	}
 }
 
@@ -293,6 +294,7 @@ func (e *Entry) ExtendBaseWidget(wid fyne.Widget) {
 // Implements: fyne.Focusable
 func (e *Entry) FocusGained() {
 	e.setFieldsAndRefresh(func() {
+		e.dirty = true
 		e.focused = true
 	})
 }
@@ -1026,6 +1028,10 @@ func (e *Entry) textProvider() *textProvider {
 		return e.text
 	}
 
+	if e.Text != "" {
+		e.dirty = true
+	}
+
 	text := newTextProvider(e.Text, e)
 	text.ExtendBaseWidget(text)
 	text.extraPad = fyne.NewSize(theme.Padding(), theme.InputBorderSize())
@@ -1073,14 +1079,16 @@ func (e *Entry) updateText(text string) {
 		changed := e.Text != text
 		e.Text = text
 
+		if e.Text != "" {
+			e.dirty = true
+		}
+
 		if changed {
 			callback = e.OnChanged
 		}
 	})
 
-	if validate := e.Validator; validate != nil {
-		e.SetValidationError(validate(text))
-	}
+	e.Validate()
 
 	if callback != nil {
 		callback(text)
@@ -1252,7 +1260,7 @@ func (r *entryRenderer) Refresh() {
 	}
 
 	if r.entry.Validator != nil {
-		if !r.entry.focused && !r.entry.Disabled() && r.entry.Text != "" && r.entry.validationError != nil {
+		if !r.entry.focused && !r.entry.Disabled() && r.entry.dirty && r.entry.validationError != nil {
 			r.line.FillColor = theme.ErrorColor()
 		}
 		r.ensureValidationSetup()
@@ -1271,9 +1279,8 @@ func (r *entryRenderer) ensureValidationSetup() {
 		r.objects = append(r.objects, r.entry.validationStatus)
 		r.Layout(r.entry.size)
 
-		if r.entry.Text != "" {
-			r.entry.Validate()
-		}
+		r.entry.Validate()
+
 		r.Refresh()
 	}
 }
