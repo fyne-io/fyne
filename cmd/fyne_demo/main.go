4dee5b999e3f4c165d79f18632008cde9da9942d
Andy Williams <andy@andy.xyz>
2021-07-09 09:25:28 +0100

    Break out menu code

--- cmd/fyne_demo/main.go
+++ cmd/fyne_demo/main.go
@@ -27,6 +27,61 @@ func main() {
 	w := a.NewWindow("Fyne Demo")
 	topWindow = w
 
+	w.SetMainMenu(makeMenu(a, w))
+	w.SetMaster()
+
+	content := container.NewMax()
+	title := widget.NewLabel("Component name")
+	intro := widget.NewLabel("An introduction would probably go\nhere, as well as a")
+	intro.Wrapping = fyne.TextWrapWord
+	setTutorial := func(t tutorials.Tutorial) {
+		if fyne.CurrentDevice().IsMobile() {
+			child := a.NewWindow(t.Title)
+			topWindow = child
+			child.SetContent(t.View(topWindow))
+			child.Show()
+			child.SetOnClosed(func() {
+				topWindow = w
+			})
+			return
+		}
+
+		title.SetText(t.Title)
+		intro.SetText(t.Intro)
+
+		content.Objects = []fyne.CanvasObject{t.View(w)}
+		content.Refresh()
+	}
+
+	tutorial := container.NewBorder(
+		container.NewVBox(title, widget.NewSeparator(), intro), nil, nil, nil, content)
+	if fyne.CurrentDevice().IsMobile() {
+		w.SetContent(makeNav(setTutorial, false))
+	} else {
+		split := container.NewHSplit(makeNav(setTutorial, true), tutorial)
+		split.Offset = 0.2
+		w.SetContent(split)
+	}
+	w.Resize(fyne.NewSize(640, 460))
+	w.ShowAndRun()
+}
+
+func logLifecycle(a fyne.App) {
+	a.Lifecycle().SetOnStarted(func() {
+		log.Println("Lifecycle: Started")
+	})
+	a.Lifecycle().SetOnStopped(func() {
+		log.Println("Lifecycle: Stopped")
+	})
+	a.Lifecycle().SetOnEnteredForeground(func() {
+		log.Println("Lifecycle: Entered Foreground")
+	})
+	a.Lifecycle().SetOnExitedForeground(func() {
+		log.Println("Lifecycle: Exited Foreground")
+	})
+}
+
+func makeMenu(a fyne.App, w fyne.Window) *fyne.MainMenu {
 	newItem := fyne.NewMenuItem("New", nil)
 	otherItem := fyne.NewMenuItem("Other", nil)
 	otherItem.ChildMenu = fyne.NewMenu("",
@@ -80,64 +135,12 @@ func main() {
 	if !fyne.CurrentDevice().IsMobile() {
 		file.Items = append(file.Items, fyne.NewMenuItemSeparator(), settingsItem)
 	}
-	mainMenu := fyne.NewMainMenu(
+	return fyne.NewMainMenu(
 		// a quit item will be appended to our first menu
 		file,
 		fyne.NewMenu("Edit", cutItem, copyItem, pasteItem, fyne.NewMenuItemSeparator(), findItem),
 		helpMenu,
 	)
-	w.SetMainMenu(mainMenu)
-	w.SetMaster()
-
-	content := container.NewMax()
-	title := widget.NewLabel("Component name")
-	intro := widget.NewLabel("An introduction would probably go\nhere, as well as a")
-	intro.Wrapping = fyne.TextWrapWord
-	setTutorial := func(t tutorials.Tutorial) {
-		if fyne.CurrentDevice().IsMobile() {
-			child := a.NewWindow(t.Title)
-			topWindow = child
-			child.SetContent(t.View(topWindow))
-			child.Show()
-			child.SetOnClosed(func() {
-				topWindow = w
-			})
-			return
-		}
-
-		title.SetText(t.Title)
-		intro.SetText(t.Intro)
-
-		content.Objects = []fyne.CanvasObject{t.View(w)}
-		content.Refresh()
-	}
-
-	tutorial := container.NewBorder(
-		container.NewVBox(title, widget.NewSeparator(), intro), nil, nil, nil, content)
-	if fyne.CurrentDevice().IsMobile() {
-		w.SetContent(makeNav(setTutorial, false))
-	} else {
-		split := container.NewHSplit(makeNav(setTutorial, true), tutorial)
-		split.Offset = 0.2
-		w.SetContent(split)
-	}
-	w.Resize(fyne.NewSize(640, 460))
-	w.ShowAndRun()
-}
-
-func logLifecycle(a fyne.App) {
-	a.Lifecycle().SetOnStarted(func() {
-		log.Println("Lifecycle: Started")
-	})
-	a.Lifecycle().SetOnStopped(func() {
-		log.Println("Lifecycle: Stopped")
-	})
-	a.Lifecycle().SetOnEnteredForeground(func() {
-		log.Println("Lifecycle: Entered Foreground")
-	})
-	a.Lifecycle().SetOnExitedForeground(func() {
-		log.Println("Lifecycle: Exited Foreground")
-	})
 }
 
 func makeNav(setTutorial func(tutorial tutorials.Tutorial), loadPrevious bool) fyne.CanvasObject {
