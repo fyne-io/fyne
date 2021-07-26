a54ab4a100b5ad6a322d96ab63cede9f6d26b9c9
Andy Williams <andy@andywilliams.me>
2021-07-26 12:06:47 +0100

    Clarify naming

--- internal/cache/text.go
+++ internal/cache/text.go
@@ -7,16 +7,16 @@ import (
 )
 
 var (
-	sizeCache = map[sizeEntry]fontMetric{}
-	sizeLock  = sync.RWMutex{}
+	fontSizeCache = map[fontSizeEntry]fontMetric{}
+	fontSizeLock  = sync.RWMutex{}
 )
 
 type fontMetric struct {
-	bound    fyne.Size
+	size     fyne.Size
 	baseLine float32
 }
 
-type sizeEntry struct {
+type fontSizeEntry struct {
 	text  string
 	size  float32
 	style fyne.TextStyle
@@ -24,20 +24,20 @@ type sizeEntry struct {
 
 // GetFontSize looks up a calculated size required for the specified text parameters.
 func GetFontSize(text string, size float32, style fyne.TextStyle) (fyne.Size, float32) {
-	ent := sizeEntry{text, size, style}
-	sizeLock.RLock()
-	ret, ok := sizeCache[ent]
-	sizeLock.RUnlock()
+	ent := fontSizeEntry{text, size, style}
+	fontSizeLock.RLock()
+	ret, ok := fontSizeCache[ent]
+	fontSizeLock.RUnlock()
 	if !ok {
 		return fyne.Size{Width: 0, Height: 0}, 0
 	}
-	return ret.bound, ret.baseLine
+	return ret.size, ret.baseLine
 }
 
 // SetFontSize stores a calculated font size for parameters that were missing from the cache.
-func SetFontSize(text string, size float32, style fyne.TextStyle, bound fyne.Size, base float32) {
-	ent := sizeEntry{text, size, style}
-	sizeLock.Lock()
-	sizeCache[ent] = fontMetric{bound: bound, baseLine: base}
-	sizeLock.Unlock()
+func SetFontSize(text string, fontSize float32, style fyne.TextStyle, size fyne.Size, base float32) {
+	ent := fontSizeEntry{text, fontSize, style}
+	fontSizeLock.Lock()
+	fontSizeCache[ent] = fontMetric{size: size, baseLine: base}
+	fontSizeLock.Unlock()
 }
--- internal/cache/text_test.go
+++ internal/cache/text_test.go
@@ -9,14 +9,14 @@ import (
 
 func TestTextCacheGet(t *testing.T) {
 	ResetThemeCaches()
-	assert.Equal(t, 0, len(sizeCache))
+	assert.Equal(t, 0, len(fontSizeCache))
 
 	bound, base := GetFontSize("hi", 10, fyne.TextStyle{})
 	assert.True(t, bound.IsZero())
 	assert.Equal(t, float32(0), base)
 
 	SetFontSize("hi", 10, fyne.TextStyle{}, fyne.NewSize(10, 10), 8)
-	assert.Equal(t, 1, len(sizeCache))
+	assert.Equal(t, 1, len(fontSizeCache))
 
 	bound, base = GetFontSize("hi", 10, fyne.TextStyle{})
 	assert.Equal(t, fyne.NewSize(10, 10), bound)
