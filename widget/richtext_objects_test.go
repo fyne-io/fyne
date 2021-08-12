6bf11a8810aa63228cc6c29cf3b408f0a182906b
Andy Williams <andy@andy.xyz>
2021-08-12 17:13:11 +0100

    Add missing test

---
+++ widget/richtext_objects_test.go
@@ -0,0 +1,21 @@
+package widget
+
+import (
+	"strings"
+	"testing"
+
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/test"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRichText_List(t *testing.T) {
+	seg := trailingBoldErrorSegment()
+	seg.Text = "Test"
+	text := NewRichText(&ListSegment{Items: []RichTextSegment{
+		seg,
+	}})
+	texts := test.WidgetRenderer(text).Objects()
+	assert.Equal(t, "•", strings.TrimSpace(texts[0].(*canvas.Text).Text))
+	assert.Equal(t, "Test", texts[1].(*canvas.Text).Text)
+}
