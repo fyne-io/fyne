5e4915447924132989c0d903bed83c4c30a24d52
Andy Williams <andy@andy.xyz>
2021-08-12 17:23:48 +0100

    Support ordered lists as well :)

--- widget/richtext_objects_test.go
+++ widget/richtext_objects_test.go
@@ -19,3 +19,15 @@ func TestRichText_List(t *testing.T) {
 	assert.Equal(t, "•", strings.TrimSpace(texts[0].(*canvas.Text).Text))
 	assert.Equal(t, "Test", texts[1].(*canvas.Text).Text)
 }
+
+func TestRichText_OrderedList(t *testing.T) {
+	text := NewRichText(&ListSegment{Ordered: true, Items: []RichTextSegment{
+		&TextSegment{Text: "One"},
+		&TextSegment{Text: "Two"},
+	}})
+	texts := test.WidgetRenderer(text).Objects()
+	assert.Equal(t, "1.", strings.TrimSpace(texts[0].(*canvas.Text).Text))
+	assert.Equal(t, "One", texts[1].(*canvas.Text).Text)
+	assert.Equal(t, "2.", strings.TrimSpace(texts[2].(*canvas.Text).Text))
+	assert.Equal(t, "Two", texts[3].(*canvas.Text).Text)
+}
