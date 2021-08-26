9f34a031861958efbd484f9b99dc08d6e288ea43
Changkun Ou <hi@changkun.de>
2021-08-26 12:35:08 +0200

    Improve error message while packaging mobile

--- cmd/fyne/internal/commands/package-mobile.go
+++ cmd/fyne/internal/commands/package-mobile.go
@@ -1,8 +1,10 @@
 package commands
 
 import (
+	"bytes"
 	"io/ioutil"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"strconv"
 
@@ -11,7 +13,6 @@ import (
 	"fyne.io/fyne/v2/cmd/fyne/internal/templates"
 	"fyne.io/fyne/v2/cmd/fyne/internal/util"
 	"github.com/pkg/errors"
-	"golang.org/x/sys/execabs"
 )
 
 func (p *Packager) packageAndroid(arch string) error {
@@ -61,14 +62,37 @@ func (p *Packager) packageIOS() error {
 	}
 
 	appDir := filepath.Join(p.dir, mobile.AppOutputName(p.os, p.name))
-	cmd := execabs.Command("xcrun", "actool", "Images.xcassets", "--compile", appDir, "--platform",
+	return runCmd("xcrun", "actool", "Images.xcassets", "--compile", appDir, "--platform",
 		"iphoneos", "--target-device", "iphone", "--minimum-deployment-target", "9.0", "--app-icon", "AppIcon",
-		"--output-partial-info-plist", "/dev/null")
-	return cmd.Run()
+		"--output-format", "human-readable-text", "--output-partial-info-plist", "/dev/null")
 }
 
 func copyResizeIcon(size int, dir, source string) error {
 	strSize := strconv.Itoa(size)
 	path := dir + "/Icon_" + strSize + ".png"
-	return execabs.Command("sips", "-o", path, "-Z", strSize, source).Run()
+	return runCmd("sips", "-o", path, "-Z", strSize, source)
+}
+
+// runCmd is a exec.Command wrapper that offers better error messages from stdout and stderr.
+func runCmd(name string, args ...string) error {
+	var (
+		outbuf = &bytes.Buffer{}
+		errbuf = &bytes.Buffer{}
+	)
+	cmd := exec.Command(name, args...)
+	cmd.Stdout = outbuf
+	cmd.Stderr = errbuf
+	err := cmd.Run()
+	if err != nil {
+		outstr := outbuf.String()
+		errstr := errbuf.String()
+		if outstr != "" {
+			err = errors.Wrap(err, outbuf.String())
+		}
+		if errstr != "" {
+			err = errors.Wrap(err, errbuf.String())
+		}
+		return err
+	}
+	return nil
 }
