9d6bdf7f99b8e752ccd87b6fd48dc5c644fffb7c
Andy Williams <andy@andy.xyz>
2021-09-01 17:23:17 +0100

    Move to core hint init

--- internal/driver/glfw/window.go
+++ internal/driver/glfw/window.go
@@ -1399,7 +1399,6 @@ func (w *window) create() {
 		} else {
 			glfw.WindowHint(glfw.Resizable, 1)
 		}
-		glfw.WindowHint(glfw.CocoaGraphicsSwitching, glfw.True)
 		initWindowHints()
 
 		pixWidth, pixHeight := w.screenSize(w.canvas.size)
