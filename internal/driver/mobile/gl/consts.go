64590144c913f2a126bd63873c067519cc0bb1c1
Changkun Ou <hi@changkun.de>
2021-09-03 13:22:34 +0200

    driver/mobile/gl: offer consts to mobile build tag

--- internal/driver/mobile/gl/consts.go
+++ internal/driver/mobile/gl/consts.go
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build android || ios
-// +build android ios
+//go:build android || ios || mobile
+// +build android ios mobile
 
 package gl
 
