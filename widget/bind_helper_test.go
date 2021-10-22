2b9a41e3c9743103270d15919a3b63e656b4d923
Dmitrii Pirozhkov <7746216+nullst@users.noreply.github.com>
2021-10-22 15:22:23 +0200

    Add tests for the basicBinder utility type

---
+++ widget/bind_helper_test.go
@@ -0,0 +1,43 @@
+package widget
+
+import (
+	"testing"
+
+	"fyne.io/fyne/v2/data/binding"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBasicBinder(t *testing.T) {
+	var binder basicBinder
+	var value int = -1
+
+	item1 := binding.NewInt()
+
+	callback := func(data binding.DataItem) {
+		val, err := data.(binding.Int).Get()
+		assert.NoError(t, err)
+		value = val
+	}
+	binder.SetCallback(callback)
+
+	assert.Equal(t, -1, value)
+	item1.Set(100)
+	binder.Bind(item1)
+	waitForBinding()
+	assert.Equal(t, 100, value) // callback on the initial value
+	item1.Set(150)
+	waitForBinding()
+	assert.Equal(t, 150, value) // callback on the changed value
+
+	item2 := binding.NewInt()
+	item2.Set(200)
+	binder.Bind(item2)
+	waitForBinding()
+	assert.Equal(t, value, 200)
+	item1.Set(300)
+	waitForBinding()
+	assert.Equal(t, value, 200)
+	item2.Set(400)
+	waitForBinding()
+	assert.Equal(t, value, 400)
+}
