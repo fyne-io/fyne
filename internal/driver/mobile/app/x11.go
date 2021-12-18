f6be3a655d213cd1ae3f8ec809b5bf342fca2667
Jacob Alzén <jacob.alzen@gmail.com>
2021-12-18 12:21:13 +0100

    Make the build-tags happier

--- internal/driver/mobile/app/x11.go
+++ internal/driver/mobile/app/x11.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build linux && !android || freebsd
+//go:build (linux && !android) || freebsd
 // +build linux,!android freebsd
 
 package app
