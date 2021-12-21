e3b8977977906f93a7b9edcb252dd4ebab5b6469
Cedric BAIL <cedric@ddlm.me>
2021-12-20 17:44:18 -0700

    Add a databinding that will be able to pretty print and concatenate a series of data bindings.

---
+++ data/binding/printf.go
@@ -0,0 +1,254 @@
+package binding
+
+import (
+	"fmt"
+)
+
+type sprintfString struct {
+	String
+
+	format string
+	source []interface{}
+	err    error
+}
+
+// NewSprintf returns a String binding that format its content using the
+// format string and the provide additional parameter that must be other
+// data bindings.
+//
+// Since: 2.2
+func NewSprintf(format string, b ...interface{}) (String, error) {
+	ret := &sprintfString{String: NewString(), format: format, source: make([]interface{}, 0)}
+
+	for _, value := range b {
+		dataItem, ok := value.(DataItem)
+		if !ok {
+			return nil, fmt.Errorf("unexpected data type %T", value)
+		}
+		dataItem.AddListener(ret)
+
+		ret.source = append(ret.source, value)
+	}
+
+	return ret, nil
+}
+
+func (s *sprintfString) DataChanged() {
+	data := make([]interface{}, 0)
+
+	s.err = nil
+	for _, value := range s.source {
+		switch x := value.(type) {
+		case Bool:
+			{
+				b, err := x.Get()
+				if err != nil {
+					s.err = err
+					return
+				}
+
+				data = append(data, b)
+				break
+			}
+		case Bytes:
+			{
+				b, err := x.Get()
+				if err != nil {
+					s.err = err
+					return
+				}
+
+				data = append(data, b)
+				break
+			}
+		case Float:
+			{
+				f, err := x.Get()
+				if err != nil {
+					s.err = err
+					return
+				}
+
+				data = append(data, f)
+				break
+			}
+		case Int:
+			{
+				i, err := x.Get()
+				if err != nil {
+					s.err = err
+					return
+				}
+
+				data = append(data, i)
+				break
+			}
+		case Rune:
+			{
+				r, err := x.Get()
+				if err != nil {
+					s.err = err
+					return
+				}
+
+				data = append(data, r)
+				break
+			}
+		case String:
+			{
+				str, err := x.Get()
+				if err != nil {
+					s.err = err
+					// Set error?
+					return
+				}
+
+				data = append(data, str)
+				break
+			}
+		case URI:
+			{
+				u, err := x.Get()
+				if err != nil {
+					s.err = err
+					return
+				}
+
+				data = append(data, u)
+				break
+			}
+		}
+	}
+
+	r := fmt.Sprintf(s.format, data...)
+	s.String.Set(r)
+}
+
+func (s *sprintfString) Get() (string, error) {
+	if s.err != nil {
+		return "", s.err
+	}
+	return s.String.Get()
+}
+
+func (s *sprintfString) Set(str string) error {
+	data := make([]interface{}, 0)
+
+	s.err = nil
+	for _, value := range s.source {
+		switch value.(type) {
+		case Bool:
+			{
+				data = append(data, new(bool))
+				break
+			}
+		case Bytes:
+			{
+				return fmt.Errorf("impossible to convert '%s' to []bytes type", str)
+			}
+		case Float:
+			{
+				data = append(data, new(float64))
+				break
+			}
+		case Int:
+			{
+				data = append(data, new(int))
+				break
+			}
+		case Rune:
+			{
+				data = append(data, new(rune))
+				break
+			}
+		case String:
+			{
+				data = append(data, new(string))
+				break
+			}
+		case URI:
+			{
+				return fmt.Errorf("impossible to convert '%s' to fyne.URI type", str)
+			}
+		}
+	}
+
+	count, err := fmt.Sscanf(str, s.format, data...)
+	if err != nil {
+		return err
+	}
+
+	if count != len(data) {
+		return fmt.Errorf("impossible to decode more than %v parameters in '%s' with format '%s'", count, str, s.format)
+	}
+
+	for i, value := range s.source {
+		switch x := value.(type) {
+		case Bool:
+			{
+				v := data[i].(*bool)
+
+				err := x.Set(*v)
+				if err != nil {
+					return err
+				}
+
+				break
+			}
+		case Bytes:
+			{
+				return fmt.Errorf("impossible to convert '%s' to []bytes type", str)
+			}
+		case Float:
+			{
+				v := data[i].(*float64)
+
+				err := x.Set(*v)
+				if err != nil {
+					return err
+				}
+
+				break
+			}
+		case Int:
+			{
+				v := data[i].(*int)
+
+				err := x.Set(*v)
+				if err != nil {
+					return err
+				}
+
+				break
+			}
+		case Rune:
+			{
+				v := data[i].(*rune)
+
+				err := x.Set(*v)
+				if err != nil {
+					return err
+				}
+
+				break
+			}
+		case String:
+			{
+				v := data[i].(*string)
+
+				err := x.Set(*v)
+				if err != nil {
+					return err
+				}
+
+				break
+			}
+		case URI:
+			{
+				return fmt.Errorf("impossible to convert '%s' to fyne.URI type", str)
+			}
+		}
+	}
+
+	return nil
+}
---
+++ data/binding/printf_test.go
@@ -0,0 +1,114 @@
+package binding
+
+import (
+	"fmt"
+	"testing"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/storage"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSprintfWrongType(t *testing.T) {
+	_, err := NewSprintf("shouldn't work %v", true)
+
+	assert.NotNil(t, err)
+}
+
+func TestSprintfConversionRead(t *testing.T) {
+	var b bool = true
+	var f float64 = 42
+	var i int = 7
+	var r rune = 'B'
+	var s string = "this is a string"
+	var u fyne.URI = storage.NewFileURI("/")
+
+	bb := BindBool(&b)
+	bf := BindFloat(&f)
+	bi := BindInt(&i)
+	br := BindRune(&r)
+	bs := BindString(&s)
+	bu := BindURI(&u)
+
+	format := "Bool %v, Float %f, Int %i, Rune %v, String '%s', URI '%s'"
+	sp, err := NewSprintf(format, bb, bf, bi, br, bs, bu)
+	expected := fmt.Sprintf(format, b, f, i, r, s, u)
+
+	assert.Nil(t, err)
+	assert.NotNil(t, sp)
+
+	waitForItems()
+
+	sGenerated, err := sp.Get()
+
+	assert.Nil(t, err)
+	assert.NotNil(t, sGenerated)
+	assert.Equal(t, expected, sGenerated)
+
+	bb.Set(false)
+	bf.Set(7)
+	bi.Set(42)
+	br.Set('C')
+	bs.Set("a different string")
+
+	expectedChange := fmt.Sprintf(format, b, f, i, r, s, u)
+
+	waitForItems()
+
+	sChange, err := sp.Get()
+
+	assert.Nil(t, err)
+	assert.NotNil(t, sChange)
+	assert.Equal(t, expectedChange, sChange)
+	assert.NotEqual(t, sGenerated, sChange)
+}
+
+func TestSprintfConversionReadWrite(t *testing.T) {
+	var b bool = true
+	var f float64 = 42
+	var i int = 7
+	var r rune = 'B'
+	var s string = "this is a string"
+
+	bb := BindBool(&b)
+	bf := BindFloat(&f)
+	bi := BindInt(&i)
+	br := BindRune(&r)
+	bs := BindString(&s)
+
+	format := "Bool %v , Float %f , Int %v , Rune %v , String %s"
+	sp, err := NewSprintf(format, bb, bf, bi, br, bs)
+	expected := fmt.Sprintf(format, b, f, i, r, s)
+
+	assert.Nil(t, err)
+	assert.NotNil(t, sp)
+
+	waitForItems()
+
+	sGenerated, err := sp.Get()
+
+	assert.Nil(t, err)
+	assert.NotNil(t, sGenerated)
+	assert.Equal(t, expected, sGenerated)
+
+	err = sp.Set("Bool false , Float 7.000000 , Int 42 , Rune 67 , String nospacestring")
+
+	assert.Nil(t, err)
+
+	waitForItems()
+
+	assert.Equal(t, b, false)
+	assert.Equal(t, f, float64(7))
+	assert.Equal(t, i, 42)
+	assert.Equal(t, r, 'C')
+	assert.Equal(t, s, "nospacestring")
+
+	expectedChange := fmt.Sprintf(format, b, f, i, r, s)
+
+	sChange, err := sp.Get()
+
+	assert.Nil(t, err)
+	assert.NotNil(t, sChange)
+	assert.Equal(t, expectedChange, sChange)
+	assert.NotEqual(t, sGenerated, sChange)
+}
