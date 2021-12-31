b233decc77eaa5ed7d0e27645915eef55f69e7ae
Andy Williams <andy@fyne.io>
2021-12-30 16:56:13 -0800

    Fix resetting when button is selected

--- container/doctabs.go
+++ container/doctabs.go
@@ -64,7 +64,7 @@ func (t *DocTabs) CreateRenderer() fyne.WidgetRenderer {
 			buttonCache: make(map[*TabItem]*tabButton),
 		},
 		docTabs:  t,
-		scroller: NewScroll(nil),
+		scroller: NewScroll(&fyne.Container{}),
 	}
 	r.action = r.buildAllTabsButton()
 	r.create = r.buildCreateTabsButton()
@@ -291,8 +291,8 @@ func (r *docTabsRenderer) buildCreateTabsButton() *widget.Button {
 	return create
 }
 
-func (r *docTabsRenderer) buildTabButtons(count int) *fyne.Container {
-	buttons := &fyne.Container{}
+func (r *docTabsRenderer) buildTabButtons(count int, buttons *fyne.Container) {
+	buttons.Objects = nil
 
 	var iconPos buttonIconPosition
 	if fyne.CurrentDevice().IsMobile() {
@@ -337,7 +337,6 @@ func (r *docTabsRenderer) buildTabButtons(count int) *fyne.Container {
 		button.Refresh()
 		buttons.Objects = append(buttons.Objects, button)
 	}
-	return buttons
 }
 
 func (r *docTabsRenderer) scrollToSelected() {
@@ -443,8 +442,7 @@ func (r *docTabsRenderer) updateCreateTab() {
 
 func (r *docTabsRenderer) updateTabs() {
 	tabCount := len(r.docTabs.Items)
-
-	r.scroller.Content = r.buildTabButtons(tabCount)
+	r.buildTabButtons(tabCount, r.scroller.Content.(*fyne.Container))
 
 	// Set layout of tab bar containing tab buttons and overflow action
 	if r.docTabs.location == TabLocationLeading || r.docTabs.location == TabLocationTrailing {
