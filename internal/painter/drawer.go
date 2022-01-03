2afe97ec0db07fe3d94b483e0cb2422ff27e574c
Tilo Prütz <tilo@pruetz.net>
2022-01-03 14:13:35 +0100

    inline painter.FontDrawer
    There is no need for a type.

--- internal/painter/drawer.go
+++ internal/painter/drawer.go
@@ -14,12 +14,15 @@ import (
 
 // DrawString draws a string into an image.
 func DrawString(dst draw.Image, s string, color color.Color, face font.Face, height int, tabWidth int) {
-	d := FontDrawer{}
-	d.Dst = dst
-	d.Src = &image.Uniform{C: color}
-	d.Face = face
-	d.Dot = freetype.Pt(0, height-face.Metrics().Descent.Ceil())
-	d.DrawString(s, tabWidth)
+	src := &image.Uniform{C: color}
+	dot := freetype.Pt(0, height-face.Metrics().Descent.Ceil())
+	walkString(face, s, tabWidth, &dot.X, func(r rune) (fixed.Int26_6, bool) {
+		dr, mask, maskp, advance, ok := face.Glyph(dot, r)
+		if ok {
+			draw.DrawMask(dst, dr, src, image.Point{}, mask, maskp, draw.Over)
+		}
+		return advance, ok
+	})
 }
 
 // MeasureString returns how far dot would advance by drawing s with f.
@@ -27,29 +30,6 @@ func DrawString(dst draw.Image, s string, color color.Color, face font.Face, hei
 func MeasureString(f font.Face, s string, tabWidth int) (advance fixed.Int26_6) {
 	walkString(f, s, tabWidth, &advance, f.GlyphAdvance)
 	return
-}
-
-// FontDrawer is a simplified implementation of  "golang.org/x/image/font.Drawer" which includes
-// support for tabs.
-//
-// A FontDrawer is not safe for concurrent use by multiple goroutines, since its Face is not.
-type FontDrawer struct {
-	Dst  draw.Image
-	Src  image.Image
-	Face font.Face
-	Dot  fixed.Point26_6
-}
-
-// DrawString draws s at the dot and advances the dot's location.
-// Tabs are translated into a dot location change.
-func (d *FontDrawer) DrawString(s string, tabWidth int) {
-	walkString(d.Face, s, tabWidth, &d.Dot.X, func(r rune) (fixed.Int26_6, bool) {
-		dr, mask, maskp, advance, ok := d.Face.Glyph(d.Dot, r)
-		if ok {
-			draw.DrawMask(d.Dst, dr, d.Src, image.Point{}, mask, maskp, draw.Over)
-		}
-		return advance, ok
-	})
 }
 
 func tabStop(f font.Face, x fixed.Int26_6, tabWidth int) fixed.Int26_6 {
