9696c4da3041698b1e17f69cadb3523a8239037f
Tilo Prütz <tilo@pruetz.net>
2022-01-03 14:57:30 +0100

    add test for painter.MeasureString

--- internal/painter/drawer_test.go
+++ internal/painter/drawer_test.go
@@ -6,32 +6,34 @@ import (
 	"testing"
 
 	"github.com/goki/freetype/truetype"
+	"github.com/stretchr/testify/assert"
 	"golang.org/x/image/font"
+	"golang.org/x/image/math/fixed"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/internal/painter"
 	"fyne.io/fyne/v2/internal/test"
 )
 
-func TestDrawString(t *testing.T) {
-	regular := painter.CachedFontFace(
-		fyne.TextStyle{},
-		&truetype.Options{
-			Size: 40.0,
-			DPI:  painter.TextDPI,
-		},
-	)
-	boldItalic := painter.CachedFontFace(
-		fyne.TextStyle{
-			Bold:   true,
-			Italic: true,
-		},
-		&truetype.Options{
-			Size: 27.42,
-			DPI:  painter.TextDPI,
-		},
-	)
+var regular = painter.CachedFontFace(
+	fyne.TextStyle{},
+	&truetype.Options{
+		Size: 40.0,
+		DPI:  painter.TextDPI,
+	},
+)
+var boldItalic = painter.CachedFontFace(
+	fyne.TextStyle{
+		Bold:   true,
+		Italic: true,
+	},
+	&truetype.Options{
+		Size: 27.42,
+		DPI:  painter.TextDPI,
+	},
+)
 
+func TestDrawString(t *testing.T) {
 	for name, tt := range map[string]struct {
 		color    color.Color
 		face     font.Face
@@ -61,6 +63,33 @@ func TestDrawString(t *testing.T) {
 			img := image.NewNRGBA(image.Rect(0, 0, 300, 100))
 			painter.DrawString(img, tt.string, tt.color, tt.face, tt.height, tt.tabWidth)
 			test.AssertImageMatches(t, "font/"+tt.want, img)
+		})
+	}
+}
+
+func TestMeasureString(t *testing.T) {
+	for name, tt := range map[string]struct {
+		face     font.Face
+		string   string
+		tabWidth int
+		want     fixed.Int26_6
+	}{
+		"regular": {
+			face:     regular,
+			string:   "Hello\tworld!",
+			tabWidth: 7,
+			want:     18263, // 285.359375
+		},
+		"bold italic": {
+			face:     boldItalic,
+			string:   "Hello\tworld!",
+			tabWidth: 3,
+			want:     11576, // 180.875
+		},
+	} {
+		t.Run(name, func(t *testing.T) {
+			got := painter.MeasureString(tt.face, tt.string, tt.tabWidth)
+			assert.Equal(t, tt.want, got)
 		})
 	}
 }
