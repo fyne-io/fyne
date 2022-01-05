56be185900a17c468ee421a87bcbe856cd8bbcb1
Andy Williams <andy@fyne.io>
2022-01-04 21:21:52 -0800

    Fix compile error for wayland mobile devices

--- internal/driver/mobile/device_wayland.go
+++ internal/driver/mobile/device_wayland.go
@@ -5,6 +5,8 @@ package mobile
 
 import "fyne.io/fyne/v2"
 
+const tapYOffset = -4.0 // to compensate for how we hold our fingers on the device
+
 func (*device) SystemScaleForWindow(_ fyne.Window) float32 {
 	return 1 // PinePhone simplification, our only wayland mobile currently
 }
