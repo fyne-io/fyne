748e80b3d9911b4eaa47e0ea7db758bf5288cd3e
Andy Williams <andy@fyne.io>
2022-01-20 19:27:00 -0800

    Fix possible nil pointer on Tapped

--- widget/radio_item.go
+++ widget/radio_item.go
@@ -113,7 +113,11 @@ func (i *radioItem) SetSelected(selected bool) {
 // Implements: fyne.Tappable
 func (i *radioItem) Tapped(_ *fyne.PointEvent) {
 	if !i.focused && !fyne.CurrentDevice().IsMobile() {
-		fyne.CurrentApp().Driver().CanvasForObject(i.super()).Focus(i.super().(fyne.Focusable))
+		impl := i.super()
+
+		if c := fyne.CurrentApp().Driver().CanvasForObject(impl); c != nil {
+			c.Focus(impl.(fyne.Focusable))
+		}
 	}
 	i.toggle()
 }
