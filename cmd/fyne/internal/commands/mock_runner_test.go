25221d423b8c05b3f9d1962a003e07f528a5b210
Cedric BAIL <cedric@ddlm.me>
2022-02-18 14:31:06 -0700

    Use require instead of assert.

--- cmd/fyne/internal/commands/mock_runner_test.go
+++ cmd/fyne/internal/commands/mock_runner_test.go
@@ -5,7 +5,7 @@ import (
 	"sort"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 type mockExpectedValue struct {
@@ -39,14 +39,12 @@ type testCommandCall struct {
 
 func (t *testCommandCall) runOutput(args ...string) ([]byte, error) {
 	// Check that we have less than the expected number of call
-	assert.Less(t.t, t.index, len(t.calls))
+	require.Less(t.t, t.index, len(t.calls))
 	// Check that we have the expected number of parameters for this call
-	assert.Equal(t.t, len(t.calls[t.index].args), len(args))
+	require.Equal(t.t, len(t.calls[t.index].args), len(args))
 	// Check that each argument match our expectation
-	if len(t.calls[t.index].args) == len(args) {
-		for index, value := range args {
-			assert.Equal(t.t, t.calls[t.index].args[index], value)
-		}
+	for index, value := range args {
+		require.Equal(t.t, t.calls[t.index].args[index], value)
 	}
 
 	ret, err := t.calls[t.index].ret, t.calls[t.index].err
@@ -57,15 +55,15 @@ func (t *testCommandCall) runOutput(args ...string) ([]byte, error) {
 
 func (t *testCommandCall) setDir(dir string) {
 	// Check that we have less than the expected number of call
-	assert.Less(t.t, t.index, len(t.calls))
+	require.Less(t.t, t.index, len(t.calls))
 
-	assert.Equal(t.t, t.calls[t.index].dir.(string), dir)
+	require.Equal(t.t, t.calls[t.index].dir.(string), dir)
 	t.calls[t.index].dirSet = true
 }
 
 func (t *testCommandCall) setEnv(env []string) {
 	// Check that we have less than the expected number of call
-	assert.Less(t.t, t.index, len(t.calls))
+	require.Less(t.t, t.index, len(t.calls))
 
 	// Prepare array for comparison
 	expectedEnv := t.calls[t.index].env
@@ -76,26 +74,24 @@ func (t *testCommandCall) setEnv(env []string) {
 	sort.Strings(env)
 
 	// First check length of expected and passed environment are equal
-	assert.Equal(t.t, len(expectedEnv), len(env))
-	if len(expectedEnv) == len(env) {
-		// Check each independent environement variable match our expectation
-		for index, value := range env {
-			assert.Equal(t.t, expectedEnv[index], value)
-		}
+	require.Equal(t.t, len(expectedEnv), len(env))
+	// Check each independent environement variable match our expectation
+	for index, value := range env {
+		require.Equal(t.t, expectedEnv[index], value)
 	}
 	t.calls[t.index].envSet = true
 }
 
 func (t *testCommandCall) verifyExpectation() {
 	// Expected as many call as we got
-	assert.Equal(t.t, len(t.calls), t.index)
+	require.Equal(t.t, len(t.calls), t.index)
 	// Check if every call really matched our expectaction
 	for _, value := range t.calls {
 		if value.dir != nil {
-			assert.Equal(t.t, true, value.dirSet)
+			require.Equal(t.t, true, value.dirSet)
 		}
 		if len(value.env) > 0 {
-			assert.Equal(t.t, true, value.envSet)
+			require.Equal(t.t, true, value.envSet)
 		}
 	}
 }
