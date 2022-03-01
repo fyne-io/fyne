3ddda4e513fa21d61489e5784bb869a3ce9888bf
Andy Williams <andy@andywilliams.me>
2022-03-01 09:59:30 +0000

    Fix broken align

--- internal/app/meta.go
+++ internal/app/meta.go
@@ -2,8 +2,8 @@ package app
 
 // these internal variables are set by the fyne build command so that the "FyneApp.toml" data is readable at runtime.
 var (
-	MetaID = "com.example"
-	MetaName = "Fyne App"
+	MetaID      = "com.example"
+	MetaName    = "Fyne App"
 	MetaVersion = "1.0.0"
-	MetaBuild = "1"
+	MetaBuild   = "1"
 )
