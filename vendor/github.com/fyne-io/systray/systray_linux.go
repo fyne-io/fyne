374365726b5774af5a139ee952386e497fe73682
Andy Williams <andy@fyne.io>
2022-03-04 20:46:05 +0000

    Update to Linux supported systray

--- vendor/github.com/fyne-io/systray/systray_linux.go
+++ vendor/github.com/fyne-io/systray/systray_linux.go
@@ -1,3 +1,7 @@
+//Note that you need to have github.com/knightpp/dbus-codegen-go installed from "custom" branch
+//go:generate dbus-codegen-go -prefix org.kde -package notifier -output internal/generated/notifier/status_notifier_item.go internal/StatusNotifierItem.xml
+//go:generate dbus-codegen-go -prefix com.canonical -package menu -output internal/generated/menu/dbus_menu.go internal/DbusMenu.xml
+
 package systray
 
 import (
@@ -8,9 +12,16 @@ import (
 	"log"
 	"os"
 
+	"github.com/fyne-io/systray/internal/generated/menu"
+	"github.com/fyne-io/systray/internal/generated/notifier"
 	"github.com/godbus/dbus/v5"
 	"github.com/godbus/dbus/v5/introspect"
 	"github.com/godbus/dbus/v5/prop"
+)
+
+const (
+	path     = "/StatusNotifierItem"
+	menuPath = "/StatusNotifierMenu"
 )
 
 var (
@@ -22,6 +33,9 @@ var (
 
 	// the title of our system tray icon
 	title string
+
+	// instance is the current instance of our DBus tray server
+	instance *tray
 )
 
 // SetTemplateIcon sets the systray icon as a template icon (on macOS), falling back
@@ -32,7 +46,6 @@ func SetTemplateIcon(templateIconBytes []byte, regularIconBytes []byte) {
 	// TODO handle the templateIconBytes?
 	iconData = regularIconBytes
 }
-
 
 // SetIcon sets the systray icon.
 // iconBytes should be the content of .ico for windows and .ico/.jpg/.png
@@ -51,19 +64,11 @@ func SetTitle(t string) {
 func SetTooltip(tooltip string) {
 }
 
-// SetIcon sets the icon of a menu item. Only works on macOS and Windows.
-// iconBytes should be the content of .ico/.jpg/.png
-func (item *MenuItem) SetIcon(iconBytes []byte) {
-}
-
 // SetTemplateIcon sets the icon of a menu item as a template icon (on macOS). On Windows, it
 // falls back to the regular icon bytes and on Linux it does nothing.
 // templateIconBytes and regularIconBytes should be the content of .ico for windows and
 // .ico/.jpg/.png for other platforms.
 func (item *MenuItem) SetTemplateIcon(templateIconBytes []byte, regularIconBytes []byte) {
-}
-
-func addOrUpdateMenuItem(item *MenuItem) {
 }
 
 func setInternalLoop(_ bool) {
@@ -73,19 +78,10 @@ func setInternalLoop(_ bool) {
 func registerSystray() {
 }
 
-func addSeparator(id uint32) {
-}
-
-func hideMenuItem(item *MenuItem) {
-}
-
-func showMenuItem(item *MenuItem) {
-}
-
 func nativeLoop() int {
 	nativeStart()
 	select {
-	case <- quitChan:
+	case <-quitChan:
 		break
 	}
 	nativeEnd()
@@ -94,6 +90,7 @@ func nativeLoop() int {
 
 func nativeEnd() {
 	systrayExit()
+	instance.conn.Close()
 }
 
 func quit() {
@@ -102,116 +99,61 @@ func quit() {
 
 func nativeStart() {
 	systrayReady()
-
-	intro := introspect.Introspectable(`<node>
-<!--  Based on:
- https://invent.kde.org/frameworks/knotifications/-/blob/master/src/org.kde.StatusNotifierItem.xml
- -->
-<interface name="org.kde.StatusNotifierItem">
-<property name="Category" type="s" access="read"/>
-<property name="Id" type="s" access="read"/>
-<property name="Title" type="s" access="read"/>
-<property name="Status" type="s" access="read"/>
-<property name="WindowId" type="i" access="read"/>
-<!--  An additional path to add to the theme search path to find the icons specified above.  -->
-<property name="IconThemePath" type="s" access="read"/>
-<property name="Menu" type="o" access="read"/>
-<property name="ItemIsMenu" type="b" access="read"/>
-<!--  main icon  -->
-<!--  names are preferred over pixmaps  -->
-<property name="IconName" type="s" access="read"/>
-<!-- struct containing width, height and image data -->
-<property name="IconPixmap" type="a(iiay)" access="read">
-<annotation name="org.qtproject.QtDBus.QtTypeName" value="KDbusImageVector"/>
-</property>
-<property name="OverlayIconName" type="s" access="read"/>
-<property name="OverlayIconPixmap" type="a(iiay)" access="read">
-<annotation name="org.qtproject.QtDBus.QtTypeName" value="KDbusImageVector"/>
-</property>
-<!--  Requesting attention icon  -->
-<property name="AttentionIconName" type="s" access="read"/>
-<!-- same definition as image -->
-<property name="AttentionIconPixmap" type="a(iiay)" access="read">
-<annotation name="org.qtproject.QtDBus.QtTypeName" value="KDbusImageVector"/>
-</property>
-<property name="AttentionMovieName" type="s" access="read"/>
-<!--  tooltip data  -->
-<!-- (iiay) is an image -->
-<!--  We disable this as we don't support tooltip, so no need to go through it
-    <property name="ToolTip" type="(sa(iiay)ss)" access="read">
-        <annotation name="org.qtproject.QtDBus.QtTypeName" value="KDbusToolTipStruct"/>
-    </property>
-     -->
-<!--  interaction: the systemtray wants the application to do something  -->
-<method name="ContextMenu">
-<!--  we're passing the coordinates of the icon, so the app knows where to put the popup window  -->
-<arg name="x" type="i" direction="in"/>
-<arg name="y" type="i" direction="in"/>
-</method>
-<method name="Activate">
-<arg name="x" type="i" direction="in"/>
-<arg name="y" type="i" direction="in"/>
-</method>
-<method name="SecondaryActivate">
-<arg name="x" type="i" direction="in"/>
-<arg name="y" type="i" direction="in"/>
-</method>
-<method name="Scroll">
-<arg name="delta" type="i" direction="in"/>
-<arg name="orientation" type="s" direction="in"/>
-</method>
-<!--  Signals: the client wants to change something in the status -->
-<signal name="NewTitle"> </signal>
-<signal name="NewIcon"> </signal>
-<signal name="NewAttentionIcon"> </signal>
-<signal name="NewOverlayIcon"> </signal>
-<!--  We disable this as we don't support tooltip, so no need to go through it
-    <signal name="NewToolTip">
-    </signal>
-     -->
-<signal name="NewStatus">
-<arg name="status" type="s"/>
-</signal>
-<!--  The following items are not supported by specs, but widely used  -->
-<signal name="NewIconThemePath">
-<arg type="s" name="icon_theme_path" direction="out"/>
-</signal>
-<signal name="NewMenu"/>
-<!--  ayatana labels  -->
-<!--  These are commented out because GDBusProxy would otherwise require them,
-         but they are not available for KDE indicators
-     -->
-<!-- <signal name="XAyatanaNewLabel">
-        <arg type="s" name="label" direction="out" />
-        <arg type="s" name="guide" direction="out" />
-    </signal>
-    <property name="XAyatanaLabel" type="s" access="read" />
-    <property name="XAyatanaLabelGuide" type="s" access="read" /> -->
-</interface>`+ introspect.IntrospectDataString+prop.IntrospectDataString+"</node>")
+	instance = &tray{menu: &menuLayout{}}
 
 	conn, _ := dbus.ConnectSessionBus()
-	conn.Export(introspect.Introspectable(intro), "/StatusNotifierItem",
-		"org.freedesktop.DBus.Introspectable")
-
-	_, err := prop.Export(conn, "/StatusNotifierItem", createPropSpec())
-	if err != nil {
-		log.Printf("Failed to export notifier item properties to bus")
-		return
-	}
-	err = conn.Export(&tray{}, "/StatusNotifierItem", "org.kde.StatusNotifierItem")
-	if err != nil {
-		log.Printf("Failed to export notifier item to bus")
-		return
-	}
+	instance.conn = conn
+	notifier.ExportStatusNotifierItem(conn, path, instance)
+	menu.ExportDbusmenu(conn, menuPath, instance)
 
 	name := fmt.Sprintf("org.kde.StatusNotifierItem-%d-1", os.Getpid()) // register id 1 for this process
-	_, err = conn.RequestName(name, dbus.NameFlagDoNotQueue)
+	_, err := conn.RequestName(name, dbus.NameFlagDoNotQueue)
 	if err != nil {
 		// fall back to existing name
 		name = conn.Names()[0]
 	}
 
-	obj := conn.Object("org.kde.StatusNotifierWatcher", dbus.ObjectPath("/StatusNotifierWatcher"))
+	_, err = prop.Export(conn, path, createPropSpec())
+	if err != nil {
+		log.Printf("Failed to export notifier item properties to bus")
+		return
+	}
+	_, err = prop.Export(conn, menuPath, createMenuPropSpec())
+	if err != nil {
+		log.Printf("Failed to export notifier menu properties to bus")
+		return
+	}
+
+	node := introspect.Node{
+		Name: path,
+		Interfaces: []introspect.Interface{
+			introspect.IntrospectData,
+			prop.IntrospectData,
+			notifier.IntrospectDataStatusNotifierItem,
+		},
+	}
+	err = conn.Export(introspect.NewIntrospectable(&node), path,
+		"org.freedesktop.DBus.Introspectable")
+	if err != nil {
+		log.Printf("Failed to export introspection")
+		return
+	}
+	menuNode := introspect.Node{
+		Name: menuPath,
+		Interfaces: []introspect.Interface{
+			introspect.IntrospectData,
+			prop.IntrospectData,
+			menu.IntrospectDataDbusmenu,
+		},
+	}
+	err = conn.Export(introspect.NewIntrospectable(&menuNode), menuPath,
+		"org.freedesktop.DBus.Introspectable")
+	if err != nil {
+		log.Printf("Failed to export introspection")
+		return
+	}
+
+	obj := conn.Object("org.kde.StatusNotifierWatcher", "/StatusNotifierWatcher")
 	call := obj.Call("org.kde.StatusNotifierWatcher.RegisterStatusNotifierItem", 0, name)
 	if call.Err != nil {
 		log.Printf("Failed to register our icon with the notifier watcher, maybe no tray running?")
@@ -220,28 +162,29 @@ func nativeStart() {
 
 // tray is a basic type that handles the dbus functionality
 type tray struct {
-	prop.Properties
+	conn *dbus.Conn
+	menu *menuLayout
 }
 
 // ContextMenu method is called when the user has right-clicked on our icon.
-func (t *tray) ContextMenu(x, y int) *dbus.Error {
+func (t *tray) ContextMenu(x, y int32) *dbus.Error {
 	// not supported for systray lib
 	return nil
 }
 
 // Activate requests that we perform the primary action, such as showing a menu.
-func (t *tray) Activate(x, y int) *dbus.Error {
+func (t *tray) Activate(x, y int32) *dbus.Error {
 	// TODO show menu, or have it handled in the dbus?
 	return nil
 }
 
 // SecondaryActivate is alternative non-context click, such as middle mouse button.
-func (t *tray) SecondaryActivate(x, y int) *dbus.Error {
+func (t *tray) SecondaryActivate(x, y int32) *dbus.Error {
 	return nil
 }
 
 // Scroll is called when the mouse wheel scrolls over the icon.
-func (t *tray) Scroll(delta int, orient string) *dbus.Error {
+func (t *tray) Scroll(delta int32, orient string) *dbus.Error {
 	return nil
 }
 
@@ -255,29 +198,33 @@ func convertToPixels(data []byte) PX {
 		return PX{}
 	}
 
-	p, _, err := image.Decode(bytes.NewReader(iconData))
+	img, _, err := image.Decode(bytes.NewReader(iconData))
 	if err != nil {
 		log.Printf("Failed to read icon format %v", err)
 		return PX{}
 	}
 
-	w, h := p.Bounds().Dx(), p.Bounds().Dy()
-	data2 := make([]byte, w*h*4)
+	return PX{
+		img.Bounds().Dx(), img.Bounds().Dy(),
+		argbForImage(img),
+	}
+}
+
+func argbForImage(img image.Image) []byte {
+	w, h := img.Bounds().Dx(), img.Bounds().Dy()
+	data := make([]byte, w*h*4)
 	i := 0
 	for y := 0; y < h; y++ {
 		for x := 0; x < w; x++ {
-			r, g, b, a := p.At(x, y).RGBA()
-			data2[i] = byte(a)
-			data2[i+1] = byte(r)
-			data2[i+2] = byte(g)
-			data2[i+3] = byte(b)
+			r, g, b, a := img.At(x, y).RGBA()
+			data[i] = byte(a)
+			data[i+1] = byte(r)
+			data[i+2] = byte(g)
+			data[i+3] = byte(b)
 			i += 4
 		}
 	}
-	return PX{
-		w, h,
-		data2,
-	}
+	return data
 }
 
 func createPropSpec() map[string]map[string]*prop.Prop {
@@ -321,6 +268,12 @@ func createPropSpec() map[string]map[string]*prop.Prop {
 			},
 			"IconThemePath": {
 				"",
+				false,
+				prop.EmitTrue,
+				nil,
+			},
+			"Menu": {
+				menuPath,
 				false,
 				prop.EmitTrue,
 				nil,
---
+++ vendor/github.com/fyne-io/systray/systray_menu_linux.go
@@ -0,0 +1,205 @@
+package systray
+
+import (
+	"log"
+
+	"github.com/fyne-io/systray/internal/generated/menu"
+	"github.com/godbus/dbus/v5"
+	"github.com/godbus/dbus/v5/prop"
+)
+
+// SetIcon sets the icon of a menu item. Only works on macOS and Windows.
+// iconBytes should be the content of .ico/.jpg/.png
+func (item *MenuItem) SetIcon(iconBytes []byte) {
+}
+
+func (t *tray) GetLayout(parentId int32, recursionDepth int32, propertyNames []string) (revision uint32, layout menuLayout, err *dbus.Error) {
+	return 1, *instance.menu, nil
+}
+
+// GetGroupProperties is com.canonical.dbusmenu.GetGroupProperties method.
+func (t *tray) GetGroupProperties(ids []int32, propertyNames []string) (properties []struct {
+	V0 int32
+	V1 map[string]dbus.Variant
+}, err *dbus.Error) {
+	return
+}
+
+// GetProperty is com.canonical.dbusmenu.GetProperty method.
+func (t *tray) GetProperty(id int32, name string) (value dbus.Variant, err *dbus.Error) {
+	return
+}
+
+// Event is com.canonical.dbusmenu.Event method.
+func (t *tray) Event(id int32, eventId string, data dbus.Variant, timestamp uint32) (err *dbus.Error) {
+	if eventId == "clicked" {
+		item, ok := menuItems[uint32(id)]
+		if !ok {
+			log.Printf("Failed to look up clicked menu item")
+			return
+		}
+
+		item.ClickedCh <- struct{}{}
+	}
+	return
+}
+
+// EventGroup is com.canonical.dbusmenu.EventGroup method.
+func (t *tray) EventGroup(events []struct {
+	V0 int32
+	V1 string
+	V2 dbus.Variant
+	V3 uint32
+}) (idErrors []int32, err *dbus.Error) {
+	return
+}
+
+// AboutToShow is com.canonical.dbusmenu.AboutToShow method.
+func (t *tray) AboutToShow(id int32) (needUpdate bool, err *dbus.Error) {
+	return
+}
+
+// AboutToShowGroup is com.canonical.dbusmenu.AboutToShowGroup method.
+func (t *tray) AboutToShowGroup(ids []int32) (updatesNeeded []int32, idErrors []int32, err *dbus.Error) {
+	return
+}
+
+func createMenuPropSpec() map[string]map[string]*prop.Prop {
+	return map[string]map[string]*prop.Prop{
+		"com.canonical.dbusmenu": {
+			"Version": {
+				uint32(1),
+				false,
+				prop.EmitTrue,
+				nil,
+			},
+			"TextDirection": {
+				"ltr",
+				false,
+				prop.EmitTrue,
+				nil,
+			},
+			"Status": {
+				"active",
+				false,
+				prop.EmitTrue,
+				nil,
+			},
+			"IconThemePath": {
+				[]string{},
+				false,
+				prop.EmitTrue,
+				nil,
+			},
+		},
+	}
+}
+
+type menuLayout = struct {
+	V0 int32
+	V1 map[string]dbus.Variant
+	V2 []dbus.Variant
+}
+
+func addOrUpdateMenuItem(item *MenuItem) {
+	var layout *menuLayout
+	m, exists := findLayout(int32(item.id))
+	if exists {
+		layout = m
+	} else {
+		layout = &menuLayout{
+			V0: int32(item.id),
+			V1: map[string]dbus.Variant{},
+			V2: []dbus.Variant{},
+		}
+
+		parent := instance.menu
+		if item.parent != nil {
+			m, ok := findLayout(int32(item.parent.id))
+			if ok {
+				parent = m
+				parent.V1["children-display"] = dbus.MakeVariant("submenu")
+			}
+		}
+		parent.V2 = append(parent.V2, dbus.MakeVariant(layout))
+	}
+
+	applyItemToLayout(item, layout)
+	if exists {
+		refresh()
+	}
+}
+
+func addSeparator(id uint32) {
+	layout := &menuLayout{
+		V0: int32(id),
+		V1: map[string]dbus.Variant{
+			"type": dbus.MakeVariant("separator"),
+		},
+		V2: []dbus.Variant{},
+	}
+
+	instance.menu.V2 = append(instance.menu.V2, dbus.MakeVariant(layout))
+}
+
+func applyItemToLayout(in *MenuItem, out *menuLayout) {
+	out.V1["enabled"] = dbus.MakeVariant(!in.disabled)
+	out.V1["label"] = dbus.MakeVariant(in.title)
+
+	if in.isCheckable {
+		out.V1["toggle-type"] = dbus.MakeVariant("checkmark")
+		if in.checked {
+			out.V1["toggle-state"] = dbus.MakeVariant(1)
+		} else {
+			out.V1["toggle-state"] = dbus.MakeVariant(0)
+		}
+	} else {
+		out.V1["toggle-type"] = dbus.MakeVariant("")
+		out.V1["toggle-state"] = dbus.MakeVariant(0)
+	}
+}
+
+func findLayout(id int32) (*menuLayout, bool) {
+	return findSubLayout(id, instance.menu.V2)
+}
+
+func findSubLayout(id int32, vals []dbus.Variant) (*menuLayout, bool) {
+	for _, i := range vals {
+		item := i.Value().(*menuLayout)
+		if item.V0 == id {
+			return item, true
+		}
+
+		if len(item.V2) > 0 {
+			child, ok := findSubLayout(id, item.V2)
+			if ok {
+				return child, true
+			}
+		}
+	}
+
+	return nil, false
+}
+
+func hideMenuItem(item *MenuItem) {
+	m, exists := findLayout(int32(item.id))
+	if exists {
+		m.V1["visible"] = dbus.MakeVariant(false)
+		refresh()
+	}
+}
+
+func showMenuItem(item *MenuItem) {
+	m, exists := findLayout(int32(item.id))
+	if exists {
+		m.V1["visible"] = dbus.MakeVariant(true)
+		refresh()
+	}
+}
+
+func refresh() {
+	menu.Emit(instance.conn, &menu.Dbusmenu_LayoutUpdatedSignal{
+		Path: menuPath,
+		Body: &menu.Dbusmenu_LayoutUpdatedSignalBody{},
+	})
+}
