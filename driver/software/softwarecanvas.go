ca59b8ba0cb9b2d737d675931ff80ec955e82f19
Andy Williams <andy@fyne.io>
2022-03-19 20:48:14 +0000

    Support creating a transparent software canvas
    Cool for capturing images of transparent widgets

--- driver/software/softwarecanvas.go
+++ driver/software/softwarecanvas.go
@@ -5,7 +5,14 @@ import (
 	"fyne.io/fyne/v2/test"
 )
 
-// NewCanvas creates a new canvas in memory that can render without hardware support
+// NewCanvas creates a new canvas in memory that can render without hardware support.
 func NewCanvas() test.WindowlessCanvas {
 	return test.NewCanvasWithPainter(software.NewPainter())
+}
+
+// NewTransparentCanvas creates a new canvas in memory that can render without hardware support without a background color.
+//
+// Since: 2.2
+func NewTransparentCanvas() test.WindowlessCanvas {
+	return test.NewTransparentCanvasWithPainter(software.NewPainter())
 }
