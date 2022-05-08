0f13bfe0f42fd8d89703d9acdaa68ad7df9478ee
Andy Williams <andy@andy.xyz>
2022-05-08 17:05:37 +0100

    Don't crash on macOS native menu shortcut when window is not set yet

--- cmd/fyne_demo/main.go
+++ cmd/fyne_demo/main.go
@@ -239,6 +239,14 @@ func makeNav(setTutorial func(tutorial tutorials.Tutorial), loadPrevious bool) f
 }
 
 func shortcutFocused(s fyne.Shortcut, w fyne.Window) {
+	switch sh := s.(type) {
+	case *fyne.ShortcutCopy:
+		sh.Clipboard = w.Clipboard()
+	case *fyne.ShortcutCut:
+		sh.Clipboard = w.Clipboard()
+	case *fyne.ShortcutPaste:
+		sh.Clipboard = w.Clipboard()
+	}
 	if focused, ok := w.Canvas().Focused().(fyne.Shortcutable); ok {
 		focused.TypedShortcut(s)
 	}
