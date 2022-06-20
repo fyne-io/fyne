2ade38fe1a31ed40a1031adc6d8f8cd5e84806dd
Andy Williams <andy@fyne.io>
2022-06-20 09:55:02 +0100

    How did a space slip in?

--- cmd/fyne/internal/commands/package_test.go
+++ cmd/fyne/internal/commands/package_test.go
@@ -27,7 +27,7 @@ func Test_fixedVersionInfo(t *testing.T) {
 		ver   string
 		fixed goversioninfo.FileVersion
 	}{
-		{"",  goversioninfo.FileVersion{Major: 0, Minor: 0, Patch: 0, Build: 1}},
+		{"", goversioninfo.FileVersion{Major: 0, Minor: 0, Patch: 0, Build: 1}},
 		{"1.1.1.1", goversioninfo.FileVersion{Major: 1, Minor: 1, Patch: 1, Build: 1}},
 		{"2.2.2", goversioninfo.FileVersion{Major: 2, Minor: 2, Patch: 2, Build: 1}},
 		{"3.3.3.3.3", goversioninfo.FileVersion{Major: 3, Minor: 3, Patch: 3, Build: 3}},
