054990bd8981000b8150b762d759d8f28a5fa5d6
George Leman <gleman@mailgun.com>
2022-07-23 01:41:53 -0400

    Fix: use MinSize() of current tab if width > Size(); fix markup expectations in tests"

--- container/doctabs.go
+++ container/doctabs.go
@@ -372,11 +372,17 @@ func (r *docTabsRenderer) updateIndicator(animate bool) {
 		if a := r.action; a != nil {
 			selectedPos = a.Position()
 			selectedSize = a.Size()
+			if a.MinSize().Width > selectedSize.Width {
+				selectedSize = a.MinSize()
+			}
 		}
 	} else {
 		selected := buttons[r.docTabs.current]
 		selectedPos = selected.Position()
 		selectedSize = selected.Size()
+		if selected.MinSize().Width > selectedSize.Width {
+			selectedSize = selected.MinSize()
+		}
 	}
 
 	scrollOffset := r.scroller.Offset
