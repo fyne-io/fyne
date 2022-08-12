bc3636bc894eb7c34cc20d1aa939118e6006992a
jiawei <b33b33o11@gmail.com>
2022-08-12 23:59:12 +0800

    test: updated tests to include creating folders

--- dialog/file_test.go
+++ dialog/file_test.go
@@ -269,6 +269,9 @@ func TestHiddenFiles(t *testing.T) {
 
 	ui := popup.Content.(*fyne.Container)
 
+	createNewFolderButton := ui.Objects[1].(*fyne.Container).Objects[0].(*fyne.Container).Objects[0].(*widget.Button)
+	assert.Equal(t, "", createNewFolderButton.Text)
+	assert.Equal(t, theme.FolderNewIcon(), createNewFolderButton.Icon)
 	toggleViewButton := ui.Objects[1].(*fyne.Container).Objects[0].(*fyne.Container).Objects[1].(*widget.Button)
 	assert.Equal(t, "", toggleViewButton.Text)
 	assert.Equal(t, theme.ListIcon(), toggleViewButton.Icon)
@@ -560,4 +563,50 @@ func TestSetFileNameAfterShow(t *testing.T) {
 
 	assert.NotEqual(t, "testfile.zip", dOpen.dialog.fileName.(*widget.Label).Text)
 
+}
+
+func TestCreateNewFolderInDir(t *testing.T) {
+	win := test.NewWindow(widget.NewLabel("Content"))
+
+	folderDialog := NewFolderOpen(func(lu fyne.ListableURI, err error) {
+		assert.Nil(t, err)
+	}, win)
+	folderDialog.SetConfirmText("Choose")
+	folderDialog.SetDismissText("Cancel")
+	folderDialog.Show()
+
+	folderDialogPopup := win.Canvas().Overlays().Top().(*widget.PopUp)
+	defer win.Canvas().Overlays().Remove(folderDialogPopup)
+	assert.NotNil(t, folderDialogPopup)
+
+	folderDialogUi := folderDialogPopup.Content.(*fyne.Container)
+
+	createNewFolderButton := folderDialogUi.Objects[1].(*fyne.Container).Objects[0].(*fyne.Container).Objects[0].(*widget.Button)
+	assert.Equal(t, "", createNewFolderButton.Text)
+	assert.Equal(t, theme.FolderNewIcon(), createNewFolderButton.Icon)
+
+	// open folder name input dialog
+	test.Tap(createNewFolderButton)
+
+	inputPopup := win.Canvas().Overlays().Top().(*widget.PopUp)
+	defer win.Canvas().Overlays().Remove(inputPopup)
+	assert.NotNil(t, inputPopup)
+
+	folderNameInputUi := inputPopup.Content.(*fyne.Container)
+
+	folderNameInputTitle := folderNameInputUi.Objects[4].(*widget.Label)
+	assert.Equal(t, "New Folder", folderNameInputTitle.Text)
+
+	folderNameInputLabel := folderNameInputUi.Objects[2].(*fyne.Container).Objects[0].(*widget.Label)
+	assert.Equal(t, "Name", folderNameInputLabel.Text)
+
+	folderNameInputEntry := folderNameInputUi.Objects[2].(*fyne.Container).Objects[1].(*widget.Entry)
+	assert.Equal(t, "", folderNameInputEntry.Text)
+
+	folderNameInputCancel := folderNameInputUi.Objects[3].(*fyne.Container).Objects[1].(*widget.Button)
+	assert.Equal(t, "Cancel", folderNameInputCancel.Text)
+	assert.Equal(t, theme.CancelIcon(), folderNameInputCancel.Icon)
+
+	folderNameInputCreate := folderNameInputUi.Objects[3].(*fyne.Container).Objects[2].(*widget.Button)
+	assert.Equal(t, theme.ConfirmIcon(), folderNameInputCreate.Icon)
 }
