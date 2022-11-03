aab8954bd511bf1a246c46b7caf6406853eac1de
Andy Williams <andy@fyne.io>
2022-11-03 08:29:06 +0000

    Update markup renderer to know the new colours

--- test/markup_renderer.go
+++ test/markup_renderer.go
@@ -387,19 +387,23 @@ func nrgbaColor(c color.Color) color.NRGBA {
 
 func knownColor(c color.Color) string {
 	return map[color.Color]string{
-		nrgbaColor(theme.BackgroundColor()):     "background",
-		nrgbaColor(theme.ButtonColor()):         "button",
-		nrgbaColor(theme.DisabledButtonColor()): "disabled button",
-		nrgbaColor(theme.DisabledColor()):       "disabled",
-		nrgbaColor(theme.ErrorColor()):          "error",
-		nrgbaColor(theme.FocusColor()):          "focus",
-		nrgbaColor(theme.ForegroundColor()):     "foreground",
-		nrgbaColor(theme.HoverColor()):          "hover",
-		nrgbaColor(theme.PlaceHolderColor()):    "placeholder",
-		nrgbaColor(theme.PrimaryColor()):        "primary",
-		nrgbaColor(theme.ScrollBarColor()):      "scrollbar",
-		nrgbaColor(theme.SelectionColor()):      "selection",
-		nrgbaColor(theme.ShadowColor()):         "shadow",
+		nrgbaColor(theme.BackgroundColor()):        "background",
+		nrgbaColor(theme.ButtonColor()):            "button",
+		nrgbaColor(theme.DisabledButtonColor()):    "disabled button",
+		nrgbaColor(theme.DisabledColor()):          "disabled",
+		nrgbaColor(theme.ErrorColor()):             "error",
+		nrgbaColor(theme.FocusColor()):             "focus",
+		nrgbaColor(theme.ForegroundColor()):        "foreground",
+		nrgbaColor(theme.HoverColor()):             "hover",
+		nrgbaColor(theme.InputBackgroundColor()):   "inputBackground",
+		nrgbaColor(theme.InputBorderColor()):       "inputBorder",
+		nrgbaColor(theme.MenuBackgroundColor()):    "menuBackground",
+		nrgbaColor(theme.OverlayBackgroundColor()): "overlayBackground",
+		nrgbaColor(theme.PlaceHolderColor()):       "placeholder",
+		nrgbaColor(theme.PrimaryColor()):           "primary",
+		nrgbaColor(theme.ScrollBarColor()):         "scrollbar",
+		nrgbaColor(theme.SelectionColor()):         "selection",
+		nrgbaColor(theme.ShadowColor()):            "shadow",
 	}[nrgbaColor(c)]
 }
 
