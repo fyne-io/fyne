33c72fc5a79391b8d1f8039fe698ecdb58ab137d
Andy Williams <andy@fyne.io>
2023-01-12 14:04:11 +0000

    Add rounded rect support to the software renderer

--- canvas/rectangle_test.go
+++ canvas/rectangle_test.go
@@ -6,6 +6,7 @@ import (
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/driver/software"
 	"fyne.io/fyne/v2/test"
 
 	"github.com/stretchr/testify/assert"
@@ -35,4 +36,12 @@ func TestRectangle_Radius(t *testing.T) {
 
 	rect.Resize(fyne.NewSize(50, 50))
 	test.AssertObjectRendersToMarkup(t, "rounded_rect.xml", rect)
+
+	c := software.NewCanvas()
+	c.SetContent(rect)
+	c.Resize(fyne.NewSize(60, 60))
+	test.AssertRendersToImage(t, "rounded_rect_stroke.png", c)
+
+	rect.StrokeWidth = 0
+	test.AssertRendersToImage(t, "rounded_rect.png", c)
 }
