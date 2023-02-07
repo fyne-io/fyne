5043432e1baafb56b69f3d52f52bee1129299aef
Denys Ivanenko <ivanenko.denys01@gmail.com>
2023-02-07 18:32:47 +0200

    Added tests

---
+++ test/file_android_test.go
@@ -0,0 +1,73 @@
+package test
+
+import (
+	"net/url"
+	"testing"
+	"mobile"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNativeFileSave(t *testing.T) {
+	tests := []struct {
+		name        string
+		fileSave    *fileSave
+		expectedErr error
+	}{
+		{
+			name: "returns resource not found error",
+			fileSave: &fileSave{
+				uri: &url.URL{
+					String: func() string { return "not_found" },
+				},
+			},
+			expectedErr: ErrResourceNotFound,
+		},
+		{
+			name: "returns nil error",
+			fileSave: &fileSave{
+				uri: &url.URL{
+					String: func() string { return "found" },
+				},
+			},
+			expectedErr: nil,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			_, err := nativeFileSave(test.fileSave)
+			assert.Equal(t, test.expectedErr, err, "unexpected error")
+		})
+	}
+}
+
+func TestNativeFileOpen(t *testing.T) {
+	tests := []struct {
+		name        string
+		uri         *url.URL
+		expectedErr error
+	}{
+		{
+			name: "returns resource not found error",
+			uri: &url.URL{
+				String: func() string { return "not_found" },
+			},
+			expectedErr: ErrResourceNotFound,
+		},
+		{
+			name: "returns nil error",
+			uri: &url.URL{
+				String: func() string { return "found" },
+			},
+			expectedErr: nil,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			_, err := nativeFileOpen(test.uri)
+			assert.Equal(t, test.expectedErr, err, "unexpected error")
+		})
+	}
+}
+
