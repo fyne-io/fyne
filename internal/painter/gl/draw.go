0dabb7ca0cb439585d48afa1cb5428fe89e69288
Cedric BAIL <cedric@ddlm.me>
2023-02-14 15:06:41 -0700

    Switch font rendering to Gray image to reduce peak memory consumption.

--- internal/painter/gl/draw.go
+++ internal/painter/gl/draw.go
@@ -7,6 +7,7 @@ import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/canvas"
 	paint "fyne.io/fyne/v2/internal/painter"
+	"fyne.io/fyne/v2/theme"
 )
 
 func (p *painter) createBuffer(points []float32) Buffer {
@@ -123,9 +124,44 @@ func (p *painter) drawText(text *canvas.Text, pos fyne.Position, frame fyne.Size
 		pos = fyne.NewPos(pos.X, pos.Y+(containerSize.Height-size.Height)/2)
 	}
 
+	color := text.Color
+	if color == nil {
+		color = theme.ForegroundColor()
+	}
+
 	// text size is sensitive to position on screen
 	size, _ = roundToPixelCoords(size, text.Position(), p.pixScale)
-	p.drawTextureWithDetails(text, p.newGlTextTexture, pos, size, frame, canvas.ImageFillStretch, 1.0, 0)
+	p.drawGrayTexture(text, p.newGlTextTexture, pos, size, frame, color, 0)
+}
+
+func (p *painter) drawGrayTexture(o fyne.CanvasObject, creator func(canvasObject fyne.CanvasObject) Texture,
+	pos fyne.Position, size, frame fyne.Size, c color.Color, pad float32) {
+	texture, err := p.getTexture(o, creator)
+	if err != nil {
+		return
+	}
+
+	points := p.rectCoords(size, pos, frame, canvas.ImageFillStretch, 0, pad)
+	p.ctx.UseProgram(p.grayProgram)
+	vbo := p.createBuffer(points)
+	p.defineVertexArray(p.grayProgram, "vert", 3, 5, 0)
+	p.defineVertexArray(p.grayProgram, "vertTexCoord", 2, 5, 3)
+
+	p.ctx.BlendFunc(srcAlpha, oneMinusSrcAlpha)
+	p.logError()
+
+	shaderColor := p.ctx.GetUniformLocation(p.grayProgram, "color")
+	r, g, b, a := getFragmentColor(c)
+	p.ctx.Uniform4f(shaderColor, r, g, b, a)
+
+	p.ctx.ActiveTexture(texture0)
+	p.ctx.BindTexture(texture2D, texture)
+	p.logError()
+
+	p.ctx.DrawArrays(triangleStrip, 0, 4)
+	p.logError()
+	p.freeBuffer(vbo)
+
 }
 
 func (p *painter) drawTextureWithDetails(o fyne.CanvasObject, creator func(canvasObject fyne.CanvasObject) Texture,
