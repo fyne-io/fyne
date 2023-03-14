501be5d00e783b5a8a60d591d48429ffbe063ac5
Andy Williams <andy@ndy.xyz>
2023-03-14 16:31:59 +0000

    Update systray impl for submenu fixes

--- vendor/fyne.io/systray/systray_darwin.m
+++ vendor/fyne.io/systray/systray_darwin.m
@@ -170,9 +170,14 @@ NSMenuItem *find_menu_item(NSMenu *ourMenu, NSNumber *menuId) {
   return NULL;
 };
 
-- (void) add_separator:(NSNumber*) menuId
+- (void) add_separator:(NSNumber*) parentMenuId
 {
-  [menu addItem: [NSMenuItem separatorItem]];
+  NSMenuItem* menuItem = find_menu_item(menu, parentMenuId);
+  if (menuItem != NULL) {
+    [menuItem.submenu addItem: [NSMenuItem separatorItem]];
+  } else {
+    [menu addItem: [NSMenuItem separatorItem]];
+  }
 }
 
 - (void) hide_menu_item:(NSNumber*) menuId
@@ -314,9 +319,9 @@ void add_or_update_menu_item(int menuId, int parentMenuId, char* title, char* to
   runInMainThread(@selector(add_or_update_menu_item:), (id)item);
 }
 
-void add_separator(int menuId) {
-  NSNumber *mId = [NSNumber numberWithInt:menuId];
-  runInMainThread(@selector(add_separator:), (id)mId);
+void add_separator(int menuId, int parentId) {
+  NSNumber *pId = [NSNumber numberWithInt:parentId];
+  runInMainThread(@selector(add_separator:), (id)pId);
 }
 
 void hide_menu_item(int menuId) {
