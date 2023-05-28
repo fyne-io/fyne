c2441fcb16cb9f895a5b4021638c6f618cce1675
Drew Weymouth <dweymouth@gmail.com>
2023-05-28 16:15:07 -0700

    rename and change build guards

--- internal/driver/glfw/scroll_speed_default.go
+++ internal/driver/glfw/scroll_speed_default.go
@@ -1,10 +1,10 @@
-//go:build !(windows || linux || freebsd || netbsd || openbsd)
-// +build !windows,!linux,!freebsd,!netbsd,!openbsd
+//go:build !darwin
+// +build !darwin
 
 package glfw
 
 const (
-	scrollAccelerateRate   = float64(5)
-	scrollAccelerateCutoff = float64(5)
-	scrollSpeed            = float32(10)
+	scrollAccelerateRate   = float64(125)
+	scrollAccelerateCutoff = float64(10)
+	scrollSpeed            = float32(25)
 )
