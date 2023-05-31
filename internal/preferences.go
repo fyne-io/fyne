97cde49cef572d43031edd533d52724ec43bc3d0
Andy Williams <andy@andy.xyz>
2023-05-31 22:23:56 +0100

    Fix public method order

--- internal/preferences.go
+++ internal/preferences.go
@@ -26,29 +26,6 @@ func (p *InMemoryPreferences) AddChangeListener(listener func()) {
 	p.changeListeners = append(p.changeListeners, listener)
 }
 
-// ChangeListeners returns the list of listeners registered for this set of preferences.
-func (p *InMemoryPreferences) ChangeListeners() []func() {
-	return p.changeListeners
-}
-
-// ReadValues provides read access to the underlying value map - for internal use only...
-// You should not retain a reference to the map nor write to the values in the callback function
-func (p *InMemoryPreferences) ReadValues(fn func(map[string]interface{})) {
-	p.lock.RLock()
-	fn(p.values)
-	p.lock.RUnlock()
-}
-
-// WriteValues provides write access to the underlying value map - for internal use only...
-// You should not retain a reference to the map passed to the callback function
-func (p *InMemoryPreferences) WriteValues(fn func(map[string]interface{})) {
-	p.lock.Lock()
-	fn(p.values)
-	p.lock.Unlock()
-
-	p.fireChange()
-}
-
 // Bool looks up a boolean value for the key
 func (p *InMemoryPreferences) Bool(key string) bool {
 	return p.BoolWithFallback(key, false)
@@ -81,13 +58,9 @@ func (p *InMemoryPreferences) BoolWithFallback(key string, fallback bool) bool {
 	return val
 }
 
-// SetBool saves a boolean value for the given key
-func (p *InMemoryPreferences) SetBool(key string, value bool) {
-	p.set(key, value)
-}
-
-func (p *InMemoryPreferences) SetBoolList(key string, value []bool) {
-	p.set(key, value)
+// ChangeListeners returns the list of listeners registered for this set of preferences.
+func (p *InMemoryPreferences) ChangeListeners() []func() {
+	return p.changeListeners
 }
 
 // Float looks up a float64 value for the key
@@ -120,15 +93,6 @@ func (p *InMemoryPreferences) FloatWithFallback(key string, fallback float64) fl
 		return 0.0
 	}
 	return val
-}
-
-// SetFloat saves a float64 value for the given key
-func (p *InMemoryPreferences) SetFloat(key string, value float64) {
-	p.set(key, value)
-}
-
-func (p *InMemoryPreferences) SetFloatList(key string, value []float64) {
-	p.set(key, value)
 }
 
 // Int looks up an integer value for the key
@@ -176,12 +140,52 @@ func (p *InMemoryPreferences) IntWithFallback(key string, fallback int) int {
 	return int(val)
 }
 
+// ReadValues provides read access to the underlying value map - for internal use only...
+// You should not retain a reference to the map nor write to the values in the callback function
+func (p *InMemoryPreferences) ReadValues(fn func(map[string]interface{})) {
+	p.lock.RLock()
+	fn(p.values)
+	p.lock.RUnlock()
+}
+
+// RemoveValue deletes a value on the given key
+func (p *InMemoryPreferences) RemoveValue(key string) {
+	p.remove(key)
+}
+
+// SetBool saves a boolean value for the given key
+func (p *InMemoryPreferences) SetBool(key string, value bool) {
+	p.set(key, value)
+}
+
+func (p *InMemoryPreferences) SetBoolList(key string, value []bool) {
+	p.set(key, value)
+}
+
+// SetFloat saves a float64 value for the given key
+func (p *InMemoryPreferences) SetFloat(key string, value float64) {
+	p.set(key, value)
+}
+
+func (p *InMemoryPreferences) SetFloatList(key string, value []float64) {
+	p.set(key, value)
+}
+
 // SetInt saves an integer value for the given key
 func (p *InMemoryPreferences) SetInt(key string, value int) {
 	p.set(key, value)
 }
 
 func (p *InMemoryPreferences) SetIntList(key string, value []int) {
+	p.set(key, value)
+}
+
+// SetString saves a string value for the given key
+func (p *InMemoryPreferences) SetString(key string, value string) {
+	p.set(key, value)
+}
+
+func (p *InMemoryPreferences) SetStringList(key string, value []string) {
 	p.set(key, value)
 }
 
@@ -213,18 +217,14 @@ func (p *InMemoryPreferences) StringWithFallback(key, fallback string) string {
 	return value.(string)
 }
 
-// SetString saves a string value for the given key
-func (p *InMemoryPreferences) SetString(key string, value string) {
-	p.set(key, value)
-}
+// WriteValues provides write access to the underlying value map - for internal use only...
+// You should not retain a reference to the map passed to the callback function
+func (p *InMemoryPreferences) WriteValues(fn func(map[string]interface{})) {
+	p.lock.Lock()
+	fn(p.values)
+	p.lock.Unlock()
 
-func (p *InMemoryPreferences) SetStringList(key string, value []string) {
-	p.set(key, value)
-}
-
-// RemoveValue deletes a value on the given key
-func (p *InMemoryPreferences) RemoveValue(key string) {
-	p.remove(key)
+	p.fireChange()
 }
 
 // NewInMemoryPreferences creates a new preferences implementation stored in memory
