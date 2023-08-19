1716c2bd46a4a12366e5689b3dd0549f2189d71d
Jacalz <Jacalz@users.noreply.github.com>
2023-08-19 13:19:40 +0200

    Remove unnecesary resize

--- widget/entry_internal_test.go
+++ widget/entry_internal_test.go
@@ -367,8 +367,6 @@ func TestEntry_PlaceholderTextStyle(t *testing.T) {
 	e.TextStyle = fyne.TextStyle{Bold: true, Italic: true}
 
 	w := test.NewWindow(e)
-	w.Resize(fyne.NewSize(86, 86))
-
 	assert.Equal(t, e.TextStyle, e.placeholder.Segments[0].(*TextSegment).Style.TextStyle)
 
 	w.Canvas().Focus(e)
