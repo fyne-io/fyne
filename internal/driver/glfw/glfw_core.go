aaafe56ddfced208c44a518847bc28ab45216135
Mark Gascoyne <contact@markgascoyne.co.uk>
2023-10-10 19:54:49 +0100

    Fix windows dual graphics chip
    closes https://github.com/fyne-io/fyne/issues/437
    
    Upgrade to opengl 2.1

--- internal/driver/glfw/glfw_core.go
+++ internal/driver/glfw/glfw_core.go
@@ -10,7 +10,7 @@ import "github.com/go-gl/glfw/v3.3/glfw"
 
 func initWindowHints() {
 	glfw.WindowHint(glfw.ContextVersionMajor, 2)
-	glfw.WindowHint(glfw.ContextVersionMinor, 0)
+	glfw.WindowHint(glfw.ContextVersionMinor, 1)
 
 	glfw.WindowHint(glfw.CocoaGraphicsSwitching, glfw.True)
 }
--- internal/driver/glfw/glfw_es.go
+++ internal/driver/glfw/glfw_es.go
@@ -12,5 +12,5 @@ import "github.com/go-gl/glfw/v3.3/glfw"
 func initWindowHints() {
 	glfw.WindowHint(glfw.ClientAPI, glfw.OpenGLESAPI)
 	glfw.WindowHint(glfw.ContextVersionMajor, 2)
-	glfw.WindowHint(glfw.ContextVersionMinor, 0)
+	glfw.WindowHint(glfw.ContextVersionMinor, 1)
 }
