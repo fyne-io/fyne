7dbbdee926cc4f503822565eeac84e3390a6377f
BogdanTheGeek <bogdan.ionescu.work@gmail.com>
2023-10-17 19:07:17 +0100

    driver: mobile: key: add gamepad keys

--- driver/mobile/key.go
+++ driver/mobile/key.go
@@ -6,5 +6,10 @@ import (
 
 const (
 	// KeyBack represents the back button which may be hardware or software
-	KeyBack fyne.KeyName = "Back"
+	KeyBack       fyne.KeyName = "Back"
+	KeyDPadLeft   fyne.KeyName = "DPadLeft"
+	KeyDPadRight  fyne.KeyName = "DPadRight"
+	KeyDPadUp     fyne.KeyName = "DPadUp"
+	KeyDPadDown   fyne.KeyName = "DPadDown"
+	KeyDPadCenter fyne.KeyName = "DPadCenter"
 )
