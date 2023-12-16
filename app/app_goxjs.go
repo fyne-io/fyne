80342ff265a792e1732b4d97d783a92d69726cb3
Dorbmon <dorbmons@gmail.com>
2023-12-16 17:46:02 -0500

    Add Notification & watchTheme Support for web platform

--- app/app_goxjs.go
+++ app/app_goxjs.go
@@ -6,14 +6,61 @@
 package app
 
 import (
+	"encoding/base64"
+	"fmt"
+	"net/http"
+	"syscall/js"
+
 	"fyne.io/fyne/v2"
 )
 
-func (app *fyneApp) SendNotification(_ *fyne.Notification) {
-	// TODO #2735
-	fyne.LogError("Sending notification is not supported yet.", nil)
+func (app *fyneApp) SendNotification(n *fyne.Notification) {
+	window := js.Global().Get("window")
+	if window.IsUndefined() {
+		fyne.LogError("Current browser does not support notifications.", nil)
+		return
+	}
+	notification := window.Get("Notification")
+	if window.IsUndefined() {
+		fyne.LogError("Current browser does not support notifications.", nil)
+		return
+	}
+	// check permission
+	permission := notification.Get("permission")
+	showNotification := func() {
+		icon := app.icon.Content()
+		base64Str := base64.StdEncoding.EncodeToString(icon)
+		mimeType := http.DetectContentType(icon)
+		base64Img := fmt.Sprintf("data:%s;base64,%s", mimeType, base64Str)
+		notification.New(n.Title, map[string]interface{}{
+			"body": n.Content,
+			"icon": base64Img,
+		})
+		fyne.LogError("done show...", nil)
+	}
+	if permission.Type() != js.TypeString || permission.String() != "granted" {
+		// need to request for permission
+		notification.Call("requestPermission", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+			if len(args) > 0 && args[0].Type() == js.TypeString && args[0].String() == "granted" {
+				showNotification()
+			} else {
+				fyne.LogError("User rejected the request for notifications.", nil)
+			}
+			return nil
+		}))
+	} else {
+		showNotification()
+	}
 }
 
 func rootConfigDir() string {
 	return "/data/"
+}
+func watchTheme() {
+	js.Global().Call("matchMedia", "(prefers-color-scheme: dark)").Call("addEventListener", "change", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		if len(args) > 0 && args[0].Type() == js.TypeObject {
+			fyne.CurrentApp().Settings().(*settings).setupTheme()
+		}
+		return nil
+	}))
 }
