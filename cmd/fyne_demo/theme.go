eb0c18e203fbb2c22c9442bb509727e73c987770
Andy Williams <andy@fyne.io>
2023-12-23 16:44:30 +0000

    Fix naming

--- cmd/fyne_demo/theme.go
+++ cmd/fyne_demo/theme.go
@@ -6,12 +6,12 @@ import (
 	"fyne.io/fyne/v2"
 )
 
-type forceVariantTheme struct {
+type forcedVariant struct {
 	fyne.Theme
 
 	variant fyne.ThemeVariant
 }
 
-func (f *forceVariantTheme) Color(name fyne.ThemeColorName, _ fyne.ThemeVariant) color.Color {
+func (f *forcedVariant) Color(name fyne.ThemeColorName, _ fyne.ThemeVariant) color.Color {
 	return f.Theme.Color(name, f.variant)
 }
--- cmd/fyne_demo/theme_test.go
+++ cmd/fyne_demo/theme_test.go
@@ -3,14 +3,15 @@ package main
 import (
 	"testing"
 
+	"github.com/stretchr/testify/assert"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/theme"
-	"github.com/stretchr/testify/assert"
 )
 
 func TestForceVariantTheme_Color(t *testing.T) {
-	dark := &forceVariantTheme{Theme: theme.DefaultTheme(), variant: theme.VariantDark}
-	light := &forceVariantTheme{Theme: theme.DefaultTheme(), variant: theme.VariantLight}
+	dark := &forcedVariant{Theme: theme.DefaultTheme(), variant: theme.VariantDark}
+	light := &forcedVariant{Theme: theme.DefaultTheme(), variant: theme.VariantLight}
 	unspecified := fyne.ThemeVariant(99)
 
 	assert.Equal(t, theme.DefaultTheme().Color(theme.ColorNameForeground, theme.VariantDark), dark.Color(theme.ColorNameForeground, unspecified))
