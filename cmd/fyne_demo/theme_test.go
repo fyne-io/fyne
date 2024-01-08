ef3ce038dfbf260473ceab8138b936b354b9cb74
Andy Williams <andy@fyne.io>
2024-01-08 10:23:19 +0000

    Ensure an app is available in tester

--- cmd/fyne_demo/theme_test.go
+++ cmd/fyne_demo/theme_test.go
@@ -6,10 +6,12 @@ import (
 	"github.com/stretchr/testify/assert"
 
 	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/test"
 	"fyne.io/fyne/v2/theme"
 )
 
 func TestForceVariantTheme_Color(t *testing.T) {
+	test.NewApp()
 	dark := &forcedVariant{Theme: theme.DefaultTheme(), variant: theme.VariantDark}
 	light := &forcedVariant{Theme: theme.DefaultTheme(), variant: theme.VariantLight}
 	unspecified := fyne.ThemeVariant(99)
