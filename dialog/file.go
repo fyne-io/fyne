e0f3467f92b8e4faa55f7522a584e7705758294f
Ivan Voras <ivoras@gmail.com>
2024-03-03 22:03:10 +0100

    Focusing on the fileName Entry as soon as the dialog is shown
    Not terribly happy with the need to use Sleep() to do this asynchronously

--- dialog/file.go
+++ dialog/file.go
@@ -8,6 +8,7 @@ import (
 	"runtime"
 	"strings"
 	"sync"
+	"time"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
@@ -647,6 +648,13 @@ func showFile(file *FileDialog) *fileDialog {
 
 	d.setLocation(file.effectiveStartingDir())
 	d.win.Show()
+	if file.save {
+		// Focus on the filename Entry as soon as the dialog is shown
+		go func() {
+			time.Sleep(25 * time.Millisecond)
+			file.dialog.win.Canvas.Focus(file.dialog.fileName.(*widget.Entry))
+		}()
+	}
 	return d
 }
 
