9b01be298f0d3c10c59cd989758892ee1b99e3da
Andy Williams <andy@fyne.io>
2024-03-11 21:07:53 +0000

    Support reading metadata file in development mode
    This means we can just 'go run .' and get icons etc.
    Fixes #4688

--- app/meta.go
+++ app/meta.go
@@ -1,7 +1,11 @@
 package app
 
 import (
+	"os"
+	"path/filepath"
+
 	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/internal/metadata"
 )
 
 var meta = fyne.AppMetadata{
@@ -24,5 +28,34 @@ func SetMetadata(m fyne.AppMetadata) {
 }
 
 func (a *fyneApp) Metadata() fyne.AppMetadata {
+	if meta.ID == "" && meta.Name == "" {
+		file, _ := filepath.Abs("FyneApp.toml")
+		ref, err := os.Open(file)
+		if err != nil { // no worries, this is just an optional fallback
+			return meta
+		}
+
+		data, err := metadata.Load(ref)
+		if err != nil || data == nil {
+			fyne.LogError("failed to parse FyneApp.toml", err)
+			return meta
+		}
+
+		meta.ID = data.Details.ID
+		meta.Name = data.Details.Name
+		meta.Version = data.Details.Version
+		meta.Build = data.Details.Build
+
+		if data.Details.Icon != "" {
+			res, err := fyne.LoadResourceFromPath(data.Details.Icon)
+			if err == nil {
+				meta.Icon = res
+			}
+		}
+
+		meta.Release = false
+		meta.Custom = data.Development
+	}
+
 	return meta
 }
