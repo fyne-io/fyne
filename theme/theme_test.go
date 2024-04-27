ccd79e84c4dd021a940a4947a6e8ea93e2c814a0
Tilo Prütz <tilo@pruetz.net>
2024-04-27 11:47:13 +0200

    [theme] move theme test to theme_test package

--- theme/theme_test.go
+++ theme/theme_test.go
@@ -1,16 +1,17 @@
-package theme
+package theme_test
 
 import (
 	"image/color"
 	"testing"
 
 	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/theme"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestBuiltinTheme_ShadowColor(t *testing.T) {
-	shadow := ShadowColor()
+	shadow := theme.ShadowColor()
 
 	_, _, _, a := shadow.RGBA()
 	assert.NotEqual(t, 255, a)
@@ -18,18 +19,18 @@ func TestBuiltinTheme_ShadowColor(t *testing.T) {
 
 func TestEmptyTheme(t *testing.T) {
 	fyne.CurrentApp().Settings().SetTheme(&emptyTheme{})
-	assert.NotNil(t, ForegroundColor())
-	assert.NotNil(t, TextFont())
-	assert.NotNil(t, HelpIcon())
-	fyne.CurrentApp().Settings().SetTheme(DarkTheme())
+	assert.NotNil(t, theme.ForegroundColor())
+	assert.NotNil(t, theme.TextFont())
+	assert.NotNil(t, theme.HelpIcon())
+	fyne.CurrentApp().Settings().SetTheme(theme.DarkTheme())
 }
 
 func TestThemeChange(t *testing.T) {
-	fyne.CurrentApp().Settings().SetTheme(DarkTheme())
-	bg := BackgroundColor()
+	fyne.CurrentApp().Settings().SetTheme(theme.DarkTheme())
+	bg := theme.BackgroundColor()
 
-	fyne.CurrentApp().Settings().SetTheme(LightTheme())
-	assert.NotEqual(t, bg, BackgroundColor())
+	fyne.CurrentApp().Settings().SetTheme(theme.LightTheme())
+	assert.NotEqual(t, bg, theme.BackgroundColor())
 }
 
 func TestTheme_Bootstrapping(t *testing.T) {
@@ -37,21 +38,21 @@ func TestTheme_Bootstrapping(t *testing.T) {
 	fyne.CurrentApp().Settings().SetTheme(nil)
 
 	// this should not crash
-	BackgroundColor()
+	theme.BackgroundColor()
 
 	fyne.CurrentApp().Settings().SetTheme(current)
 }
 
 func TestTheme_Dark_ReturnsCorrectBackground(t *testing.T) {
-	fyne.CurrentApp().Settings().SetTheme(DarkTheme())
-	bg := BackgroundColor()
-	assert.Equal(t, DarkTheme().Color(ColorNameBackground, VariantDark), bg, "wrong dark theme background color")
+	fyne.CurrentApp().Settings().SetTheme(theme.DarkTheme())
+	bg := theme.BackgroundColor()
+	assert.Equal(t, theme.DarkTheme().Color(theme.ColorNameBackground, theme.VariantDark), bg, "wrong dark theme background color")
 }
 
 func TestTheme_Light_ReturnsCorrectBackground(t *testing.T) {
-	fyne.CurrentApp().Settings().SetTheme(LightTheme())
-	bg := BackgroundColor()
-	assert.Equal(t, LightTheme().Color(ColorNameBackground, VariantLight), bg, "wrong light theme background color")
+	fyne.CurrentApp().Settings().SetTheme(theme.LightTheme())
+	bg := theme.BackgroundColor()
+	assert.Equal(t, theme.LightTheme().Color(theme.ColorNameBackground, theme.VariantLight), bg, "wrong light theme background color")
 }
 
 type emptyTheme struct {
