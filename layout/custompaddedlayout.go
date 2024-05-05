2a8ca260894fdd635aba23babcc1d13acb777441
Drew Weymouth <dweymouth@users.noreply.github.com>
2024-05-05 13:09:54 -0700

    Update Layout and MinSize doc comments

--- layout/custompaddedlayout.go
+++ layout/custompaddedlayout.go
@@ -45,7 +45,7 @@ func NewSquareCustomPaddedLayout(pad float32) fyne.Layout {
 }
 
 // Layout is called to pack all child objects into a specified size.
-// For PaddedLayout this sets all children to the full size passed minus padding all around.
+// For CustomPaddedLayout this sets all children to the full size passed minus the given paddings all around.
 func (c CustomPaddedLayout) Layout(objects []fyne.CanvasObject, size fyne.Size) {
 	pos := fyne.NewPos(c.LeftPadding, c.TopPadding)
 	siz := fyne.Size{
@@ -59,7 +59,7 @@ func (c CustomPaddedLayout) Layout(objects []fyne.CanvasObject, size fyne.Size) 
 }
 
 // MinSize finds the smallest size that satisfies all the child objects.
-// For PaddedLayout this is determined simply as the MinSize of the largest child plus padding all around.
+// For CustomPaddedLayout this is determined simply as the MinSize of the largest child plus the given paddings all around.
 func (c CustomPaddedLayout) MinSize(objects []fyne.CanvasObject) (min fyne.Size) {
 	for _, child := range objects {
 		if !child.Visible() {
