52365e0c4f1de96933735793964a6db695f32be2
Drew Weymouth <dweymouth@users.noreply.github.com>
2024-05-19 14:01:11 -0700

    Update widget.go

--- widget.go
+++ widget.go
@@ -15,7 +15,10 @@ type Widget interface {
 // This is returned from a widget's declarative object through the CreateRenderer()
 // function and should be exactly one instance per widget in memory.
 type WidgetRenderer interface {
-	// Destroy is for internal use.
+	// Destroy is a hook that is called when the renderer is being destroyed.
+	// This happens at some time after the widget is no longer visible, and
+	// once destroyed a renderer will not be reused.
+	// Renderers should dispose and clean up any related resources, if necessary.
 	Destroy()
 	// Layout is a hook that is called if the widget needs to be laid out.
 	// This should never call Refresh.
