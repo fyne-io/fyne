689aee42b02209b613cec3e3f3f0544bd45ec14b
Drew Weymouth <dweymouth@users.noreply.github.com>
2024-05-21 08:14:01 -0700

    Add documentation for RunNative

--- driver/desktop/window.go
+++ driver/desktop/window.go
@@ -4,5 +4,8 @@ package desktop
 //
 // Since: 2.5
 type Window interface {
+	// RunNative executes the given function, passing platform-specific context
+	// information related to the window. The possible contexts that may be passed
+	// are defined in the `driver` package and differ per OS.
 	RunNative(func(ctx any) error) error
 }
