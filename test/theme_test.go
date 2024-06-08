d10b6c33c73f75962b2727965acd124d808f4b5f
Tilo Prütz <tilo@pruetz.net>
2024-06-08 17:55:33 +0200

    [test] refactor test theme tests to use a suite

--- test/theme_test.go
+++ test/theme_test.go
@@ -5,23 +5,31 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 
 	"fyne.io/fyne/v2"
 )
 
-func Test_NewTheme_checkForUniqueColors(t *testing.T) {
-	th := NewTheme().(*configurableTheme)
-	seen := map[string]fyne.ThemeColorName{}
-	for cn, c := range th.colors {
-		r, g, b, a := c.RGBA()
-		key := fmt.Sprintf("%d %d %d %d", r, g, b, a)
-		assert.True(t, seen[key] == "", "color value %#v for color %s already used for color %s in theme %s", c, cn, seen[key], th.name)
-		seen[key] = cn
-	}
+func Test_NewTheme(t *testing.T) {
+	suite.Run(t, &configurableThemeTestSuite{constructor: func() *configurableTheme {
+		return NewTheme().(*configurableTheme)
+	}})
 }
 
-func Test_Theme_checkForUniqueColors(t *testing.T) {
-	th := Theme().(*configurableTheme)
+func Test_Theme(t *testing.T) {
+	suite.Run(t, &configurableThemeTestSuite{constructor: func() *configurableTheme {
+		return Theme().(*configurableTheme)
+	}})
+}
+
+type configurableThemeTestSuite struct {
+	suite.Suite
+	constructor func() *configurableTheme
+}
+
+func (s *configurableThemeTestSuite) TestUniqueColorValues() {
+	t := s.T()
+	th := s.constructor()
 	seen := map[string]fyne.ThemeColorName{}
 	for cn, c := range th.colors {
 		r, g, b, a := c.RGBA()
