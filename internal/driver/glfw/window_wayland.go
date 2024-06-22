3080cb553fd93885262308c6fd46fb64abcfda04
Drew Weymouth <dweymouth@users.noreply.github.com>
2024-06-22 07:54:10 -0700

    remove unused eglSurface

--- internal/driver/glfw/window_wayland.go
+++ internal/driver/glfw/window_wayland.go
@@ -18,7 +18,7 @@ var _ driver.NativeWindow = (*window)(nil)
 
 func (w *window) RunNative(f func(any)) {
 	runOnMain(func() {
-		var waylandSurface, eglSurface uintptr
+		var waylandSurface uintptr
 		if v := w.view(); v != nil {
 			waylandSurface = uintptr(unsafe.Pointer(v.GetWaylandWindow()))
 		}
