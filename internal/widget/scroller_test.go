60eaa9bba230c75f6b18362f0de49beb4164a266
Andy Williams <andy@andy.xyz>
2024-06-23 14:05:02 +0100

    Fix the theming and refresh of scroll/shadow internal widgets

---
+++ internal/widget/scroller_test.go
@@ -0,0 +1,50 @@
+package widget_test
+
+import (
+	"image/color"
+	"testing"
+	"time"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/internal/widget"
+	"fyne.io/fyne/v2/test"
+	"fyne.io/fyne/v2/theme"
+)
+
+func TestScrollContainer_Theme(t *testing.T) {
+	rect := canvas.NewRectangle(color.Transparent)
+	rect.SetMinSize(fyne.NewSize(250, 250))
+	scroll := widget.NewScroll(rect)
+
+	w := test.NewTempWindow(t, scroll)
+	w.SetPadded(false)
+	w.Resize(fyne.NewSize(100, 100))
+	test.AssertImageMatches(t, "scroll/theme_initial.png", w.Canvas().Capture())
+
+	test.WithTestTheme(t, func() {
+		time.Sleep(100 * time.Millisecond)
+		scroll.Refresh()
+		test.AssertImageMatches(t, "scroll/theme_changed.png", w.Canvas().Capture())
+	})
+}
+
+func TestScrollContainer_ThemeOverride(t *testing.T) {
+	rect := canvas.NewRectangle(color.Transparent)
+	rect.SetMinSize(fyne.NewSize(250, 250))
+	scroll := widget.NewScroll(rect)
+	scroll.Resize(fyne.NewSize(100, 100))
+
+	w := test.NewTempWindow(t, scroll)
+	w.SetPadded(false)
+	w.Resize(fyne.NewSize(100, 100))
+	test.ApplyTheme(t, test.NewTheme())
+	test.AssertImageMatches(t, "scroll/theme_changed.png", w.Canvas().Capture())
+
+	normal := test.Theme()
+	bg := canvas.NewRectangle(normal.Color(theme.ColorNameBackground, theme.VariantDark))
+	w.SetContent(container.NewStack(bg, container.NewThemeOverride(scroll, normal)))
+	w.Resize(fyne.NewSize(100, 100))
+	test.AssertImageMatches(t, "scroll/theme_initial.png", w.Canvas().Capture())
+}
