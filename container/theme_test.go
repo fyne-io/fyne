ff95f675a64f13a2602f2ad719cbbd4c7b60b2ec
Andy Williams <andy@fyne.io>
2024-06-29 21:46:44 +0100

    Document the requirement to refresh if adding items

--- container/theme_test.go
+++ container/theme_test.go
@@ -4,10 +4,26 @@ import (
 	"image"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
+
+	"fyne.io/fyne/v2/internal/cache"
 	"fyne.io/fyne/v2/test"
 	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
 )
+
+func TestThemeOverride_AddChild(t *testing.T) {
+	b := widget.NewButton("Test", func() {})
+	group := NewHBox(b)
+	override := NewThemeOverride(group, test.Theme())
+
+	child := widget.NewLabel("Added")
+	assert.NotEqual(t, cache.WidgetTheme(b), cache.WidgetTheme(child))
+
+	group.Add(child)
+	override.Refresh()
+	assert.Equal(t, cache.WidgetTheme(b), cache.WidgetTheme(child))
+}
 
 func TestThemeOverride_Icons(t *testing.T) {
 	b := widget.NewButtonWithIcon("", theme.HomeIcon(), func() {})
