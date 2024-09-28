9419b1411605e87e2d5ed0817dac6a5eb8b5ad53
Andy Williams <andy@andy.xyz>
2024-09-28 15:23:19 +0100

    Update to the latest breaking API changes in go-text

--- internal/test/text.go
+++ internal/test/text.go
@@ -2,9 +2,9 @@ package test
 
 import "github.com/go-text/typesetting/font"
 
-type FontMap []font.Face
+type FontMap []*font.Face
 
-func (f FontMap) ResolveFace(r rune) font.Face {
+func (f FontMap) ResolveFace(r rune) *font.Face {
 	if len(f) == 1 {
 		return f[0]
 	}
