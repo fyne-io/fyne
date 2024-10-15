f2f20b98f458862978844245f208bd781834ad78
Andy Williams <andy@andy.xyz>
2024-10-15 16:54:51 +0100

    Testing locale parse

---
+++ lang/locale_test.go
@@ -0,0 +1,20 @@
+package lang
+
+import (
+	"testing"
+
+	"github.com/jeandeaual/go-locale"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSystemLocale(t *testing.T) {
+	info, err := locale.GetLocale()
+	if err != nil {
+		// something not testable
+		t.Log("Unable to run locale test because", err)
+		return
+	}
+
+	loc := SystemLocale()
+	assert.Equal(t, info[:2], loc.String()[:2])
+}
