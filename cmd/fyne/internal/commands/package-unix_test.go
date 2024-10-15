96ed118b7b3ba4cf7533eab82330197943ec9e7c
Andy Williams <andy@andy.xyz>
2024-10-15 15:46:41 +0100

    Add optional Source metadata to the FyneApp.toml
    Use this to write an extension of the .desktop file for Linux.
    I am not sure if we can extend other package formats with this information also?

--- cmd/fyne/internal/commands/package-unix_test.go
+++ cmd/fyne/internal/commands/package-unix_test.go
@@ -1,8 +1,11 @@
 package commands
 
 import (
+	"bytes"
+	"strings"
 	"testing"
 
+	"fyne.io/fyne/v2/cmd/fyne/internal/templates"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -10,4 +13,24 @@ func TestFormatDesktopFileList(t *testing.T) {
 	assert.Equal(t, "", formatDesktopFileList([]string{}))
 	assert.Equal(t, "One;", formatDesktopFileList([]string{"One"}))
 	assert.Equal(t, "One;Two;", formatDesktopFileList([]string{"One", "Two"}))
+}
+
+func TestDesktopFileSource(t *testing.T) {
+	tplData := unixData{
+		Name: "Testing",
+	}
+	buf := &bytes.Buffer{}
+
+	err := templates.DesktopFileUNIX.Execute(buf, tplData)
+	assert.Nil(t, err)
+	assert.False(t, strings.Contains(buf.String(), "[X-Fyne"))
+
+	tplData.SourceRepo = "https://example.com"
+	tplData.SourceDir = "cmd/name"
+
+	err = templates.DesktopFileUNIX.Execute(buf, tplData)
+	assert.Nil(t, err)
+	assert.True(t, strings.Contains(buf.String(), "[X-Fyne"))
+	assert.True(t, strings.Contains(buf.String(), "Repo=https://example.com"))
+	assert.True(t, strings.Contains(buf.String(), "Dir=cmd/name"))
 }
