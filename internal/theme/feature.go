7a4a1594eb3f65b17001373ab5000fcb876e632d
Andy Williams <andy@andy.xyz>
2024-11-06 16:55:14 +0000

    Don't document a public API release version for internal APIs

--- internal/theme/feature.go
+++ internal/theme/feature.go
@@ -11,8 +11,6 @@ const FeatureNameDeviceIsMobile = FeatureName("deviceIsMobile")
 
 // FeatureTheme defines the method to look up features that we use internally to apply functional
 // differences through a theme override.
-//
-// Since: 2.6
 type FeatureTheme interface {
 	Feature(FeatureName) any
 }
@@ -20,8 +18,6 @@ type FeatureTheme interface {
 // FeatureForWidget looks up the specified feature flag for the requested widget using the current theme.
 // This is for internal purposes and will do nothing if the theme has not been overridden with the
 // ThemeOverride container.
-//
-// Since: 2.6
 func FeatureForWidget(name FeatureName, w fyne.Widget) any {
 	if custom := cache.WidgetTheme(w); custom != nil {
 		if f, ok := custom.(FeatureTheme); ok {
