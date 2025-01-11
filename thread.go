82e2aa5c4eff645b3ed056bc68aa977b95d63e3e
Andy Williams <andy@fyne.io>
2025-01-11 15:55:49 +0000

    Fix typo

--- thread.go
+++ thread.go
@@ -1,7 +1,7 @@
 package fyne
 
 // Do is used to execute a specified function in the main Fyne runtime context.
-// This is required when a background processes wishes to adjust graphical elements of a running app.
+// This is required when a background process wishes to adjust graphical elements of a running app.
 // Developers should use this only from within goroutines they have created.
 //
 // Since: 2.6
