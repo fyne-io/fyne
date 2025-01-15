fe50f1c2af946b9680069e74490229e78c9239f8
Andy Williams <andy@andy.xyz>
2025-01-15 14:09:25 +0000

    Moving window handling functions into a safer transition mode
    Avoid the most likely crashes as people try out 2.6

--- internal/async/goroutine.go
+++ internal/async/goroutine.go
@@ -1,5 +1,14 @@
 package async
 
+import (
+	"log"
+	"runtime"
+	"strings"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/internal/build"
+)
+
 // mainGoroutineID stores the main goroutine ID.
 // This ID must be initialized in main.init because
 // a main goroutine may not equal to 1 due to the
@@ -12,4 +21,27 @@ func init() {
 
 func IsMainGoroutine() bool {
 	return goroutineID() == mainGoroutineID
+}
+
+func EnsureMain(fn func()) {
+	if build.DisableThreadChecks || IsMainGoroutine() {
+		fn()
+		return
+	}
+
+	log.Println("*** Error in Fyne call thread, this should have been called in fyne.Do ***")
+
+	pc := make([]uintptr, 2)
+	count := runtime.Callers(3, pc)
+	frames := runtime.CallersFrames(pc)
+	frame, more := frames.Next()
+	if more && count > 1 {
+		nextFrame, _ := frames.Next()                     // skip an occasional driver call to itself
+		if !strings.Contains(nextFrame.File, "runtime") { // don't descend into Go
+			frame = nextFrame
+		}
+	}
+	log.Printf("  From: %s:%d", frame.File, frame.Line)
+
+	fyne.Do(fn)
 }
