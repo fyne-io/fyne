52e95e4256ab2b567be222560c6e84f2e57f960d
Jacalz <jacalz@tutanota.com>
2025-01-16 10:48:57 +0100

    Remove locks from lifecycle as well

--- internal/app/lifecycle.go
+++ internal/app/lifecycle.go
@@ -1,8 +1,6 @@
 package app
 
 import (
-	"sync/atomic"
-
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/internal/async"
 )
@@ -13,10 +11,10 @@ var _ fyne.Lifecycle = (*Lifecycle)(nil)
 //
 // Since: 2.1
 type Lifecycle struct {
-	onForeground atomic.Pointer[func()]
-	onBackground atomic.Pointer[func()]
-	onStarted    atomic.Pointer[func()]
-	onStopped    atomic.Pointer[func()]
+	onForeground func()
+	onBackground func()
+	onStarted    func()
+	onStopped    func()
 
 	onStoppedHookExecuted func()
 
@@ -31,75 +29,60 @@ func (l *Lifecycle) SetOnStoppedHookExecuted(f func()) {
 
 // SetOnEnteredForeground hooks into the app becoming foreground.
 func (l *Lifecycle) SetOnEnteredForeground(f func()) {
-	l.onForeground.Store(&f)
+	l.onForeground = f
 }
 
 // SetOnExitedForeground hooks into the app having moved to the background.
 // Depending on the platform it may still be  visible but will not receive keyboard events.
 // On some systems hover or desktop mouse move events may still occur.
 func (l *Lifecycle) SetOnExitedForeground(f func()) {
-	l.onBackground.Store(&f)
+	l.onBackground = f
 }
 
 // SetOnStarted hooks into an event that says the app is now running.
 func (l *Lifecycle) SetOnStarted(f func()) {
-	l.onStarted.Store(&f)
+	l.onStarted = f
 }
 
 // SetOnStopped hooks into an event that says the app is no longer running.
 func (l *Lifecycle) SetOnStopped(f func()) {
-	l.onStopped.Store(&f)
+	l.onStopped = f
 }
 
 // OnEnteredForeground returns the focus gained hook, if one is registered.
 func (l *Lifecycle) OnEnteredForeground() func() {
-	f := l.onForeground.Load()
-	if f == nil {
-		return nil
-	}
-
-	return *f
+	return l.onForeground
 }
 
 // OnExitedForeground returns the focus lost hook, if one is registered.
 func (l *Lifecycle) OnExitedForeground() func() {
-	f := l.onBackground.Load()
-	if f == nil {
-		return nil
-	}
-
-	return *f
+	return l.onBackground
 }
 
 // OnStarted returns the started hook, if one is registered.
 func (l *Lifecycle) OnStarted() func() {
-	f := l.onStarted.Load()
-	if f == nil {
-		return nil
-	}
-
-	return *f
+	return l.onStarted
 }
 
 // OnStopped returns the stopped hook, if one is registered.
 func (l *Lifecycle) OnStopped() func() {
-	stopped := l.onStopped.Load()
+	stopped := l.onStopped
 	stopHook := l.onStoppedHookExecuted
-	if (stopped == nil || *stopped == nil) && stopHook == nil {
+	if stopped == nil && stopHook == nil {
 		return nil
 	}
 
 	if stopHook == nil {
-		return *stopped
+		return stopped
 	}
 
-	if stopped == nil || *stopped == nil {
+	if stopped == nil {
 		return stopHook
 	}
 
 	// we have a stopped handle and the onStoppedHook
 	return func() {
-		(*stopped)()
+		stopped()
 		stopHook()
 	}
 }
