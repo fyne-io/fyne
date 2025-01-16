c1e737dc418720b73f36cffa0809ce8e95fbbda6
Jacalz <jacalz@tutanota.com>
2025-01-16 18:16:39 +0100

    Add build.HasNativeMenu

--- internal/driver/glfw/menu_other.go
+++ internal/driver/glfw/menu_other.go
@@ -1,12 +1,8 @@
-//go:build !darwin || wasm || test_web_driver || no_native_menus
+//go:build !darwin || no_native_menus
 
 package glfw
 
 import "fyne.io/fyne/v2"
-
-func hasNativeMenu() bool {
-	return false
-}
 
 func setupNativeMenu(_ *window, _ *fyne.MainMenu) {
 	// no-op
