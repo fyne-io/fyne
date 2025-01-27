8217295ebc03e1153695f1defac658555e66911e
Andy Williams <andy@andy.xyz>
2025-01-27 13:02:33 +0000

    Revert "container: remove unnecessary slice copy from Container.Remove"
    This reverts commit 75a23c73f7258ee3c6e629a5a54c9b37398cb6ce.

--- container.go
+++ container.go
@@ -139,9 +139,12 @@ func (c *Container) Remove(rem CanvasObject) {
 		if o != rem {
 			continue
 		}
-		copy(c.Objects[i:], c.Objects[i+1:])
-		c.Objects[len(c.Objects)-1] = nil
-		c.Objects = c.Objects[:len(c.Objects)-1]
+
+		removed := make([]CanvasObject, len(c.Objects)-1)
+		copy(removed, c.Objects[:i])
+		copy(removed[i:], c.Objects[i+1:])
+
+		c.Objects = removed
 		c.layout()
 		return
 	}
