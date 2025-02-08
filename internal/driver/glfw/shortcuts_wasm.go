35372bbe2c0124c0cff9a4532aeaf34981df8f13
Jacalz <jacalz@tutanota.com>
2025-02-08 15:42:40 +0100

    Avoid need for making platform lowercase

--- internal/driver/glfw/shortcuts_wasm.go
+++ internal/driver/glfw/shortcuts_wasm.go
@@ -8,7 +8,7 @@ import (
 	"syscall/js"
 )
 
-var isMacOS = strings.Contains(strings.ToLower(js.Global().Get("window").Get("navigator").Get("platform").String()), "mac")
+var isMacOS = strings.Contains(js.Global().Get("window").Get("navigator").Get("platform").String(), "Mac")
 
 // Checks if running on Mac OSX
 func isMacOSRuntime() bool {
