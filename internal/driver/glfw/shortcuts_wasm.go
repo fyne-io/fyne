5a87ee3e16c17316bc7fe6be32233c226bdc5171
Jacalz <jacalz@tutanota.com>
2025-02-08 15:41:33 +0100

    Cache value of isMacOSRuntime

--- internal/driver/glfw/shortcuts_wasm.go
+++ internal/driver/glfw/shortcuts_wasm.go
@@ -8,7 +8,9 @@ import (
 	"syscall/js"
 )
 
+var isMacOS = strings.Contains(strings.ToLower(js.Global().Get("window").Get("navigator").Get("platform").String()), "mac")
+
 // Checks if running on Mac OSX
 func isMacOSRuntime() bool {
-	return strings.Contains(strings.ToLower(js.Global().Get("window").Get("navigator").Get("platform").String()), "mac")
+	return isMacOS
 }
