a2440b11266bce5f072572f91c694bb9cdc75111
Jacalz <jacalz@tutanota.com>
2025-02-08 16:12:29 +0100

    Move isMacOSRuntime into device files

--- internal/driver/glfw/device_wasm.go
+++ internal/driver/glfw/device_wasm.go
@@ -4,13 +4,17 @@ package glfw
 
 import (
 	"regexp"
+	"strings"
 	"syscall/js"
 
 	"fyne.io/fyne/v2"
 )
 
-var isMobile = regexp.MustCompile("Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile").
-	MatchString(js.Global().Get("navigator").Get("userAgent").String())
+var (
+	isMobile = regexp.MustCompile("Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile").
+			MatchString(js.Global().Get("navigator").Get("userAgent").String())
+	isMacOS = strings.Contains(js.Global().Get("window").Get("navigator").Get("platform").String(), "Mac")
+)
 
 func (*glDevice) IsMobile() bool {
 	return isMobile
@@ -19,4 +23,8 @@ func (*glDevice) IsMobile() bool {
 func (*glDevice) SystemScaleForWindow(w fyne.Window) float32 {
 	// Get the scale information from the web browser directly
 	return float32(js.Global().Get("devicePixelRatio").Float())
+}
+
+func isMacOSRuntime() bool {
+	return isMacOS // Value depends on which OS the browser is running on.
 }
