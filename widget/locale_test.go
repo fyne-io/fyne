416e68b95235c05c6c17b4dc4725d4d95a3d8c2e
Drew Weymouth <dweymouth@users.noreply.github.com>
2025-03-06 17:25:00 -0800

    Update locale_test.go

--- widget/locale_test.go
+++ widget/locale_test.go
@@ -7,17 +7,15 @@ import (
 )
 
 func TestGetLocaleWeekStart(t *testing.T) {
-	assert.Equal(t, "Monday", lookupLocaleSetting(localeWeekStartKey, ""))
-	assert.Equal(t, "Monday", lookupLocaleSetting(localeWeekStartKey, "en"))
-	assert.Equal(t, "Monday", lookupLocaleSetting(localeWeekStartKey, "en-GB"))
-	assert.Equal(t, "Sunday", lookupLocaleSetting(localeWeekStartKey, "en-US"))
-	assert.Equal(t, "Sunday", lookupLocaleSetting(localeWeekStartKey, "es-US"))
+	assert.Equal(t, "Monday", lookupLocaleSetting("").weekStartDay.String())
+	assert.Equal(t, "Monday", lookupLocaleSetting("en").weekStartDay.String())
+	assert.Equal(t, "Monday", lookupLocaleSetting("en-GB").weekStartDay.String())
+	assert.Equal(t, "Sunday", lookupLocaleSetting("en-US").weekStartDay.String())
+	assert.Equal(t, "Sunday", lookupLocaleSetting("es-US").weekStartDay.String())
 }
 
 func TestGetLocaleDateFormat(t *testing.T) {
-	assert.Equal(t, "02/01/2006", lookupLocaleSetting(localeDateFormatKey, ""))
-	assert.Equal(t, "02/01/2006", lookupLocaleSetting(localeDateFormatKey, "en"))
-	assert.Equal(t, "02/01/2006", lookupLocaleSetting(localeDateFormatKey, "en-GB"))
-	assert.Equal(t, "01/02/2006", lookupLocaleSetting(localeDateFormatKey, "en-US"))
-	assert.Equal(t, "01/02/2006", lookupLocaleSetting(localeDateFormatKey, "es-US"))
+	assert.Equal(t, "02/01/2006", lookupLocaleSetting("").dateFormat)
+	assert.Equal(t, "01/02/2006", lookupLocaleSetting("en-US").dateFormat)
+	assert.Equal(t, "01/02/2006", lookupLocaleSetting("es-US").dateFormat)
 }
