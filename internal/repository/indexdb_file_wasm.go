a15e1522b6ee0748250f4b979d8143fe6659a45d
Simon Dassow <simon@fabams.nl>
2025-03-15 01:36:23 +0100

    Initial code for web driver storage API using IndexDB
    Fix for https://github.com/fyne-io/fyne/issues/4634
    
    Still need to figure out how/when to register and how wasm tests work.

---
+++ internal/repository/indexdb_file_wasm.go
@@ -0,0 +1,178 @@
+//go:build wasm
+
+package repository
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"syscall/js"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/storage"
+
+	"github.com/hack-pad/go-indexeddb/idb"
+)
+
+var (
+	blob       js.Value
+	uint8Array js.Value
+)
+
+func init() {
+	blob = js.Global().Get("Blob")
+	uint8Array = js.Global().Get("Uint8Array")
+}
+
+var _ fyne.URIReadCloser = (*file)(nil)
+var _ fyne.URIWriteCloser = (*file)(nil)
+
+type file struct {
+	db          *idb.Database
+	path        string
+	parent      string
+	isDir       bool
+	truncate    bool
+	isTruncated bool
+	parts       []interface{}
+	add         bool
+	isAdding    bool
+}
+
+func (f *file) Close() error {
+	return nil
+}
+
+func (f *file) URI() fyne.URI {
+	u, _ := storage.ParseURI(fileSchemePrefix + f.path)
+	return u
+}
+
+func (f *file) rwstore(name string) (*idb.ObjectStore, error) {
+	txn, err := f.db.Transaction(idb.TransactionReadWrite, name)
+	if err != nil {
+		return nil, err
+	}
+	store, err := txn.ObjectStore(name)
+	if err != nil {
+		return nil, err
+	}
+	return store, nil
+}
+
+func (f *file) Write(data []byte) (int, error) {
+	p := f.path
+	ctx := context.Background()
+
+	if f.truncate && !f.isTruncated || f.add && !f.isAdding {
+		store, err := f.rwstore("meta")
+		if err != nil {
+			return 0, err
+		}
+
+		f := map[string]interface{}{
+			"parent": f.parent,
+		}
+		req, err := store.PutKey(js.ValueOf(p), js.ValueOf(f))
+		if err != nil {
+			return 0, err
+		}
+
+		if _, err := req.Await(ctx); err != nil {
+			return 0, err
+		}
+	}
+
+	if f.truncate && !f.isTruncated {
+		store, err := f.rwstore("data")
+		if err != nil {
+			return 0, err
+		}
+		delreq, err := store.Delete(js.ValueOf(p))
+		if err != nil {
+			return 0, err
+		}
+		if err := delreq.Await(ctx); err != nil {
+			return 0, err
+		}
+		f.isTruncated = true
+	}
+
+	if f.add && !f.isAdding {
+		store, err := f.rwstore("data")
+		if err != nil {
+			return 0, err
+		}
+		req, err := store.Get(js.ValueOf(f.path))
+		if err != nil {
+			return 0, err
+		}
+		b, err := req.Await(ctx)
+		if err != nil {
+			return 0, err
+		}
+
+		bytes := b.Call("bytes")
+		outch := make(chan js.Value)
+		bytes.Call("then", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+			outch <- args[0]
+			return nil
+		}))
+		a := <-outch
+		f.parts = []interface{}{a}
+		f.isAdding = true
+	}
+
+	a := uint8Array.New(len(data))
+	n := js.CopyBytesToJS(a, data)
+	f.parts = append(f.parts, a)
+	b := blob.New(js.ValueOf(f.parts))
+
+	store, err := f.rwstore("data")
+	if err != nil {
+		return 0, err
+	}
+	req, err := store.PutKey(js.ValueOf(p), b)
+	if err != nil {
+		return 0, err
+	}
+
+	_, err = req.Await(ctx)
+	return n, err
+}
+
+func (f *file) Read(data []byte) (int, error) {
+	ctx := context.Background()
+	txn, err := f.db.Transaction(idb.TransactionReadOnly, "data")
+	if err != nil {
+		return 0, err
+	}
+
+	store, err := txn.ObjectStore("data")
+	if err != nil {
+		return 0, err
+	}
+	req, err := store.Get(js.ValueOf(f.path))
+	if err != nil {
+		return 0, err
+	}
+
+	b, err := req.Await(ctx)
+	if err != nil {
+		return 0, err
+	}
+
+	if b.IsUndefined() {
+		return 0, fmt.Errorf("file undefined")
+	}
+
+	bytes := b.Call("bytes")
+	outch := make(chan js.Value)
+	bytes.Call("then", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		outch <- args[0]
+		return nil
+	}))
+	out := <-outch
+
+	return js.CopyBytesToGo(data, out), io.EOF
+}
