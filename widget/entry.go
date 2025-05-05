32a054d4ebe58d00ba117696481f97c44828377f
mbaklor <78748463+mbaklor@users.noreply.github.com>
2025-05-05 21:18:43 +0300

    fix entry undo and redo don't trigger OnChanged (#5712)

--- widget/entry.go
+++ widget/entry.go
@@ -421,6 +421,9 @@ func (e *Entry) Redo() {
 	e.updateText(newText, false)
 	e.CursorRow, e.CursorColumn = e.rowColFromTextPos(pos)
 	e.syncSelectable()
+	if e.OnChanged != nil {
+		e.OnChanged(newText)
+	}
 	e.Refresh()
 }
 
@@ -715,6 +718,9 @@ func (e *Entry) Undo() {
 	e.updateText(newText, false)
 	e.CursorRow, e.CursorColumn = e.rowColFromTextPos(pos)
 	e.syncSelectable()
+	if e.OnChanged != nil {
+		e.OnChanged(newText)
+	}
 	e.Refresh()
 }
 
