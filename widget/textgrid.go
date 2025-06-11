e63af54db04a19b962fd37889bd94650934f4d4e
Eli Burch <eli.burch@burchbytes.com>
2025-06-11 15:42:13 -0700

    More useful comment, check that scroll content is not nil

--- widget/textgrid.go
+++ widget/textgrid.go
@@ -159,8 +159,9 @@ func (t *TextGrid) SetText(text string) {
 	oldRowsLen := len(t.Rows)
 	t.Rows = rows
 
-	// Update scroll position if new text is shorter than previous text
-	if t.Scroll != fyne.ScrollNone && len(rows) < oldRowsLen {
+	// If we don't update the scroll offset when the text is shorter,
+	// we may end up with no text displayed or text appearing partially cut off
+	if t.Scroll != fyne.ScrollNone && len(rows) < oldRowsLen && t.scroll.Content != nil {
 		offset := t.PositionForCursorLocation(len(rows), 0)
 		t.scroll.ScrollToOffset(fyne.NewPos(offset.X, t.scroll.Offset.Y))
 		t.scroll.Refresh()
