5e4a34a99a0e8385898173ed64a231582b171401
Jacalz <jacalz@tutanota.com>
2025-06-30 17:33:11 +0200

    Refactor wasm notification code

--- app/app_wasm.go
+++ app/app_wasm.go
@@ -14,42 +14,38 @@ import (
 )
 
 func (a *fyneApp) SendNotification(n *fyne.Notification) {
-	window := js.Global().Get("window")
-	if window.IsUndefined() {
+	notification := js.Global().Get("window").Get("Notification")
+	if notification.IsUndefined() {
 		fyne.LogError("Current browser does not support notifications.", nil)
 		return
 	}
-	notification := window.Get("Notification")
-	if window.IsUndefined() {
-		fyne.LogError("Current browser does not support notifications.", nil)
-		return
-	}
-	// check permission
+
 	permission := notification.Get("permission")
-	showNotification := func() {
-		icon := a.icon.Content()
-		base64Str := base64.StdEncoding.EncodeToString(icon)
-		mimeType := http.DetectContentType(icon)
-		base64Img := fmt.Sprintf("data:%s;base64,%s", mimeType, base64Str)
-		notification.New(n.Title, map[string]any{
-			"body": n.Content,
-			"icon": base64Img,
-		})
-		fyne.LogError("done show...", nil)
-	}
 	if permission.Type() != js.TypeString || permission.String() != "granted" {
-		// need to request for permission
 		notification.Call("requestPermission", js.FuncOf(func(this js.Value, args []js.Value) any {
 			if len(args) > 0 && args[0].Type() == js.TypeString && args[0].String() == "granted" {
-				showNotification()
+				a.showNotification(n, &notification)
 			} else {
 				fyne.LogError("User rejected the request for notifications.", nil)
 			}
 			return nil
 		}))
-	} else {
-		showNotification()
+		return
 	}
+
+	a.showNotification(n, &notification)
+}
+
+func (a *fyneApp) showNotification(data *fyne.Notification, notification *js.Value) {
+	icon := a.icon.Content()
+	base64Str := base64.StdEncoding.EncodeToString(icon)
+	mimeType := http.DetectContentType(icon)
+	base64Img := fmt.Sprintf("data:%s;base64,%s", mimeType, base64Str)
+	notification.New(data.Title, map[string]any{
+		"body": data.Content,
+		"icon": base64Img,
+	})
+	fyne.LogError("done show...", nil)
 }
 
 var themeChanged = js.FuncOf(func(this js.Value, args []js.Value) any {
