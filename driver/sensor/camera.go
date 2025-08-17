23aa48fbc0dba4c080001a6a7bd4ad133fd150e4
Hayden Parker <hayden@hkparker.com>
2025-08-17 13:53:02 -0500

    add comment and since tag

--- driver/sensor/camera.go
+++ driver/sensor/camera.go
@@ -4,6 +4,14 @@ import (
 	"image"
 )
 
+// A camera device is a device that has hardware support for taking photos.
+//
+// Since: 2.7
 type CameraDevice interface {
+	// CapturePhoto opens a native photo capture view that allows the user to optionally
+	// take a photograph.  It returns the image, a boolean to indicate if an image was
+	// taken, and any errors.
+	//
+	// Since: 2.7
 	CapturePhoto() (image.Image, bool, error)
 }
