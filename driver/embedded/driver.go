b5fd84f8fb84c864753be30541312ac59f668d16
Andy Williams <andy@andy.xyz>
2025-09-10 19:52:53 +0100

    Add missing docs

--- driver/embedded/driver.go
+++ driver/embedded/driver.go
@@ -6,6 +6,10 @@ import (
 	"fyne.io/fyne/v2"
 )
 
+// Driver is an embedded driver designed for handling custom hardware.
+// Various standard driver implementations are available in the fyne-x project.
+//
+// Since: 2.7
 type Driver interface {
 	Render(image.Image)
 	Run(func())
--- driver/embedded/keyboard.go
+++ driver/embedded/keyboard.go
@@ -2,13 +2,26 @@ package embedded
 
 import "fyne.io/fyne/v2"
 
+// KeyDirection specifies the press/release of a key event
+//
+// Since: 2.7
 type KeyDirection uint8
 
 const (
+	// KeyPressed specifies that a key was pushed down.
+	//
+	// Since: 2.7
 	KeyPressed KeyDirection = iota
+
+	// KeyReleased indicates a key was let back up.
+	//
+	// Since: 2.7
 	KeyReleased
 )
 
+// KeyEvent is an event from keyboard actions occurring in an embedded device keyboard.
+//
+// Since: 2.7
 type KeyEvent struct {
 	Name      fyne.KeyName
 	Direction KeyDirection
@@ -16,6 +29,9 @@ type KeyEvent struct {
 
 func (d *KeyEvent) isEvent() {}
 
+// CharacterEvent is an event specifying that a character was created by a hardware or virtual keyboard.
+//
+// Since: 2.7
 type CharacterEvent struct {
 	Rune rune
 }
