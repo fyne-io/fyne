efab70ac32b5411df8d77f60055586cf3b132df0
Charles Clinton Pustejovsky III <charles@cpustejovsky.com>
2025-09-12 18:08:46 -0400

    suppress LogError for keyobard_test.go

--- internal/driver/mobile/keyboard_test.go
+++ internal/driver/mobile/keyboard_test.go
@@ -1,11 +1,19 @@
 package mobile
 
 import (
+	"io"
+	"log"
+	"os"
 	"testing"
 
 	_ "fyne.io/fyne/v2/test"
 )
 
 func TestDevice_HideVirtualKeyboard_BeforeRun(t *testing.T) {
+	// Discarding log output for tests
+	// The following method logs an error:
+	// (&device{}).hideVirtualKeyboard() // should not crash!
+	log.SetOutput(io.Discard)
+	t.Cleanup(func() { log.SetOutput(os.Stderr) })
 	(&device{}).hideVirtualKeyboard() // should not crash!
 }
