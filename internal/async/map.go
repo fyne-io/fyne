d762cb9f5c0fb0fec8ef93463fda3eaa9567de08
Jacalz <jacalz@tutanota.com>
2025-10-25 20:34:36 +0200

    Fix TODO regarding comparable map key

--- internal/async/map.go
+++ internal/async/map.go
@@ -65,3 +65,8 @@ func (m *Map[K, V]) Range(f func(key K, value V) bool) {
 func (m *Map[K, V]) Store(key K, value V) {
 	m.Map.Store(key, value)
 }
+
+// Clear deletes all the entries, resulting in an empty Map.
+func (m *Map[K, V]) Clear() {
+	m.Map.Clear()
+}
