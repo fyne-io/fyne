d24f198dbf36efff85ab6eb8805415f2f1f800c5
Eli Burch <eli.burch@burchbytes.com>
2026-04-06 17:43:36 -0700

    Reuse shaper to use internal cache

--- internal/painter/font.go
+++ internal/painter/font.go
@@ -38,6 +38,8 @@ var (
 	loaded       bool
 )
 
+var shaper = &shaping.HarfbuzzShaper{}
+
 func loadMap() {
 	loaded = true
 
@@ -269,7 +271,6 @@ func walkString(faces shaping.Fontmap, s string, textSize fixed.Int26_6, style f
 		Face:      faces.ResolveFace(' '),
 		Size:      textSize,
 	}
-	shaper := &shaping.HarfbuzzShaper{}
 	segmenter := &shaping.Segmenter{}
 	out := shaper.Shape(in)
 
@@ -291,7 +292,7 @@ func walkString(faces shaping.Fontmap, s string, textSize fixed.Int26_6, style f
 			if r == '\t' {
 				if pending {
 					in.RunEnd = i
-					x = shapeCallback(shaper, in, x, scale, cb)
+					x = shapeCallback(in, x, scale, cb)
 				}
 				x = tabStop(spacew, x, style.TabWidth)
 
@@ -303,7 +304,7 @@ func walkString(faces shaping.Fontmap, s string, textSize fixed.Int26_6, style f
 			}
 		}
 
-		x = shapeCallback(shaper, in, x, scale, cb)
+		x = shapeCallback(in, x, scale, cb)
 	}
 
 	*advance = x
@@ -311,7 +312,7 @@ func walkString(faces shaping.Fontmap, s string, textSize fixed.Int26_6, style f
 		fixed266ToFloat32(out.LineBounds.Ascent)
 }
 
-func shapeCallback(shaper shaping.Shaper, in shaping.Input, x, scale float32, cb func(shaping.Output, float32)) float32 {
+func shapeCallback(in shaping.Input, x, scale float32, cb func(shaping.Output, float32)) float32 {
 	out := shaper.Shape(in)
 	glyphs := out.Glyphs
 	start := 0
