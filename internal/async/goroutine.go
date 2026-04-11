6d29d01e8815da6b5d999c950fe89b0d18d98eff
Andy Williams <andy@andy.xyz>
2026-04-11 21:31:55 +0100

    Use more accurate wording

--- internal/async/goroutine.go
+++ internal/async/goroutine.go
@@ -32,7 +32,7 @@ func EnsureNotMain(fn func()) {
 
 	log.Println("*** Error in Fyne call thread, fyne.Do[AndWait] called from main goroutine ***")
 	log.Println("*** This error needs to be addressed as it will become a deadlock soon ***")
-	log.Println("*** The next major Fyne release will remove this safety! ***")
+	log.Println("*** The next major Fyne release will disable this safety by default! ***")
 
 	logStackTop(2)
 	go fn()
@@ -51,7 +51,7 @@ func EnsureMain(fn func()) {
 
 	log.Println("*** Error in Fyne call thread, this should have been called in fyne.Do[AndWait] ***")
 	log.Println("*** This error needs to be addressed as it could cause concurrent errors soon ***")
-	log.Println("*** The next major Fyne release will remove this safety! ***")
+	log.Println("*** The next major Fyne release will disable this safety by default! ***")
 
 	logStackTop(1)
 	fyne.DoAndWait(fn)
