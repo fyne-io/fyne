cb295d6e57780968b743ac314e7d6212c90e3e44
Drew Weymouth <dweymouth@gmail.com>
2026-07-20 14:18:40 -0700

    rebuild native MacOS menu when switching windows

--- internal/driver/glfw/window.go
+++ internal/driver/glfw/window.go
@@ -768,6 +768,10 @@ func (w *window) processFocused(focus bool) {
 		curWindow = w
 		w.canvas.FocusGained()
 
+		if build.HasNativeMenu && w.mainmenu != nil {
+			setupNativeMenu(w, w.mainmenu)
+		}
+
 		if build.IsWayland {
 			w.frame.markReady()
 			w.canvas.SetDirty()
